71db7fb6a5bb200ee0e9956c64cc5923b56ef44b
Flavio Ceolin <flavio.ceolin@intel.com>
2016-07-06 15:47:23 -0300

    storage:zephyr: Flash implementation
    This commit introduces persistency support using memory flash.
    It was designed and tested using arduino 101, so in case of a different
    flash the proper parameters should be given in the project
    configuration.
    
    Change-Id: I3fe1f216a15087131fcfd7c43d8c91577f733d7f
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>
    Reviewed-by: Kishen Maloor <kishen.maloor@intel.com>
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

--- port/zephyr/src/storage.c
+++ port/zephyr/src/storage.c
@@ -15,24 +15,201 @@
  */
 
 #ifdef OC_SECURITY
+
+#include <zephyr.h>
+#include <flash.h>
+#include <device.h>
+
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "port/oc_storage.h"
 
-//TODO:
+struct device *flash_device;
+size_t flash_sector_size, max_rw_size;
+
+static unsigned int
+align_power2(unsigned int value)
+{
+  unsigned int left_zeros;
+
+  if (value <= 1)
+    return value;
+
+  left_zeros = __builtin_clz(value - 1);
+  if (left_zeros <= 1)
+    return UINT_MAX;
+
+  return (unsigned int)1 << ((sizeof(value) * 8) - left_zeros);
+}
+
+/*
+ * It expects the device name, flash sector size and
+ * maximum read/write size separated by comma:
+ *
+ * device_name,flash_sector_size,max_rw_size
+ *
+ * For arduino 101 internal flash it will be:
+ * W25QXXDV,4096,256
+ */
 void
 oc_storage_config(const char *path)
 {
+  char *aux, device_name[16];
+  unsigned int size;
 
+  aux = strstr(path, ",");
+  if (!aux)
+    return;
+
+  if (aux - path > sizeof(device_name) -1)
+    return;
+
+  memcpy(device_name, path, aux - path);
+  device_name[aux - path] = '\0';
+
+  flash_device = device_get_binding((char *)path);
+  if (!flash_device)
+    return;
+
+#define PARSE_VALUE(_var) \
+  do { \
+    if (!aux) \
+      goto err; \
+    errno = 0; \
+    size = strtoul(aux + 1, NULL, 0); \
+    if (errno) \
+      goto err; \
+    _var = align_power2(size / 2 + 1); \
+    path = aux + 1; \
+    aux = strstr(path, ","); \
+  } while (0)
+
+  PARSE_VALUE(flash_sector_size);
+  PARSE_VALUE(max_rw_size);
+#undef PARSE_VALUE
+
+  return;
+
+err:
+  flash_device = NULL;
 }
 
+/*
+ * store should contains the memory position to read.
+ * The value should be multiple of the flash sector size.
+ */
 void
 oc_storage_read(const char *store, uint8_t *buf, size_t *size)
 {
+  int r;
+  size_t mem_offset, times = 0, extra = 0;
+
+  errno = 0;
+  mem_offset = strtoul(store, NULL, 0);
+  if (errno != 0)
+    return;
+
+  times = *size / max_rw_size;
+  extra = *size % max_rw_size;
+
+  if (!(times || extra)) {
+    r = flash_read(flash_device, mem_offset, buf, *size);
+    if (r < 0)
+      goto err;
+  } else {
+    size_t off = 0;
+
+    while (times) {
+      r = flash_read(flash_device, mem_offset + off,
+        buf + off, max_rw_size);
+      if (r < 0)
+        goto err;
+      off += max_rw_size;
+      times--;
+    }
+
+    if (extra) {
+      r = flash_read(flash_device, mem_offset + off,
+        buf + off, extra);
+      if (r < 0)
+        goto err;
+    }
+  }
+
+  return;
+
+err:
   *size = 0;
 }
 
+/*
+ * store should contains the memory position to write.
+ * The value should be multiple of the flash sector size.
+ */
 void
 oc_storage_write(const char *store, uint8_t *buf, size_t size)
 {
+  int r = 0;
+  size_t mem_offset, times = 0, extra = 0, erase_value = flash_sector_size;
 
+  errno = 0;
+  mem_offset = strtoul(store, NULL, 0);
+  if (errno != 0)
+    return;
+
+  r = flash_write_protection_set(flash_device, false);
+  if (r < 0)
+    return;
+
+  times = size / max_rw_size;
+  extra = size % max_rw_size;
+
+  if (times) {
+    erase_value *= times;
+    if (extra)
+      erase_value += flash_sector_size;
+  }
+
+  r = flash_erase(flash_device, mem_offset, erase_value);
+  if (r < 0)
+    return;
+
+  if (!(times || extra)) {
+    r = flash_write_protection_set(flash_device, false);
+    if (r < 0)
+      return;
+
+    r = flash_write(flash_device, mem_offset, buf, size);
+    if (r < 0)
+      return;
+  } else {
+    size_t off = 0;
+
+    while (times) {
+      r = flash_write_protection_set(flash_device, false);
+      if (r < 0)
+        return;
+
+      r = flash_write(flash_device, mem_offset + off, buf + off, max_rw_size);
+      if (r < 0)
+        return;
+      off += max_rw_size;
+      times--;
+    }
+
+    if (extra) {
+      r = flash_write_protection_set(flash_device, false);
+      if (r < 0)
+        return;
+
+      r = flash_write(flash_device, mem_offset + off, buf + off, extra);
+      if (r < 0)
+        return;
+    }
+  }
 }
+
 #endif /* OC_SECURITY */
