ae91c88a733b75c41907b8f1898814bb8d12d046
Kishen Maloor <kishen.maloor@intel.com>
2016-07-10 19:09:00 -0700

    Fixes to api/*
    * Include security/* headers only within OC_SECURITY blocks
    * Minor tweaks to pass coverity static analyses
    
    Change-Id: Ifb9a4d2a68399e34993e5c62d3c1a8f805732f40
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/10403

--- api/oc_server_api.c
+++ api/oc_server_api.c
@@ -83,7 +83,7 @@ static int
 response_length()
 {
   int size = oc_rep_finalize();
-  return  (size >= 0 && size <= 2)?0:size;
+  return (size <= 2)?0:size;
 }
 
 void
@@ -278,8 +278,10 @@ oc_send_slow_response(oc_slow_response_t *handle,
 	if(cur->observe == 1) {
 	  coap_set_header_observe(response, 1);
 	}
-	coap_set_payload(response, handle->buffer,
-			 response_buffer.response_length);
+	if (response_buffer.response_length > 0) {
+	  coap_set_payload(response, handle->buffer,
+			   response_buffer.response_length);
+	}
 	t->message->length = coap_serialize_message(response,
 						    t->message->data);
 	coap_send_transaction(t);
