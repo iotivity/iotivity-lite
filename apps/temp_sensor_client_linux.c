8e0f1ed7fb5f23b8a92cede69f58f4c94d2154d3
Kishen Maloor <kishen.maloor@intel.com>
2016-07-15 16:34:08 -0700

    Added new sample applications
    * Server application exclusively for Zephyr to demonstrate
      periodic (or otherwise) reads from hardware (fake temperature sensor)
      in a fiber, and also defining an interrupt handler block using tiny
      OCF's APIs. The fiber constantly reads new values, and signals the
      interrupt handler once its finished reading a new value. The interrupt
      handler once scheduled processes any outstanding GET request for that
      value.
      The server also demonstrates use of the separate response feature
      for performing GET requests.
    * A regular client application for Linux that subscribes for notifications
      of new temperature sensor readings from the server.
    
    Change-Id: Ibb2a269b957aad53ce6b8695518daa8776b372d7
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Signed-off-by: Thiago Macieira <thiago.macieira@intel.com>

---
+++ apps/temp_sensor_client_linux.c
@@ -0,0 +1,165 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "oc_api.h"
+
+void
+app_init(void)
+{
+  oc_init_platform("GE", NULL, NULL);
+  oc_add_device("/oic/d", "oic.d.smarthub", "Smart home hub", "1.0",
+		"1.0", NULL, NULL);
+}
+
+#ifdef OC_SECURITY
+void
+fetch_credentials(void)
+{
+  oc_storage_config("./creds");
+}
+#endif
+
+#define MAX_URI_LENGTH (30)
+static char temp_1[MAX_URI_LENGTH];
+static oc_server_handle_t temp_sensor;
+static int temperature;
+
+oc_event_callback_retval_t
+stop_observe(void* data)
+{
+  PRINT("Stopping OBSERVE\n");
+  oc_stop_observe(temp_1, &temp_sensor);
+  return DONE;
+}
+
+void
+get_temp(oc_client_response_t *data)
+{
+  oc_rep_t *rep = data->payload;
+  while (rep != NULL) {
+    PRINT("key %s, value ", oc_string(rep->name));
+    switch (rep->type) {
+    case INT:
+      PRINT("%d\n", rep->value_int);
+      temperature = rep->value_int;
+      break;
+    default:
+      break;
+    }
+    rep = rep->next;
+  }
+}
+
+oc_discovery_flags_t
+discovery(const char *di,
+	  const char *uri,
+	  oc_string_array_t types,
+	  oc_interface_mask_t interfaces,
+	  oc_server_handle_t* server)
+{
+  int i;
+  int uri_len = strlen(uri);
+  uri_len = (uri_len >= MAX_URI_LENGTH)?MAX_URI_LENGTH-1:uri_len;
+
+  for (i = 0; i < oc_string_array_get_allocated_size(types); i++) {
+    char *t = oc_string_array_get_item(types, i);
+    if (strlen(t) == 16 && strncmp(t, "oic.r.tempsensor", 16) == 0) {
+      memcpy(&temp_sensor, server, sizeof(oc_server_handle_t));
+
+      strncpy(temp_1, uri, uri_len);
+      temp_1[uri_len] = '\0';
+
+      oc_do_observe(temp_1, &temp_sensor, NULL, &get_temp, HIGH_QOS);
+      oc_set_delayed_callback(NULL, &stop_observe, 30);
+
+      return OC_STOP_DISCOVERY;
+    }
+  }
+  return OC_CONTINUE_DISCOVERY;
+}
+
+void
+issue_requests(void)
+{
+  oc_do_ip_discovery("oic.r.tempsensor", &discovery);
+}
+
+#include <stdio.h>
+#include <signal.h>
+#include <pthread.h>
+#include "port/oc_signal_main_loop.h"
+#include "port/oc_clock.h"
+
+pthread_mutex_t mutex;
+pthread_cond_t cv;
+struct timespec ts;
+
+int quit = 0;
+
+void
+oc_signal_main_loop(void)
+{
+  pthread_cond_signal(&cv);
+}
+
+void
+handle_signal(int signal)
+{
+  oc_signal_main_loop();
+  quit = 1;
+}
+
+int
+main(void)
+{
+  struct sigaction sa;
+  sigfillset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sa.sa_handler = handle_signal;
+  sigaction(SIGINT, &sa, NULL);
+
+  oc_handler_t handler = {.init = app_init,
+#ifdef OC_SECURITY
+			  .get_credentials = fetch_credentials,
+#endif /* OC_SECURITY */
+			  .requests_entry = issue_requests
+  };
+
+  oc_clock_time_t next_event;
+
+  int init = oc_main_init(&handler);
+
+  if (init < 0)
+    return init;
+
+  while (quit != 1) {
+    next_event = oc_main_poll();
+    pthread_mutex_lock(&mutex);
+    if (next_event == 0) {
+      pthread_cond_wait(&cv, &mutex);
+    }
+    else {
+      ts.tv_sec = (next_event / OC_CLOCK_SECOND);
+      ts.tv_nsec = (next_event % OC_CLOCK_SECOND) * 1.e09 / OC_CLOCK_SECOND;
+      pthread_cond_timedwait(&cv, &mutex, &ts);
+    }
+    pthread_mutex_unlock(&mutex);
+  }
+
+  oc_main_shutdown();
+
+  return 0;
+}
