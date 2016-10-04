24289303a0c1d599c0fb157abaf0b4e430dbdaf0
Kishen Maloor <kishen.maloor@intel.com>
2016-10-04 11:39:43 -0700

    Add a port for Contiki
    * Contains simple implementation of interfaces to run IoTivity-Constrained
      with Contiki.
    * Added server_contiki/client_contiki samples that are also compatible with
      client/server samples for other ports (Linux, RIOT OS and Zephyr)
    * The connectivity implementation uses Contiki's simple-udp APIs.
    * README describes steps to build the samples.
    
    Change-Id: I57a5a0b2123b52d37c43a0f3627850fc38d32fb4
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>

---
+++ apps/server_contiki.c
@@ -0,0 +1,122 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "oc_api.h"
+
+static bool light_state = false;
+
+static void
+app_init(void)
+{
+  oc_init_platform("Intel", NULL, NULL);
+  oc_add_device("/oic/d", "oic.d.light", "Kishen's light", "1.0", "1.0", NULL,
+                NULL);
+}
+
+static void
+get_light(oc_request_t *request, oc_interface_mask_t interface, void *user_data)
+{
+  PRINT("GET_light:\n");
+  oc_rep_start_root_object();
+  switch (interface) {
+  case OC_IF_BASELINE:
+    oc_process_baseline_interface(request->resource);
+  case OC_IF_RW:
+    oc_rep_set_boolean(root, state, light_state);
+    break;
+  default:
+    break;
+  }
+  oc_rep_end_root_object();
+  oc_send_response(request, OC_STATUS_OK);
+  PRINT("Light state %d\n", light_state);
+}
+
+static void
+put_light(oc_request_t *request, oc_interface_mask_t interface, void *user_data)
+{
+  PRINT("PUT_light:\n");
+  bool state = false;
+  oc_rep_t *rep = request->request_payload;
+  while (rep != NULL) {
+    PRINT("key: %s ", oc_string(rep->name));
+    switch (rep->type) {
+    case BOOL:
+      state = rep->value_boolean;
+      PRINT("value: %d\n", state);
+      break;
+    default:
+      oc_send_response(request, OC_STATUS_BAD_REQUEST);
+      return;
+      break;
+    }
+    rep = rep->next;
+  }
+  oc_send_response(request, OC_STATUS_CHANGED);
+  light_state = state;
+}
+
+static void
+register_resources(void)
+{
+  oc_resource_t *res = oc_new_resource("/light/1", 1, 0);
+  oc_resource_bind_resource_type(res, "oic.r.light");
+  oc_resource_bind_resource_interface(res, OC_IF_RW);
+  oc_resource_set_default_interface(res, OC_IF_RW);
+  oc_resource_set_discoverable(res, true);
+  oc_resource_set_periodic_observable(res, 1);
+  oc_resource_set_request_handler(res, OC_GET, get_light, NULL);
+  oc_resource_set_request_handler(res, OC_PUT, put_light, NULL);
+  oc_add_resource(res);
+}
+
+#include "contiki.h"
+
+PROCESS(sample_server_process, "OCF server sample");
+AUTOSTART_PROCESSES(&sample_server_process);
+
+static void
+signal_event_loop(void)
+{
+  process_post(&sample_server_process, PROCESS_EVENT_TIMER, NULL);
+}
+
+PROCESS_THREAD(sample_server_process, ev, data)
+{
+  static struct etimer et;
+  static const oc_handler_t handler = {.init = app_init,
+                                       .signal_event_loop = signal_event_loop,
+                                       .register_resources =
+                                         register_resources };
+  static oc_clock_time_t next_event;
+
+  PROCESS_BEGIN();
+
+  int init = oc_main_init(&handler);
+  if (init < 0)
+    return init;
+
+  while (ev != PROCESS_EVENT_EXIT) {
+    next_event = oc_main_poll();
+    if (next_event != 0) {
+      next_event -= oc_clock_time();
+      etimer_set(&et, next_event);
+    }
+    PROCESS_YIELD();
+  }
+
+  PROCESS_END();
+}
