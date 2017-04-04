bf11970e338954b60d2f69e1bf323e86376efc87
ChaJiWon <jw_wonny.cha@samsung.com>
2017-04-04 17:20:32 +0900

    Zephyr port support IPv4 feature
    added features to work with IPv4 in zephyr selectively,
    reflecting the need to operate with IPv4
    
    Change-Id: If71d751932a8fa42a28115197f4c306498fa582d
    Signed-off-by: ChaJiWon <jw_wonny.cha@samsung.com>

--- port/zephyr/src/ipadapter.c
+++ port/zephyr/src/ipadapter.c
@@ -35,6 +35,10 @@
 /* Server's receive socket */
 static struct net_context *udp_recv6;
 
+#ifdef OC_IPV4
+static struct net_context *udp_recv4;
+#endif
+
 /* "All OCF nodes" multicast address and port */
 #define OCF_MCAST_IP6ADDR                                                      \
   {                                                                            \
@@ -48,6 +52,7 @@ static struct in6_addr in6addr_mcast = OCF_MCAST_IP6ADDR;
 #define OCF_MCAST_PORT (5683)
 /* Multicast receive socket */
 static struct net_context *mcast_recv6;
+static struct net_context *mcast_recv4;
 
 #ifdef OC_SECURITY
 /* DTLS receive socket */
@@ -86,10 +91,12 @@ oc_network_receive(struct net_context *context, struct net_buf *buf, int status,
   oc_message_t *message = oc_allocate_message();
 
   if (message) {
+    sa_family_t family = net_nbuf_family(buf);
     uint16_t pos;
     size_t bytes_read = net_nbuf_appdatalen(buf);
     size_t offset_from_start = net_buf_frags_len(buf) - bytes_read;
     bytes_read = (bytes_read < OC_PDU_SIZE) ? bytes_read : OC_PDU_SIZE;
+
     struct net_buf *frag = net_nbuf_read(buf->frags, offset_from_start, &pos,
                                          bytes_read, message->data);
     if (!frag && pos == 0xffff) {
@@ -99,13 +106,37 @@ oc_network_receive(struct net_context *context, struct net_buf *buf, int status,
     }
 
     message->length = bytes_read;
-    if (user_data != NULL)
-      message->endpoint.flags = IPV6 | SECURED;
-    else
-      message->endpoint.flags = IPV6;
-    memcpy(message->endpoint.addr.ipv6.address, &NET_IPV6_BUF(buf)->src, 16);
-    message->endpoint.addr.ipv6.scope = 0;
-    message->endpoint.addr.ipv6.port = ntohs(NET_UDP_BUF(buf)->src_port);
+    if (user_data != NULL) {
+      #ifdef OC_IPV4
+          if (family == AF_INET)
+            message->endpoint.flags = IPV4 | SECURED;
+          else
+      #endif
+          if (family == AF_INET6)
+            message->endpoint.flags = IPV6 | SECURED;
+    }
+    else {
+      #ifdef OC_IPV4
+          if (family == AF_INET)
+            message->endpoint.flags = IPV4;
+          else
+      #endif
+          if (family == AF_INET6)
+            message->endpoint.flags = IPV6;
+    }
+
+    #ifdef OC_IPV4
+        if (family == AF_INET) {
+          memcpy(message->endpoint.addr.ipv4.address, &NET_IPV4_BUF(buf)->src, 16);
+          message->endpoint.addr.ipv4.port = ntohs(NET_UDP_BUF(buf)->src_port);
+        }
+        else
+    #endif
+        if (family == AF_INET6) {
+        memcpy(message->endpoint.addr.ipv6.address, &NET_IPV6_BUF(buf)->src, 16);
+        message->endpoint.addr.ipv6.scope = 0;
+        message->endpoint.addr.ipv6.port = ntohs(NET_UDP_BUF(buf)->src_port);
+      }
 
     PRINT("oc_network_receive: received %d bytes\n", (int)message->length);
     PRINT("oc_network_receive: incoming message: ");
@@ -114,7 +145,6 @@ oc_network_receive(struct net_context *context, struct net_buf *buf, int status,
 
     oc_network_event(message);
   }
-
   net_nbuf_unref(buf);
 }
 
@@ -134,22 +164,45 @@ oc_send_buffer(oc_message_t *message)
   PRINTipaddr(message->endpoint);
   PRINT("\n");
 
-  /* Populate destination address structure */
-  struct sockaddr_in6 peer_addr;
-  memcpy(peer_addr.sin6_addr.in6_u.u6_addr8,
-         message->endpoint.addr.ipv6.address, 16);
-  peer_addr.sin6_family = AF_INET6;
-  peer_addr.sin6_port = htons(message->endpoint.addr.ipv6.port);
-
+#ifdef OC_IPV4
+  struct sockaddr_in peer_addr4;
+#endif
+  struct sockaddr_in6 peer_addr;  
+  
   /* Network buffer to hold data to be sent */
   struct net_buf *send_buf;
+  int ret;
+  
+  #ifdef OC_IPV4
+    if (message->endpoint.flags & IPV4) {
+      memcpy(peer_addr4.sin_addr.in4_u.u4_addr8, message->endpoint.addr.ipv4.address, 16);
+      peer_addr4.sin_family = AF_INET;
+      peer_addr4.sin_port = htons(message->endpoint.addr.ipv4.port);
+    }
+    else
+  #endif
+    if (message->endpoint.flags & IPV6) {
+  /* Populate destination address structure */
+      memcpy(peer_addr.sin6_addr.in6_u.u6_addr8, message->endpoint.addr.ipv6.address, 16);
+      peer_addr.sin6_family = AF_INET6;
+      peer_addr.sin6_port = htons(message->endpoint.addr.ipv6.port);
+    }
+
 #ifdef OC_SECURITY
   if (message->endpoint.flags & SECURED) {
     send_buf = net_nbuf_get_tx(dtls_recv6, K_NO_WAIT);
   } else
 #endif /* OC_SECURITY */
   {
-    send_buf = net_nbuf_get_tx(udp_recv6, K_NO_WAIT);
+    #ifdef OC_IPV4
+      if (message->endpoint.flags & IPV4) {
+        send_buf = net_nbuf_get_tx(udp_recv4, K_NO_WAIT);
+      } 
+      else
+    #endif
+      if (message->endpoint.flags & IPV6) {
+        send_buf = net_nbuf_get_tx(udp_recv6, K_NO_WAIT);
+    }
   }
   if (!send_buf) {
     OC_WRN("oc_send_buffer: cannot acquire send_buf\n");
@@ -162,14 +215,102 @@ oc_send_buffer(oc_message_t *message)
     return;
   }
 
-  int ret = net_context_sendto(
-    send_buf, (struct sockaddr *)&peer_addr, sizeof(struct sockaddr_in6),
-    udp_sent, 0, UINT_TO_POINTER(net_buf_frags_len(send_buf)), NULL);
+  #ifdef OC_IPV4
+      if (message->endpoint.flags & IPV4) {
+        ret = net_context_sendto(
+        send_buf, (struct sockaddr *)&peer_addr4, sizeof(struct sockaddr_in),
+        udp_sent, 0, UINT_TO_POINTER(net_buf_frags_len(send_buf)), NULL);
+      } 
+      else if (message->endpoint.flags & IPV6) {
+  #else
+      if (message->endpoint.flags & IPV6) {
+  #endif
+        ret = net_context_sendto(
+        send_buf, (struct sockaddr *)&peer_addr, sizeof(struct sockaddr_in6),
+        udp_sent, 0, UINT_TO_POINTER(net_buf_frags_len(send_buf)), NULL);
+      }
   if (ret < 0) {
     OC_WRN("oc_send_buffer: cannot send data to peer (%d)\n", ret);
     net_nbuf_unref(send_buf);
   }
 }
+
+#ifdef OC_IPV4
+static int
+connectivity_ipv4_init(void)
+{
+  int ret;
+  struct sockaddr_in mcast_addr = { 0 };
+  struct sockaddr_in my_addr = { 0 };
+
+  net_dhcpv4_start(net_if_get_default());
+
+  mcast_addr.sin_family = AF_INET;
+  mcast_addr.sin_port = htons(OCF_MCAST_PORT);
+
+  /* Wildcard address set for server with randomly chosen port */
+  my_addr.sin_family = AF_INET;
+
+  ret = net_context_get(AF_INET, SOCK_DGRAM, IPPROTO_UDP, &udp_recv4);
+  if (ret < 0) {
+    OC_WRN("oc_connectivity_init: cannot get UDP network context for server"
+           "receive (%d)\n",
+           ret);
+    goto error;
+  }
+
+  ret = net_context_bind(udp_recv4, (const struct sockaddr *)&my_addr,
+                         sizeof(struct sockaddr_in));
+  if (ret < 0) {
+    OC_WRN("oc_connectivity_init: cannot bind UDP port %d to server's network"
+           "context (%d)\n",
+           ntohs(my_addr.sin_port), ret);
+    goto error;
+  }
+
+  ret = net_context_get(AF_INET, SOCK_DGRAM, IPPROTO_UDP, &mcast_recv4);
+  if (ret < 0) {
+    OC_WRN("oc_connectivity_init: cannot get UDP network context for OCF"
+           "multicast receive (%d)\n",
+           ret);
+    goto error;
+  }
+
+  ret = net_context_bind(mcast_recv4, (const struct sockaddr *)&mcast_addr,
+                         sizeof(struct sockaddr_in));
+  if (ret < 0) {
+    OC_WRN("oc_connectivity_init: cannot bind OCF multicast network context"
+           "(%d)\n",
+           ret);
+    goto error;
+  }
+
+  ret = net_context_recv(mcast_recv4, oc_network_receive, 0, NULL);
+  if (ret < 0) {
+    OC_WRN("oc_connectivity_init: net_context_recv error from multicast socket:"
+           "(%d)\n",
+           ret);
+    goto error;
+  }
+
+  ret = net_context_recv(udp_recv4, oc_network_receive, 0, NULL);
+  if (ret < 0) {
+    OC_WRN("oc_connectivity_init: net_context_recv error from server socket:"
+           "(%d)\n",
+           ret);
+    goto error;
+  }
+
+  OC_DBG("oc_connectivity_init: successfully initialized connectivity\n");
+  return 0;
+
+error:
+  OC_ERR("oc_connectivity_init: failed to initialize connectivity\n");
+  net_context_put(udp_recv4);
+  net_context_put(mcast_recv4);
+  return -1;
+}
+#endif
 
 int
 oc_connectivity_init(void)
@@ -285,6 +426,11 @@ oc_connectivity_init(void)
   }
 #endif /* OC_SECURITY */
 
+#ifdef OC_IPV4
+  if (connectivity_ipv4_init() != 0)
+    PRINT("Could not initialize IPv4\n");
+#endif
+
   OC_DBG("oc_connectivity_init: successfully initialized connectivity\n");
   return 0;
 
@@ -301,6 +447,10 @@ oc_connectivity_shutdown(void)
 #endif /* OC_SECURITY */
   net_context_put(udp_recv6);
   net_context_put(mcast_recv6);
+#ifdef OC_IPV4
+  net_context_put(udp_recv4);
+  net_context_put(mcast_recv4);
+#endif
 }
 
 #ifdef OC_CLIENT
