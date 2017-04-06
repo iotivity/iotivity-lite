ea737cc13624f54032f673a1ca3c281d78c2ac1d
Kishen Maloor <kishen.maloor@intel.com>
2017-04-06 14:29:49 -0700

    Fix debug output message in oc_rep
    Change-Id: Iff37cd5b2bb9ac72cc43bf4c6f6ca694b8725ca2
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>

--- api/oc_rep.c
+++ api/oc_rep.c
@@ -39,7 +39,7 @@ oc_rep_finalize(void)
   int size = cbor_encoder_get_buffer_size(&g_encoder, g_buf);
 #ifdef OC_DEBUG
   if (size < 0 && g_err == CborErrorOutOfMemory)
-    oc_abort("Insufficient memory: Increase OC_MAX_PDU_BUFFER_SIZE to "
+    oc_abort("Insufficient memory: Increase OC_MAX_APP_DATA_SIZE to "
              "accomodate a larger payload\n");
 #endif /* OC_DEBUG */
   if (g_err != CborNoError)
