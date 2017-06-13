c23a32956dc7f811c9758ce49c595f2bba11ba99
Flavio Ceolin <flavio.ceolin@intel.com>
2017-06-12 20:52:40 -0700

    tests:linux: Add server init test
    Change-Id: I948c9499e879e15f0c8eda3835894975cccb2960
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/20815
    Reviewed-by: Kishen Maloor <kishen.maloor@intel.com>
    Tested-by: Kishen Maloor <kishen.maloor@intel.com>

---
+++ tests/server_init_linux.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "test.h"
+
+#include "oc_api.h"
+#include "port/oc_clock.h"
+
+#include <stdio.h>
+
+static int
+app_init(void)
+{
+  int ret;
+
+  ret = oc_init_platform("Intel", NULL, NULL);
+  ASSERT(ret == 0);
+
+  ret = oc_add_device("/oic/d", "oic.d.test-server-init", "Server init test", "1.0", "1.0",
+                       NULL, NULL);
+  return ret;
+}
+
+static void
+signal_event_loop(void)
+{
+}
+
+static void
+register_resources(void)
+{
+}
+
+int
+main(void)
+{
+  int init;
+  const oc_handler_t handler = {
+		.init = app_init,
+		.signal_event_loop = signal_event_loop,
+		.register_resources = register_resources,
+	};
+
+  init = oc_main_init(&handler);
+  ASSERT(init == 0);
+  oc_main_shutdown();
+
+  return 0;
+}
