ec2467b5a68c861a7194aafaad519977746f3ca3
Kishen Maloor <kishen.maloor@intel.com>
2017-12-21 15:02:12 -0800

    oc_acl: fix handling of wildcard ACEs
    Change-Id: I3832767ba235388949199712e6c20467c7b32c9b
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/23817

--- security/oc_acl.h
+++ security/oc_acl.h
@@ -35,9 +35,9 @@ typedef enum {
 
 typedef enum {
   OC_ACE_NO_WC = -1,
-  OC_ACE_WC_ALL = 0,
-  OC_ACE_WC_ALL_DISCOVERABLE,
-  OC_ACE_WC_ALL_NON_DISCOVERABLE,
+  OC_ACE_WC_ALL = 0x11,
+  OC_ACE_WC_ALL_DISCOVERABLE = 0x01,
+  OC_ACE_WC_ALL_NON_DISCOVERABLE = 0x10,
 } oc_ace_wildcard_t;
 
 typedef enum {
