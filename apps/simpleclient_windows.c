fd3d135e189bf259c6d778cc5a951f37a6457688
Kishen Maloor <kishen.maloor@intel.com>
2018-02-14 17:00:00 -0800

    Update Windows port
    This change builds upon the abandoned change 21971 (commit 6e7d800b
    by Reinhard Baier), and gets the Windows adaptation up to speed with
    the current states of the rest of the stack.
    
    Below is a quick summary of the changes:
    
    * Restructure solution into 3 projects: IoTivity-Constrained
      (builds framework as .lib), SimpleClient (builds simpleclient
      sample referencing the framework .lib) and SimpleServer
      (builds SimpleServer sample referenceing the framework .lib).
    * For simplicity sake, all 3 projects have the client and server
      roles compiled in. The sample apps demonstrate their
      respective roles by appropriately using the APIs.
    * The app projects create the required "creds" path (much like
      in the Linux build) for persisting the contents of all
      security resources.
    * Includes the mbedTLS library as part of the
      "IoTivity-Constrained" project. On-boarding/provisioning of
      the simpleclient/simpleserver samples running on Windows
      has been tested, and is known to work.
    * Revamps the connectivity implementation in a
      big way by using an alternative means and flow to receive network
      events from sockets and configure multicast sockets.
      It further adds the capability to monitor changes to network
      interfaces and appropriately reconfigures multicast sockets
      as a consequence of those changes.
    * simpleclient_windows and simpleserver_windows demonstrate
      API flows in the main task for Windows.
    * Removed old solution and project files.
    
    Change-Id: I85a523467f32657e76360c1d0a9ce14a29d346cf
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/24157

---
+++ apps/simpleclient_windows.c
@@ -0,0 +1,284 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "oc_api.h"
+#include "port/oc_clock.h"
+#include <signal.h>
+#include <windows.h>
+
+int quit = 0;
+
+static CONDITION_VARIABLE cv;
+static CRITICAL_SECTION cs;
+
+static int
+app_init(void)
+{
+  int ret = oc_init_platform("Apple", NULL, NULL);
+  ret |= oc_add_device("/oic/d", "oic.d.phone", "Kishen's IPhone", "ocf.1.0.0",
+                       "ocf.res.1.0.0", NULL, NULL);
+  return ret;
+}
+
+#define MAX_URI_LENGTH (30)
+static char a_light[MAX_URI_LENGTH];
+static oc_endpoint_t *light_server;
+
+static bool state;
+static int power;
+static oc_string_t name;
+
+static oc_event_callback_retval_t
+stop_observe(void *data)
+{
+  (void)data;
+  PRINT("Stopping OBSERVE\n");
+  oc_stop_observe(a_light, light_server);
+  return OC_EVENT_DONE;
+}
+
+static void
+observe_light(oc_client_response_t *data)
+{
+  PRINT("OBSERVE_light:\n");
+  oc_rep_t *rep = data->payload;
+  while (rep != NULL) {
+    PRINT("key %s, value ", oc_string(rep->name));
+    switch (rep->type) {
+    case OC_REP_BOOL:
+      PRINT("%d\n", rep->value.boolean);
+      state = rep->value.boolean;
+      break;
+    case OC_REP_INT:
+      PRINT("%d\n", rep->value.integer);
+      power = rep->value.integer;
+      break;
+    case OC_REP_STRING:
+      PRINT("%s\n", oc_string(rep->value.string));
+      if (oc_string_len(name))
+        oc_free_string(&name);
+      oc_new_string(&name, oc_string(rep->value.string),
+                    oc_string_len(rep->value.string));
+      break;
+    default:
+      break;
+    }
+    rep = rep->next;
+  }
+}
+
+static void
+post2_light(oc_client_response_t *data)
+{
+  PRINT("POST2_light:\n");
+  if (data->code == OC_STATUS_CHANGED)
+    PRINT("POST response: CHANGED\n");
+  else if (data->code == OC_STATUS_CREATED)
+    PRINT("POST response: CREATED\n");
+  else
+    PRINT("POST response code %d\n", data->code);
+
+  oc_do_observe(a_light, light_server, NULL, &observe_light, LOW_QOS, NULL);
+  oc_set_delayed_callback(NULL, &stop_observe, 30);
+  PRINT("Sent OBSERVE request\n");
+}
+
+static void
+post_light(oc_client_response_t *data)
+{
+  PRINT("POST_light:\n");
+  if (data->code == OC_STATUS_CHANGED)
+    PRINT("POST response: CHANGED\n");
+  else if (data->code == OC_STATUS_CREATED)
+    PRINT("POST response: CREATED\n");
+  else
+    PRINT("POST response code %d\n", data->code);
+
+  if (oc_init_post(a_light, light_server, NULL, &post2_light, LOW_QOS, NULL)) {
+    oc_rep_start_root_object();
+    oc_rep_set_boolean(root, state, true);
+    oc_rep_set_int(root, power, 55);
+    oc_rep_end_root_object();
+    if (oc_do_post())
+      PRINT("Sent POST request\n");
+    else
+      PRINT("Could not send POST request\n");
+  } else
+    PRINT("Could not init POST request\n");
+}
+
+static void
+put_light(oc_client_response_t *data)
+{
+  PRINT("PUT_light:\n");
+
+  if (data->code == OC_STATUS_CHANGED)
+    PRINT("PUT response: CHANGED\n");
+  else
+    PRINT("PUT response code %d\n", data->code);
+
+  if (oc_init_post(a_light, light_server, NULL, &post_light, LOW_QOS, NULL)) {
+    oc_rep_start_root_object();
+    oc_rep_set_boolean(root, state, false);
+    oc_rep_set_int(root, power, 105);
+    oc_rep_end_root_object();
+    if (oc_do_post())
+      PRINT("Sent POST request\n");
+    else
+      PRINT("Could not send POST request\n");
+  } else
+    PRINT("Could not init POST request\n");
+}
+
+static void
+get_light(oc_client_response_t *data)
+{
+  PRINT("GET_light:\n");
+  oc_rep_t *rep = data->payload;
+  while (rep != NULL) {
+    PRINT("key %s, value ", oc_string(rep->name));
+    switch (rep->type) {
+    case OC_REP_BOOL:
+      PRINT("%d\n", rep->value.boolean);
+      state = rep->value.boolean;
+      break;
+    case OC_REP_INT:
+      PRINT("%d\n", rep->value.integer);
+      power = rep->value.integer;
+      break;
+    case OC_REP_STRING:
+      PRINT("%s\n", oc_string(rep->value.string));
+      if (oc_string_len(name))
+        oc_free_string(&name);
+      oc_new_string(&name, oc_string(rep->value.string),
+                    oc_string_len(rep->value.string));
+      break;
+    default:
+      break;
+    }
+    rep = rep->next;
+  }
+
+  if (oc_init_put(a_light, light_server, NULL, &put_light, LOW_QOS, NULL)) {
+    oc_rep_start_root_object();
+    oc_rep_set_boolean(root, state, true);
+    oc_rep_set_int(root, power, 15);
+    oc_rep_end_root_object();
+
+    if (oc_do_put())
+      PRINT("Sent PUT request\n");
+    else
+      PRINT("Could not send PUT request\n");
+  } else
+    PRINT("Could not init PUT request\n");
+}
+
+static oc_discovery_flags_t
+discovery(const char *anchor, const char *uri, oc_string_array_t types,
+          oc_interface_mask_t interfaces, oc_endpoint_t *endpoint,
+          oc_resource_properties_t bm, void *user_data)
+{
+  (void)anchor;
+  (void)user_data;
+  (void)interfaces;
+  (void)bm;
+  int i;
+  int uri_len = strlen(uri);
+  uri_len = (uri_len >= MAX_URI_LENGTH) ? MAX_URI_LENGTH - 1 : uri_len;
+  PRINT("\n\nDISCOVERYCB %s %s %d\n\n", anchor, uri,
+        oc_string_array_get_allocated_size(types));
+  for (i = 0; i < (int)oc_string_array_get_allocated_size(types); i++) {
+    char *t = oc_string_array_get_item(types, i);
+    PRINT("\n\nDISCOVERED RES %s\n\n\n", t);
+    if (strlen(t) == 10 && strncmp(t, "core.light", 10) == 0) {
+      light_server = endpoint;
+      strncpy(a_light, uri, uri_len);
+      a_light[uri_len] = '\0';
+
+      PRINT("Resource %s hosted at endpoints:\n", a_light);
+      oc_endpoint_t *ep = endpoint;
+      while (ep != NULL) {
+        PRINTipaddr(*ep);
+        PRINT("\n");
+        ep = ep->next;
+      }
+
+      oc_do_get(a_light, light_server, NULL, &get_light, LOW_QOS, NULL);
+
+      return OC_STOP_DISCOVERY;
+    }
+  }
+  oc_free_server_endpoints(endpoint);
+  return OC_CONTINUE_DISCOVERY;
+}
+
+static void
+issue_requests(void)
+{
+  oc_do_ip_discovery("core.light", &discovery, NULL);
+}
+
+static void
+signal_event_loop(void)
+{
+  WakeConditionVariable(&cv);
+}
+
+void
+handle_signal(int signal)
+{
+  signal_event_loop();
+  quit = 1;
+}
+
+int
+main(void)
+{
+  InitializeCriticalSection(&cs);
+  InitializeConditionVariable(&cv);
+
+  int init;
+
+  signal(SIGINT, handle_signal);
+
+  static const oc_handler_t handler = {.init = app_init,
+                                       .signal_event_loop = signal_event_loop,
+                                       .register_resources = 0,
+                                       .requests_entry = issue_requests };
+
+  oc_clock_time_t next_event;
+
+#ifdef OC_SECURITY
+  oc_storage_config("./simpleclient_creds/");
+#endif /* OC_SECURITY */
+
+  init = oc_main_init(&handler);
+  if (init < 0)
+    return init;
+
+  while (quit != 1) {
+    next_event = oc_main_poll();
+    if (next_event == 0) {
+      SleepConditionVariableCS(&cv, &cs, INFINITE);
+    } else {
+      SleepConditionVariableCS(&cv, &cs,
+                               (DWORD)(next_event / (1000 * OC_CLOCK_SECOND)));
+    }
+  }
+
+  oc_main_shutdown();
+  return 0;
+}
---
+++ apps/simpleserver_windows.c
@@ -0,0 +1,174 @@
+/*
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "oc_api.h"
+#include "port/oc_clock.h"
+#include <signal.h>
+#include <windows.h>
+
+int quit = 0;
+
+static CONDITION_VARIABLE cv;
+static CRITICAL_SECTION cs;
+
+static bool state = false;
+int power;
+oc_string_t name;
+
+static int
+app_init(void)
+{
+  int ret = oc_init_platform("Intel", NULL, NULL);
+  ret |= oc_add_device("/oic/d", "oic.d.light", "Lamp", "ocf.1.0.0",
+                       "ocf.res.1.0.0", NULL, NULL);
+  oc_new_string(&name, "John's Light", 12);
+  return ret;
+}
+
+static void
+get_light(oc_request_t *request, oc_interface_mask_t interfaces,
+          void *user_data)
+{
+  (void)user_data;
+  ++power;
+
+  PRINT("GET_light:\n");
+  oc_rep_start_root_object();
+  switch (interfaces) {
+  case OC_IF_BASELINE:
+    oc_process_baseline_interface(request->resource);
+  case OC_IF_RW:
+    oc_rep_set_boolean(root, state, state);
+    oc_rep_set_int(root, power, power);
+    oc_rep_set_text_string(root, name, oc_string(name));
+    break;
+  default:
+    break;
+  }
+  oc_rep_end_root_object();
+  oc_send_response(request, OC_STATUS_OK);
+}
+
+static void
+post_light(oc_request_t *request, oc_interface_mask_t interfaces,
+           void *user_data)
+{
+  (void)interfaces;
+  (void)user_data;
+  PRINT("POST_light:\n");
+  oc_rep_t *rep = request->request_payload;
+  while (rep != NULL) {
+    PRINT("key: %s ", oc_string(rep->name));
+    switch (rep->type) {
+    case OC_REP_BOOL:
+      state = rep->value.boolean;
+      PRINT("value: %d\n", state);
+      break;
+    case OC_REP_INT:
+      power = rep->value.integer;
+      PRINT("value: %d\n", power);
+      break;
+    case OC_REP_STRING:
+      oc_free_string(&name);
+      oc_new_string(&name, oc_string(rep->value.string),
+                    oc_string_len(rep->value.string));
+      break;
+    default:
+      oc_send_response(request, OC_STATUS_BAD_REQUEST);
+      return;
+      break;
+    }
+    rep = rep->next;
+  }
+  oc_send_response(request, OC_STATUS_CHANGED);
+}
+
+static void
+put_light(oc_request_t *request, oc_interface_mask_t interfaces,
+          void *user_data)
+{
+  (void)interfaces;
+  (void)user_data;
+  post_light(request, interfaces, user_data);
+}
+
+static void
+register_resources(void)
+{
+  oc_resource_t *res = oc_new_resource(NULL, "/a/light", 2, 0);
+  oc_resource_bind_resource_type(res, "core.light");
+  oc_resource_bind_resource_type(res, "core.brightlight");
+  oc_resource_bind_resource_interface(res, OC_IF_RW);
+  oc_resource_set_default_interface(res, OC_IF_RW);
+  oc_resource_set_discoverable(res, true);
+  oc_resource_set_periodic_observable(res, 1);
+  oc_resource_set_request_handler(res, OC_GET, get_light, NULL);
+  oc_resource_set_request_handler(res, OC_PUT, put_light, NULL);
+  oc_resource_set_request_handler(res, OC_POST, post_light, NULL);
+  oc_add_resource(res);
+}
+
+static void
+signal_event_loop(void)
+{
+  WakeConditionVariable(&cv);
+}
+
+void
+handle_signal(int signal)
+{
+  signal_event_loop();
+  quit = 1;
+}
+
+int
+main(void)
+{
+  InitializeCriticalSection(&cs);
+  InitializeConditionVariable(&cv);
+
+  int init;
+
+  signal(SIGINT, handle_signal);
+
+  static const oc_handler_t handler = {.init = app_init,
+                                       .signal_event_loop = signal_event_loop,
+                                       .register_resources = register_resources,
+                                       .requests_entry = 0 };
+
+  oc_clock_time_t next_event;
+
+#ifdef OC_SECURITY
+  oc_storage_config("./simpleserver_creds/");
+#endif /* OC_SECURITY */
+
+  init = oc_main_init(&handler);
+  if (init < 0)
+    return init;
+
+  while (quit != 1) {
+    next_event = oc_main_poll();
+    if (next_event == 0) {
+      SleepConditionVariableCS(&cv, &cs, INFINITE);
+    } else {
+      SleepConditionVariableCS(&cv, &cs,
+                               (DWORD)(next_event / (1000 * OC_CLOCK_SECOND)));
+    }
+  }
+
+  oc_main_shutdown();
+  return 0;
+}
