3dcb05a6bfb17d40981a4faaaec476786d89d331
Andreas Zisowsky <zisowsky@lynxtechnology.com>
2018-03-06 15:20:09 +0100

    Add automatic patching of mbedTLS on Windows.
    - Added batch file to patch mbedTLS using git.
    - Added invocation as build step to Windows solution.
    
    Change-Id: I78bddd03ad99cb1798b55c13770ce4861e763c6d
    Signed-off-by: Andreas Zisowsky <zisowsky@lynxtechnology.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/24323
    Reviewed-by: Kishen Maloor <kishen.maloor@intel.com>
    Tested-by: Kishen Maloor <kishen.maloor@intel.com>

---
+++ port/windows/patch-mbedtls.bat
@@ -0,0 +1,32 @@
+@rem This batch file uses Git to apply the mbedTLS patches
+@rem on Windows. Please adapt GIT_CMD below as required.
+@rem Also adapt PATCH_CMD with the location of the patch
+@rem utility, if automatic resolution does not work.
+
+@rem If git.exe is not in PATH, then set the full path.
+set GIT_CMD=git
+
+@rem Get location of Git. ~p drops the executable name and keeps
+@rem the path only
+for /f "tokens=*" %%i in ('where %GIT_CMD%') do set GIT_LOCATION=%%~pi
+set PATCH_CMD=%GIT_LOCATION%..\usr\bin\patch.exe
+
+cd %~dp0..\..\deps\mbedtls || goto error
+"%GIT_CMD%" clean -xdf .  || goto error
+"%GIT_CMD%" reset --hard  || goto error
+@rem git apply cannot deal with "/dev/null" as input to create a new
+@rem file. Hence, we are using its patch utility directly.
+@rem -r -: do not create reject files if applying hunks fail
+@rem -s  : work silently
+@rem -N  : do not reverse the patch if already applied
+@rem -p1 : remove first path component from paths in the patches
+"%PATCH_CMD%" -r - -s -N -p1 < ..\..\patches\mbedtls_ocf_patch_1 || goto error
+"%PATCH_CMD%" -r - -s -N -p1 < ..\..\patches\mbedtls_iotivity_constrained_patch_2 || goto error
+@rem VS project can check existence of the file whether to invoke
+@rem this batch file or not.
+echo patches applied > patched.txt
+exit /b 0
+
+:error
+exit /b 1
+
