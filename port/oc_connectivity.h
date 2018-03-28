a8a5b2101cb127ef2cbf7bff281cac17a912ce04
Jaehong Jo <jaehong.jo@samsung.com>
2018-03-28 09:52:56 +0900

    Add TCP scheme(coap+tcp) for endpoint
    Change-Id: I5e2dd9d7fbc836a2679038d818eb3b81004d51c3
    Signed-off-by: Jaehong Jo <jaehong.jo@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/24497
    Reviewed-by: Kishen Maloor <kishen.maloor@intel.com>
    Tested-by: Kishen Maloor <kishen.maloor@intel.com>

--- port/oc_connectivity.h
+++ port/oc_connectivity.h
@@ -97,8 +97,9 @@ typedef struct oc_endpoint_t
     SECURED = 1 << 1,
     IPV4 = 1 << 2,
     IPV6 = 1 << 3,
-    GATT = 1 << 4,
-    MULTICAST = 1 << 5
+    TCP = 1 << 4,
+    GATT = 1 << 5,
+    MULTICAST = 1 << 6
   } flags;
 
   union dev_addr
