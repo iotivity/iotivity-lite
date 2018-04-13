3126a8748697a1b1e596533e684a724807a4beea
Pooja K <pooja.k@partner.samsung.com>
2018-04-13 15:06:54 +0530

    Adding Easy Setup APIs
    Added easy setup API header files and makefile for linux.
    
    [LITE-3] https://jira.iotivity.org/browse/LITE-3
    
    Change-Id: I0221be347a2142f5348358b43399cd00de50964d
    Signed-off-by: pooja <pooja.k@partner.samsung.com>

---
+++ service/easy-setup/enrollee/include/easysetup.h
@@ -0,0 +1,92 @@
+/* ****************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+#ifndef EASYSETUP_ENROLLEE_H
+#define EASYSETUP_ENROLLEE_H
+
+#include "estypes.h"
+#include "esresources.h"
+#include "enrolleecommon.h"
+
+/**
+ * @file
+ *
+ * This file contains Enrollee APIs
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/**
+ * This function initializes the EasySetup. This API must be called prior to invoking any other API.
+ *
+ * @param is_secured        True if the Enrollee is operating in secured mode.
+ * @param resource_mask     Easy Setup Resource Type which application wants to create.
+ *                          ES_WIFICONF_RESOURCE = 0x01,
+ *                          ES_COAPCLOUDCONF_RESOURCE = 0x02,
+ *                          ES_DEVCONF_RESOURCE = 0x04
+ * @param callbacks         ESProvisioningCallbacks for updating Easy setup Resources' data to the application
+ * @return ::ES_OK on success, some other value upon failure.
+ */
+es_result_e es_init_enrollee (bool is_secured, es_resource_mask_e resource_mask, es_provisioning_callbacks_s callbacks);
+
+/**
+ * This function sets Device information.
+ *
+ * @param es_device_property Contains device information composed of WiFiConf Structure & DevConf Structure
+ * @return ::ES_OK on success, some other value upon failure.
+ *
+ * @see es_device_property
+ */
+es_result_e es_set_device_property (es_device_property *device_property);
+
+/**
+ * This function sets Enrollee's State.
+ *
+ * @param es_enrollee_state   Contains current enrollee's state.
+ * @return ::ES_OK on success, some other value upon failure.
+ *
+ * @see es_enrollee_state
+ */
+es_result_e es_set_state (es_enrollee_state es_state);
+
+/**
+ * This function Sets Enrollee's Error Code.
+ *
+ * @param esErrCode   Contains Enrollee's error code.
+ * @return ::ES_OK on success, some other value upon failure.
+ *
+ * @see es_error_code_e
+ */
+es_result_e es_set_error_code (es_error_code es_err_code);
+
+/**
+ * This function performs termination of all Easy Setup Resources.
+ *
+ * @return ::ES_OK on success, some other value upon failure.
+ */
+es_result_e es_terminate_enrollee (void);
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+
+#endif /* EASYSETUP_ENROLLEE_H */
---
+++ service/easy-setup/enrollee/include/enrolleecommon.h
@@ -0,0 +1,118 @@
+/* ****************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+#ifndef ES_ENROLLEE_COMMON_H_
+#define ES_ENROLLEE_COMMON_H_
+
+#include "stdint.h"
+#include "estypes.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/**
+ * @brief Data structure for connect request from Mediator
+ */
+typedef struct
+{
+    es_connect_type connect[NUM_CONNECT_TYPE];
+    int num_request;
+} es_connect_request;
+
+/**
+ * @brief Data structure delivered from mediator, which provides WiFi information
+ */
+typedef struct
+{
+    char ssid[OIC_STRING_MAX_VALUE];        /**< Ssid of the Enroller**/
+    char pwd[OIC_STRING_MAX_VALUE];         /**< Pwd of the Enroller**/
+    wifi_authtype authtype;                 /**< Auth type of the Enroller**/
+    wifi_enctype enctype;                   /**< Encryption type of the Enroller**/
+    void *userdata;                         /**< Vender-specific data**/
+} es_wifi_conf_data;
+
+/**
+ * @brief Data structure delivered from mediator, which provides device configuration information
+ */
+typedef struct
+{
+    // TODO: variables can be added when some properties in oic.r.devconf resource are specified.
+    void *userdata;                         /**< Vender-specific data**/
+} es_dev_conf_data;
+
+/**
+ * @brief Data structure delivered from mediator, which provides Cloud server information
+ */
+typedef struct
+{
+    char accessToken[OIC_STRING_MAX_VALUE];     /**< Access token resolved with an auth code **/
+    oauth_tokentype access_token_type;            /**< Access token type **/
+    char auth_provider[OIC_STRING_MAX_VALUE];    /**< Auth provider ID **/
+    char ci_server[OIC_URI_STRING_MAX_VALUE];    /**< Cloud interface server URL which an Enrollee is going to registered **/
+    void *userdata;                             /**< Vender-specific data**/
+} es_coap_cloud_conf_data;
+
+/**
+ * @brief Data structure stored for Device property which includes a WiFi and device configuration.
+ */
+typedef struct
+{
+    /**
+     * @brief Data structure indicating WiFi configuration of Enrollee
+     */
+    struct
+    {
+        wifi_mode supported_mode[NUM_WIFIMODE];
+        uint8_t num_supported_mode;
+        wifi_freq supported_freq[NUM_WIFIFREQ];
+        uint8_t num_supported_freq;
+        wifi_authtype supported_auth_type[NUM_WIFIAUTHTYPE];
+        uint8_t num_supported_auth_type;
+        wifi_enctype supported_enc_type[NUM_WIFIENCTYPE];
+        uint8_t num_supported_enc_type;
+    } WiFi;
+
+    /**
+     * @brief Data structure indicating device configuration of Enrollee
+     */
+    struct
+    {
+        char device_name[OIC_STRING_MAX_VALUE];
+    } DevConf;
+} es_device_property;
+
+/**
+ * A set of functions pointers for callback functions which are called after provisioning data is
+ * received from Mediator.
+ */
+typedef struct {
+    void (*connect_request_cb)(es_connect_request *);
+    void (*wifi_prov_cb)(es_wifi_conf_data *);
+    void (*dev_conf_prov_cb)(es_dev_conf_data *);
+    void (*cloud_data_prov_cb)(es_coap_cloud_conf_data *);
+} es_provisioning_callbacks_s;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //ES_ENROLLEE_COMMON_H_
+
