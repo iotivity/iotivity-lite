c939394f30043d3e3722ef21e30917e64e59acc0
Philippe Coval <philippe.coval@osg.samsung.com>
2018-04-19 23:12:44 +0200

    WIP: tizenrt: Add minimal app for TizenRT
    Based in linux app, this can be imported to:
    TizenRT/apps/example/iotivity_constrainted_example
    
    Bug: https://jira.iotivity.org/browse/LITE-4
    Change-Id: Ia45abb8ce2e8da03f18a01694a22919d2d9bf3a7
    Origin: https://github.com/TizenTeam/iotivity-constrained/tree/sandbox/rzr/tizenrt/master
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ tests/port/tizenrt/examples/iotivity_constrained_example/main.c
@@ -0,0 +1,170 @@
+/*
+// Copyright 2018 Samsung Electronics France All Rights Reserved.
+// Copyright (c) 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+#include <tinyara/config.h>
+
+#include "oc_api.h"
+#include "port/oc_clock.h"
+
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+
+static pthread_mutex_t mutex;
+static pthread_cond_t cv;
+static struct timespec ts;
+static int quit = 0;
+static bool light_state = false;
+
+static void
+set_device_custom_property(void *data)
+{
+  (void)data;
+  oc_set_custom_device_property(purpose, "desk lamp");
+}
+
+static int
+app_init(void)
+{
+  int ret = oc_init_platform("Intel", NULL, NULL);
+  ret |= oc_add_device("/oic/d", "oic.d.light", "Kishen's light", "ocf.1.0.0",
+                       "ocf.res.1.0.0", set_device_custom_property, NULL);
+  return ret;
+}
+
+static void
+get_light(oc_request_t *request, oc_interface_mask_t interface, void *user_data)
+{
+  (void)user_data;
+  PRINT("GET_light:\n");
+  oc_rep_start_root_object();
+  switch (interface) {
+  case OC_IF_BASELINE:
+    oc_process_baseline_interface(request->resource);
+  /* fall through */
+  case OC_IF_RW:
+    oc_rep_set_boolean(root, state, light_state);
+    break;
+  default:
+    break;
+  }
+  oc_rep_end_root_object();
+  oc_send_response(request, OC_STATUS_OK);
+  PRINT("Light state %d\n", light_state);
+}
+
+static void
+post_light(oc_request_t *request, oc_interface_mask_t interface, void *user_data)
+{
+  (void)user_data;
+  (void)interface;
+  PRINT("POST_light:\n");
+  bool state = false;
+  oc_rep_t *rep = request->request_payload;
+  while (rep != NULL) {
+    PRINT("key: %s ", oc_string(rep->name));
+    switch (rep->type) {
+    case OC_REP_BOOL:
+      state = rep->value.boolean;
+      PRINT("value: %d\n", state);
+      break;
+    default:
+      oc_send_response(request, OC_STATUS_BAD_REQUEST);
+      return;
+      break;
+    }
+    rep = rep->next;
+  }
+  oc_send_response(request, OC_STATUS_CHANGED);
+  light_state = state;
+}
+
+static void
+put_light(oc_request_t *request, oc_interface_mask_t interface,
+           void *user_data)
+{
+  post_light(request, interface, user_data);
+}
+
+static void
+register_resources(void)
+{
+  oc_resource_t *res = oc_new_resource("lightbulb", "/light/1", 1, 0);
+  oc_resource_bind_resource_type(res, "oic.r.light");
+  oc_resource_bind_resource_interface(res, OC_IF_RW);
+  oc_resource_set_default_interface(res, OC_IF_RW);
+  oc_resource_set_discoverable(res, true);
+  oc_resource_set_periodic_observable(res, 1);
+  oc_resource_set_request_handler(res, OC_GET, get_light, NULL);
+  oc_resource_set_request_handler(res, OC_POST, post_light, NULL);
+  oc_resource_set_request_handler(res, OC_PUT, put_light, NULL);
+  oc_add_resource(res);
+}
+
+static void
+signal_event_loop(void)
+{
+  pthread_mutex_lock(&mutex);
+  pthread_cond_signal(&cv);
+  pthread_mutex_unlock(&mutex);
+}
+
+static void
+handle_signal(int signal)
+{
+  (void)signal;
+  signal_event_loop();
+  quit = 1;
+}
+
+#ifdef CONFIG_BUILD_KERNEL
+int main(int argc, FAR char *argv[])
+#else
+int iotivity_constrained_example_main(int argc, char *argv[])
+#endif
+
+{
+  int init;
+  static const oc_handler_t handler = {.init = app_init,
+                                       .signal_event_loop = signal_event_loop,
+                                       //TODO: tizenrt skiped
+                                       };
+  oc_clock_time_t next_event;
+  
+#ifdef OC_SECURITY
+  oc_storage_config("./server_creds");
+#endif /* OC_SECURITY */
+
+  init = oc_main_init(&handler);
+  if (init < 0)
+    return init;
+
+  while (quit != 1) {
+    next_event = oc_main_poll();
+    pthread_mutex_lock(&mutex);
+    if (next_event == 0) {
+      pthread_cond_wait(&cv, &mutex);
+    } else {
+      ts.tv_sec = (next_event / OC_CLOCK_SECOND);
+      ts.tv_nsec = (next_event % OC_CLOCK_SECOND) * 1.e09 / OC_CLOCK_SECOND;
+      pthread_cond_timedwait(&cv, &mutex, &ts);
+    }
+    pthread_mutex_unlock(&mutex);
+  }
+
+  oc_main_shutdown();
+  return 0;
+}
