491d5bd43468514e402031eda154a2cbe3557778
Jaehong Jo <jaehong.jo@samsung.com>
2018-04-23 13:34:18 +0900

    Adding Cloud-Access and RD-Client APIs
    Jira:
    https://jira.iotivity.org/browse/LITE-5
    https://jira.iotivity.org/browse/LITE-6
    
    Change-Id: Ia834726de7740240b7e3e78fd72bd06734bec6f9
    Signed-off-by: Jaehong Jo <jaehong.jo@samsung.com>

---
+++ service/resource-directory/client/include/rd_client.h
@@ -0,0 +1,75 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#ifndef RD_CLIENT_H
+#define RD_CLIENT_H
+
+/**
+  @brief Resource Directory API of IoTivity-constrained for client.
+  @file
+*/
+
+#include "oc_client_state.h"
+#include "oc_ri.h"
+
+/** Resource Directory URI used to Discover RD and Publish resources.*/
+#define OC_RSRVD_RD_URI "/oic/rd"
+
+/** To represent resource type with Publish RD.*/
+#define OC_RSRVD_RESOURCE_TYPE_RDPUBLISH "oic.wk.rdpub"
+
+/** To indicate how long RD should publish this item.*/
+#define OC_RSRVD_DEVICE_TTL "lt"
+
+#define OIC_RD_PUBLISH_TTL 86400
+
+/**
+  @brief Publish RD resource to Resource Directory.
+  @param endpoint The endpoint of the RD.
+  @param resource This is the resource which we need to register to RD.
+  @param handler To refer to the request sent out on behalf of calling this API.
+  @param user_data The user data passed from the registration function.
+  @return Returns true if success.
+*/
+bool rd_publish(oc_endpoint_t *endpoint, oc_resource_t *resource,
+                oc_response_handler_t handler, void *user_data);
+
+/**
+  @brief Publish RD resource to Resource Directory.
+  @param endpoint The endpoint of the RD.
+  @param id An unique identifier of publishing device.
+  @param resource This is the resource which we need to register to RD.
+  @param handler To refer to the request sent out on behalf of calling this API.
+  @param user_data The user data passed from the registration function.
+  @return Returns true if success.
+*/
+bool rd_publish_with_device_id(oc_endpoint_t *endpoint, const char *id,
+                               oc_resource_t *resource,
+                               oc_response_handler_t handler, void *user_data);
+
+/**
+  @brief Delete RD resource from Resource Directory.
+  @param endpoint The endpoint of the RD.
+  @param handler To refer to the request sent out on behalf of calling this API.
+  @param user_data The user data passed from the registration function.
+  @return Returns true if success.
+*/
+bool rd_delete(oc_endpoint_t *endpoint, oc_response_handler_t handler,
+               void *user_data);
+
+#endif /* RD_CLIENT_H */
---
+++ service/resource-directory/client/src/rd_client.c
@@ -0,0 +1,95 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#include "rd_client.h"
+#include "oc_api.h"
+#include "oc_core_res.h"
+#include "oc_log.h"
+
+bool
+rd_publish(oc_endpoint_t *endpoint, oc_resource_t *resource,
+           oc_response_handler_t handler, void *user_data)
+{
+  if (!endpoint) {
+    OC_ERR("Error of input parameters");
+    return false;
+  }
+
+  char id[37];
+  oc_uuid_to_str(oc_core_get_device_id(0), id, 37);
+
+  return rd_publish_with_device_id(endpoint, id, resource, handler, user_data);
+}
+
+bool
+rd_publish_with_device_id(oc_endpoint_t *endpoint, const char *id,
+                          oc_resource_t *resource,
+                          oc_response_handler_t handler, void *user_data)
+{
+  if (!endpoint || !id) {
+    OC_ERR("Error of input parameters");
+    return false;
+  }
+
+  char target_uri[23] = { 0 };
+  snprintf(target_uri, 23, "%s?rt=%s", OC_RSRVD_RD_URI,
+           OC_RSRVD_RESOURCE_TYPE_RDPUBLISH);
+
+  if (oc_init_post(target_uri, endpoint, NULL, handler, LOW_QOS, user_data)) {
+    oc_rep_start_root_object();
+    oc_rep_set_text_string(root, di, id);
+    oc_rep_set_int(root, OC_RSRVD_DEVICE_TTL, OIC_RD_PUBLISH_TTL);
+
+    oc_rep_set_array(root, links);
+    oc_rep_object_array_start_item(links);
+    oc_rep_set_text_string(links, href, oc_string(resource->uri));
+    oc_rep_set_string_array(links, rt, resource->types);
+    oc_core_encode_interfaces_mask(oc_rep_object(links), resource->interfaces);
+    oc_rep_object_array_end_item(links);
+    oc_rep_close_array(root, links);
+
+    oc_rep_end_root_object();
+  } else {
+    OC_ERR("Could not init POST request for rd publish");
+    return false;
+  }
+
+  return oc_do_post();
+}
+
+bool
+rd_delete(oc_endpoint_t *endpoint, oc_response_handler_t handler,
+          void *user_data)
+{
+  if (!endpoint) {
+    OC_ERR("Error of input parameters");
+    return false;
+  }
+
+  char uuid[37];
+  oc_uuid_to_str(oc_core_get_device_id(0), uuid, 37);
+
+  char target_uri[49] = { 0 };
+  snprintf(target_uri, 49, "%s?di=%s", OC_RSRVD_RD_URI, uuid);
+  if (!oc_do_delete(target_uri, endpoint, handler, LOW_QOS, user_data)) {
+    OC_ERR("Could not DELETE request for rd delete");
+    return false;
+  }
+
+  return true;
+}
