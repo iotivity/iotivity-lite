69f06bc91d3d1005f8cb2a4e97686c0dfecbbbd7
Jaehong Jo <jaehong.jo@samsung.com>
2018-04-23 13:34:18 +0900

    Adding Cloud-Access and RD-Client APIs
    Jira:
    https://jira.iotivity.org/browse/LITE-5
    https://jira.iotivity.org/browse/LITE-6
    
    Change-Id: Ia834726de7740240b7e3e78fd72bd06734bec6f9
    Signed-off-by: Jaehong Jo <jaehong.jo@samsung.com>

---
+++ service/cloud-access/src/cloud_access.c
@@ -0,0 +1,124 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#include "cloud_access.h"
+#include "oc_api.h"
+#include "oc_core_res.h"
+#include "oc_log.h"
+
+bool
+oc_sign_up(oc_endpoint_t *endpoint, const char *auth_provider,
+           const char *auth_code, oc_response_handler_t handler,
+           void *user_data)
+{
+  if (!endpoint || !auth_provider || !auth_code) {
+    OC_ERR("Error of input parameters");
+    return false;
+  }
+
+  if (oc_init_post(OC_RSRVD_ACCOUNT_URI, endpoint, NULL, handler, LOW_QOS,
+                   user_data)) {
+    char uuid[37];
+    oc_uuid_to_str(oc_core_get_device_id(0), uuid, 37);
+
+    oc_rep_start_root_object();
+    oc_rep_set_text_string(root, di, uuid);
+    oc_rep_set_text_string(root, authprovider, auth_provider);
+    oc_rep_set_text_string(root, authcode, auth_code);
+    oc_rep_set_text_string(root, devicetype, "device");
+    oc_rep_end_root_object();
+  } else {
+    OC_ERR("Could not init POST request for sign up");
+    return false;
+  }
+
+  return oc_do_post();
+}
+
+static bool
+oc_sign_inout(oc_endpoint_t *endpoint, const char *uid,
+              const char *access_token, bool is_sign_in,
+              oc_response_handler_t handler, void *user_data)
+{
+  if (!endpoint || (is_sign_in && !uid) || !access_token) {
+    OC_ERR("Error of input parameters");
+    return false;
+  }
+
+  if (oc_init_post(OC_RSRVD_ACCOUNT_SESSION_URI, endpoint, NULL, handler,
+                   LOW_QOS, user_data)) {
+    char uuid[37];
+    oc_uuid_to_str(oc_core_get_device_id(0), uuid, 37);
+
+    oc_rep_start_root_object();
+    if (is_sign_in)
+      oc_rep_set_text_string(root, uid, uid);
+    oc_rep_set_text_string(root, di, uuid);
+    oc_rep_set_text_string(root, accesstoken, access_token);
+    oc_rep_set_boolean(root, login, is_sign_in);
+    oc_rep_end_root_object();
+  } else {
+    OC_ERR("Could not init POST request for sign in/out");
+    return false;
+  }
+
+  return oc_do_post();
+}
+
+bool
+oc_sign_in(oc_endpoint_t *endpoint, const char *uid, const char *access_token,
+           oc_response_handler_t handler, void *user_data)
+{
+  return oc_sign_inout(endpoint, uid, access_token, true, handler, user_data);
+}
+
+bool
+oc_sign_out(oc_endpoint_t *endpoint, const char *access_token,
+            oc_response_handler_t handler, void *user_data)
+{
+  return oc_sign_inout(endpoint, NULL, access_token, false, handler, user_data);
+}
+
+bool
+oc_refresh_access_token(oc_endpoint_t *endpoint, const char *uid,
+                        const char *refresh_token,
+                        oc_response_handler_t handler, void *user_data)
+{
+  if (!endpoint || !uid || !refresh_token) {
+    OC_ERR("Error of input parameters");
+    return false;
+  }
+
+  if (oc_init_post(OC_RSRVD_ACCOUNT_TOKEN_REFRESH_URI, endpoint, NULL, handler,
+                   LOW_QOS, user_data)) {
+    char uuid[37];
+    oc_uuid_to_str(oc_core_get_device_id(0), uuid, 37);
+
+    oc_rep_start_root_object();
+    oc_rep_set_text_string(root, uid, uid);
+    oc_rep_set_text_string(root, di, uuid);
+    oc_rep_set_text_string(root, granttype, OC_RSRVD_GRANT_TYPE_REFRESH_TOKEN);
+    oc_rep_set_text_string(root, refreshtoken, refresh_token);
+    oc_rep_end_root_object();
+  } else {
+    OC_ERR("Could not init POST request for refresh access token");
+    return false;
+  }
+
+  return oc_do_post();
+}
---
+++ service/resource-directory/client/src/rd_client.c
@@ -0,0 +1,101 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#include "rd_client.h"
+#include "oc_api.h"
+#include "oc_core_res.h"
+#include "oc_log.h"
+
+#define RD_PUBLISH_TTL 86400
+
+bool
+rd_publish(oc_endpoint_t *endpoint, oc_resource_t *resource,
+           oc_response_handler_t handler, void *user_data)
+{
+  char uuid[37];
+  oc_uuid_to_str(oc_core_get_device_id(0), uuid, 37);
+
+  return rd_publish_with_device_id(endpoint, uuid, resource, handler,
+                                   user_data);
+}
+
+bool
+rd_publish_with_device_id(oc_endpoint_t *endpoint, const char *id,
+                          oc_resource_t *resource,
+                          oc_response_handler_t handler, void *user_data)
+{
+  if (!endpoint || !id) {
+    OC_ERR("Error of input parameters");
+    return false;
+  }
+
+  if (oc_init_post(OC_RSRVD_RD_URI, endpoint, "rt=oic.wk.rdpub", handler,
+                   LOW_QOS, user_data)) {
+    oc_rep_start_root_object();
+    oc_rep_set_text_string(root, di, id);
+    oc_rep_set_int(root, lt, RD_PUBLISH_TTL);
+
+    oc_rep_set_array(root, links);
+    oc_rep_object_array_start_item(links);
+    oc_rep_set_text_string(links, href, oc_string(resource->uri));
+    oc_rep_set_string_array(links, rt, resource->types);
+    oc_core_encode_interfaces_mask(oc_rep_object(links), resource->interfaces);
+    oc_rep_set_int(links, ins, 0);
+    oc_rep_object_array_end_item(links);
+    oc_rep_close_array(root, links);
+
+    oc_rep_end_root_object();
+  } else {
+    OC_ERR("Could not init POST request for rd publish");
+    return false;
+  }
+
+  return oc_do_post();
+}
+
+bool
+rd_delete(oc_endpoint_t *endpoint, oc_response_handler_t handler,
+          void *user_data)
+{
+  char uuid[37];
+  oc_uuid_to_str(oc_core_get_device_id(0), uuid, 37);
+
+  return rd_delete_with_device_id(endpoint, uuid, handler, user_data);
+}
+
+bool
+rd_delete_with_device_id(oc_endpoint_t *endpoint, const char *id,
+                         oc_response_handler_t handler, void *user_data)
+{
+  if (!endpoint || !id) {
+    OC_ERR("Error of input parameters");
+    return false;
+  }
+
+  oc_string_t query;
+  oc_concat_strings(&query, "di=", id);
+
+  bool status = oc_do_delete(OC_RSRVD_RD_URI, endpoint, oc_string(query),
+                             handler, LOW_QOS, user_data);
+  if (!status) {
+    OC_ERR("Could not DELETE request for rd delete");
+  }
+  oc_free_string(&query);
+
+  return status;
+}
