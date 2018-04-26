23f9ebfc64812383ed2179052a3acb7582fd918c
Kishen Maloor <kishen.maloor@intel.com>
2018-04-26 13:24:31 -0700

    Handle session (dis)connect, close_session() API
    This patch adds a mechanism for cleaning up any
    internal session state upon termination of TCP, TLS,
    or DTLS sessions. Two new APIs: oc_session_start_event()
    and oc_session_end_event() may be invoked from the
    (separate) network thread in the connectivity implementation,
    upon initiating new sessions or detecting a session close
    via the network stack. This applies only to TCP and TLS
    sessions.
    This will not apply to DTLS sessions which receive no such
    indication via the network stack, but internal state
    is nonetheless cleared upon receipt of an explicit
    DTLS CLOSE_NOTIFY message.
    
    In addition, applications will now have the ability to
    (optionally) set a callback to be invoked upon session
    establishment and disconnections, supplying the specific
    oc_endpoint_t of the remote device along with an
    OC_SESSION_CONNECTED/OC_SESSION_DISCONNECTED status. This
    applies to DTLS, TLS and plain TCP sessions.
    
    Lastly, clients will have an oc_close_session() API they can
    call to tear down any open DTLS, TLS and TCP sessions.
    
    Change-Id: Icee7f3d6006d779b173ffaa9dd508dbbbdc327d2
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/24943

---
+++ api/oc_session_events.c
@@ -0,0 +1,95 @@
+/*
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "oc_session_events.h"
+#include "oc_buffer.h"
+#include "oc_session_state.h"
+#include "oc_signal_event_loop.h"
+#include "util/oc_list.h"
+#ifdef OC_SECURITY
+#include "security/oc_dtls.h"
+#endif /* OC_SECURITY */
+#if defined(OC_SERVER)
+#include "messaging/coap/observe.h"
+#endif /* OC_SERVER */
+
+#ifdef OC_TCP
+OC_LIST(session_start_events);
+OC_LIST(session_end_events);
+
+static void oc_process_session_event(void) {
+  oc_network_event_handler_mutex_lock();
+  oc_endpoint_t *head = (oc_endpoint_t *)oc_list_pop(session_start_events);
+  while (head != NULL) {
+    oc_handle_session(head, OC_SESSION_CONNECTED);
+    oc_free_endpoint(head);
+    head = oc_list_pop(session_start_events);
+  }
+  head = (oc_endpoint_t *)oc_list_pop(session_end_events);
+  while (head != NULL) {
+    oc_handle_session(head, OC_SESSION_DISCONNECTED);
+    oc_free_endpoint(head);
+    head = oc_list_pop(session_end_events);
+  }
+  oc_network_event_handler_mutex_unlock();
+}
+
+OC_PROCESS(oc_session_events, "");
+OC_PROCESS_THREAD(oc_session_events, ev, data) {
+  (void)data;
+  OC_PROCESS_POLLHANDLER(oc_process_session_event());
+  OC_PROCESS_BEGIN();
+  while (oc_process_is_running(&(oc_session_events))) {
+    OC_PROCESS_YIELD();
+  }
+  OC_PROCESS_END();
+}
+
+void oc_session_start_event(oc_endpoint_t *endpoint) {
+  oc_endpoint_t *ep = oc_new_endpoint();
+  memcpy(ep, endpoint, sizeof(oc_endpoint_t));
+  ep->next = NULL;
+  oc_network_event_handler_mutex_lock();
+  oc_list_add(session_start_events, ep);
+  oc_network_event_handler_mutex_unlock();
+
+  oc_process_poll(&(oc_session_events));
+  _oc_signal_event_loop();
+}
+
+void oc_session_end_event(oc_endpoint_t *endpoint) {
+  oc_endpoint_t *ep = oc_new_endpoint();
+  memcpy(ep, endpoint, sizeof(oc_endpoint_t));
+  ep->next = NULL;
+  oc_network_event_handler_mutex_lock();
+  oc_list_add(session_end_events, ep);
+  oc_network_event_handler_mutex_unlock();
+
+  oc_process_poll(&(oc_session_events));
+  _oc_signal_event_loop();
+}
+#endif /* OC_TCP */
+
+void oc_handle_session(oc_endpoint_t *endpoint, oc_session_state_t state) {
+  if (state == OC_SESSION_DISCONNECTED) {
+#ifdef OC_SERVER
+    coap_remove_observer_by_client(endpoint);
+#else  /* OC_SERVER */
+    (void)endpoint;
+#endif /* !OC_SERVER */
+  }
+  _oc_session_state(endpoint, state);
+}
