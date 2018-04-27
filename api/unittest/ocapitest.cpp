e266ab625775f262b0d449e94bd8bf6ebebb031f
Mushfiqul Islam Antu <i.mushfiq@samsung.com>
2018-04-27 10:35:15 +0600

    Add UTC with callback checking
    UTC added to verify discovery callback
    
    Change-Id: I62ce3acc8375b055f51dbb48201141e695acd273
    Signed-off-by: Mushfiqul Islam Antu <i.mushfiq@samsung.com>

--- api/unittest/ocapitest.cpp
+++ api/unittest/ocapitest.cpp
@@ -19,54 +19,240 @@
  ******************************************************************/
 
 #include <cstdlib>
+#include <string>
 #include <gtest/gtest.h>
 
 extern "C" {
     #include "oc_api.h"
+    #include "port/oc_clock.h"
 }
 
-#define UUID "12345678123412341234123456789012"
+#define MAX_WAIT_TIME 10
+#define RESOURCE_URI "/LightResourceURI"
+#define DEVICE_URI "/oic/d"
+#define RESOURCE_TYPE "oic.r.light"
+#define DEVICE_TYPE "oic.d.light"
+#define RESOURCE_INTERFACE "oic.if.baseline"
+#define MANUFACTURER_NAME "Samsung"
+#define DEVICE_NAME "Table Lamp"
+#define OCF_SPEC_VERSION "ocf.1.0.0"
+#define OCF_DATA_MODEL_VERSION "ocf.res.1.0.0"
 
-
-static int app_init(void)
+class TestServerClient: public testing::Test
 {
-  return 0;
-}
+protected:
+    virtual void SetUp()
+    {
 
-static void register_resources(void)
+    }
+
+    virtual void TearDown()
+    {
+
+    }
+};
+
+class ApiHelper
 {
-}
+private:
+    static oc_handler_t s_handler;
+    static bool s_isServerStarted;
+    static bool s_isCallbackReceived;
+    static oc_resource_t *s_pResource;
+    static pthread_mutex_t s_mutex;
+    static pthread_cond_t s_cv;
 
-static void signal_event_loop(void)
+public:
+    static int appInit(void)
+    {
+        int result = oc_init_platform(MANUFACTURER_NAME, NULL, NULL);
+        result |= oc_add_device(DEVICE_URI, DEVICE_TYPE, DEVICE_NAME,
+                         OCF_SPEC_VERSION, OCF_DATA_MODEL_VERSION, NULL, NULL);
+        return result;
+    }
+
+    static void registerResources(void)
+    {
+        s_pResource = oc_new_resource(NULL, RESOURCE_URI, 1, 0);
+        oc_resource_bind_resource_type(s_pResource, RESOURCE_TYPE);
+        oc_resource_bind_resource_interface(s_pResource, OC_IF_BASELINE);
+        oc_resource_set_default_interface(s_pResource, OC_IF_BASELINE);
+        oc_resource_set_discoverable(s_pResource, true);
+        oc_resource_set_periodic_observable(s_pResource, 1);
+        oc_resource_set_request_handler(s_pResource, OC_GET, onGet, NULL);
+        oc_process_baseline_interface(s_pResource);
+        oc_add_resource(s_pResource);
+    }
+
+    static void signalEventLoop(void)
+    {
+        pthread_mutex_lock(&s_mutex);
+        pthread_cond_signal(&s_cv);
+        pthread_mutex_unlock(&s_mutex);
+    }
+
+    static void requestsEntry(void)
+    {
+    }
+
+    static oc_discovery_flags_t onResourceDiscovered(const char *di, const char *uri,
+        oc_string_array_t types, oc_interface_mask_t interfaces,
+        oc_endpoint_t *endpoint, oc_resource_properties_t bm, void *user_data)
+    {
+        std::string discoveredResourceUri = std::string(uri);
+        if (discoveredResourceUri.compare(RESOURCE_URI) == 0)
+        {
+            PRINT("Light Resource Discovered....\n");
+            s_isCallbackReceived = true;
+            return OC_STOP_DISCOVERY;        
+        }
+        return OC_CONTINUE_DISCOVERY;
+    }
+
+    static void onGet(const char *di, const char *uri,
+        oc_string_array_t types, oc_interface_mask_t interfaces,
+        oc_endpoint_t *endpoint, oc_resource_properties_t bm, void *user_data)
+    {
+        s_isCallbackReceived = true;
+    }
+
+    static bool startServer(std::string& errorMessage)
+    {
+        bool isPassed = true;
+        s_handler.init = appInit;
+        s_handler.signal_event_loop = signalEventLoop;
+        s_handler.register_resources = registerResources;
+        s_handler.requests_entry = requestsEntry;
+
+        oc_set_con_res_announced(false);
+
+        int initResult = oc_main_init(&s_handler);
+        if ( initResult < 0)
+        {
+            isPassed = false;
+            errorMessage += "Initialization of main server failed";
+            s_isServerStarted = false;
+        }
+        else
+        {
+            s_isServerStarted = true;
+        }
+
+        return isPassed;
+    }
+
+    static void stopServer()
+    {
+        if (s_isServerStarted)
+        {
+            oc_main_shutdown();
+        }
+
+    }
+    static bool discoverResource(std::string& errorMessage)
+    {
+        s_isCallbackReceived = false;
+        oc_do_ip_discovery(NULL, &onResourceDiscovered, NULL);
+        waitForEvent(MAX_WAIT_TIME);
+        if(!s_isCallbackReceived)
+        {
+            errorMessage += "Unable to discover Light Resource";
+        }
+        return s_isCallbackReceived;
+    }
+
+    static void waitForEvent(int waitTime)
+    {
+        oc_clock_time_t next_event;
+
+        while (waitTime && !s_isCallbackReceived)
+        {
+            PRINT("Waiting for callback....\n");
+            next_event = oc_main_poll();
+            sleep(1);
+            waitTime--;
+        }
+    }
+
+    static bool sendGetRequest(std::string& errorMessage)
+    {
+        bool isPassed = true;
+
+        // waitForEvent(MAX_WAIT_TIME);
+        return isPassed;
+    }
+
+    static bool unregisterReresource(std::string& errorMessage)
+    {
+        bool isPassed = true;
+        oc_delete_resource(s_pResource);
+        return isPassed;
+    }
+};
+
+bool ApiHelper::s_isServerStarted = false;
+bool ApiHelper::s_isCallbackReceived = false;
+oc_resource_t* ApiHelper::s_pResource = nullptr;
+oc_handler_t ApiHelper::s_handler;
+pthread_mutex_t ApiHelper::s_mutex;
+pthread_cond_t ApiHelper::s_cv;
+
+class TestUnicastRequest: public testing::Test
 {
+protected:
+    virtual void SetUp()
+    {
+        std::string msg = "";
+        ASSERT_TRUE(ApiHelper::startServer(msg)) << msg;
+        ASSERT_TRUE(ApiHelper::discoverResource(msg)) << msg;
+    }
+
+    virtual void TearDown()
+    {
+        std::string msg = "";
+        ApiHelper::unregisterReresource(msg);
+        ApiHelper::stopServer();
+    }
+};
+
+// TEST(TestServerClient, ServerStartTest_P) {
+
+//     static const oc_handler_t handler = {.init = ApiHelper::appInit,
+//                                        .signal_event_loop = ApiHelper::signalEventLoop,
+//                                        .register_resources = ApiHelper::registerResources,
+//                                        .requests_entry = ApiHelper::requestsEntry };
+
+//     int result = oc_main_init(&handler);
+//     EXPECT_GE(result,  0);
+
+//     oc_main_shutdown();
+// }
+
+// TEST(TestServerClient, ServerStopTest_P) {
+
+//     static const oc_handler_t handler = {.init = ApiHelper::appInit,
+//                                        .signal_event_loop = ApiHelper::signalEventLoop,
+//                                        .register_resources = ApiHelper::registerResources,
+//                                        .requests_entry = ApiHelper::requestsEntry };
+
+//     int result = oc_main_init(&handler);
+//     ASSERT_GE(result,  0);
+
+//     EXPECT_NO_THROW(oc_main_shutdown());
+// }
+
+TEST(TestUnicastRequest, SendGetRequest_P) {
+
+    std::string result = "";
+
+    EXPECT_TRUE(ApiHelper::sendGetRequest(result)) << result;    
 }
 
-static void requests_entry(void)
-{
-}
+TEST(TestUnicastRequest, SendGetRequestTwice_P) {
 
-TEST(TestServerClient, ServerStartTest_P) {
-
-    static const oc_handler_t handler = {.init = app_init,
-                                       .signal_event_loop = signal_event_loop,
-                                       .register_resources = register_resources,
-                                       .requests_entry = requests_entry };
-
-    int result = oc_main_init(&handler);
-    EXPECT_GE(result,  0);
-
-    oc_main_shutdown();
-}
-
-TEST(TestServerClient, ServerStopTest_P) {
-
-    static const oc_handler_t handler = {.init = app_init,
-                                       .signal_event_loop = signal_event_loop,
-                                       .register_resources = register_resources,
-                                       .requests_entry = requests_entry };
-
-    int result = oc_main_init(&handler);
-    ASSERT_GE(result,  0);
-
-    EXPECT_NO_THROW(oc_main_shutdown());
+    std::string result = "";
+    EXPECT_TRUE(ApiHelper::sendGetRequest(result)) << result;
+    
+    result = "";
+    EXPECT_TRUE(ApiHelper::sendGetRequest(result)) << result; 
 }
