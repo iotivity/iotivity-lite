b875b733983fdf56891c14920bfa5a8e5fc8a118
Jaehyun Cho <jaehyun3.cho@samsung.com>
2018-05-02 20:25:52 +0900

    Add Network interface event monitoring logics.
    IP interface list.
    - To manage IP network interface up/down events,
      available interfaces will be included in the list.
    Network interface event callback.
    - After netlink socket receive new or delete interface
      event, interface state changed callback is called if
      application set regarding callback.
    Callback List.
    - network interface & session event handler
      can be added multiple callback by using oc_list.
    Move endpoint structure location.
    - oc_session_event use oc_endpoint_t. And also
      oc_connectivity need oc_session_event header.
      To reduce circular dependency, oc_endpoint_t
      structure location is changed in oc_endpoint header.
    
    Change-Id: I2a58aa98613da8dac454304c52124393403f8a20
    Signed-off-by: Jaehyun Cho <jaehyun3.cho@samsung.com>

--- include/oc_api.h
+++ include/oc_api.h
@@ -33,7 +33,6 @@
 #include "oc_buffer_settings.h"
 #include "oc_rep.h"
 #include "oc_ri.h"
-#include "oc_session_state.h"
 #include "oc_signal_event_loop.h"
 #include "port/oc_storage.h"
 
@@ -48,8 +47,6 @@ typedef struct {
 #ifdef OC_CLIENT
   void (*requests_entry)(void);
 #endif /* OC_CLIENT */
-
-  void (*session_state)(oc_endpoint_t *, oc_session_state_t);
 } oc_handler_t;
 
 typedef void (*oc_init_platform_cb_t)(void *data);
