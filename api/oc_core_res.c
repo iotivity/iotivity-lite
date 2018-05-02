a044962229ba28487202d6bd6cd936e3e38da397
Andreas Zisowsky <zisowsky@lynxtechnology.com>
2018-05-02 17:59:05 +0200

    Merge master
    Merge master, adapt to lynx branch, fix Windows compilation.
    Contains the following commits from review:
    * 1b460d7 Fix Windows compile errors
    Contains the following commits from master:
    * 6d8c052 Free resources during shutdown sequence
    * 10aa52e Modify ip_context struct using MEMB.
    * d09f823 Restrict MBEDTLS_MAX_CONTENT_LEN in static build
    * bfc4a5c mbedTLS dynamic buffers
    * 44e20aa tizenrt: Port with stubbed noop functions
    * 443961f Setting tcp port in OIC 1.1 discovery response.
    * 96bef08 Add UTC with callback checking
    * 1ebbcd8 linux: Actually run tests on make test
    * 90fc165 process: Test preproc symbols not values
    * 1ccaebe Setting content/accept version from endpoint
    * d1a76df Linux Makefile: list "all" target before others
    * f1b7e69 Added query param at oc_do_delete
    * c57ed10 Move session_state cb to end of oc_handler_t
    * b785bce [IOT-3029] Add Sample Unit Test Case
    * abad369 Update Linux TCP adapter
    * 7721d9a Add TLS support to security layer
    * 23f9ebf Handle session (dis)connect, close_session() API
    * fbbb44f Update connectivity interface
    
    Change-Id: I2a1ca659fbe50d295c35603048b1a074c99432d6
    Signed-off-by: Andreas Zisowsky <zisowsky@lynxtechnology.com>

--- api/oc_core_res.c
+++ api/oc_core_res.c
@@ -23,8 +23,8 @@
 
 #ifdef OC_SECURITY
 #include "security/oc_doxm.h"
-#include "security/oc_dtls.h"
 #include "security/oc_pstat.h"
+#include "security/oc_tls.h"
 #endif /* OC_SECURITY */
 
 #include "port/oc_assert.h"
@@ -33,8 +33,8 @@
 #ifdef OC_DYNAMIC_ALLOCATION
 #include "oc_endpoint.h"
 #include <stdlib.h>
-static oc_resource_t *core_resources;
-static oc_device_info_t *oc_device_info;
+static oc_resource_t *core_resources = NULL;
+static oc_device_info_t *oc_device_info = NULL;
 #else  /* OC_DYNAMIC_ALLOCATION */
 static oc_resource_t core_resources[1 + OCF_D * OC_MAX_NUM_DEVICES];
 static oc_device_info_t oc_device_info[OC_MAX_NUM_DEVICES];
@@ -42,7 +42,7 @@ static oc_device_info_t oc_device_info[OC_MAX_NUM_DEVICES];
 static oc_platform_info_t oc_platform_info;
 
 static bool announce_con_res = true;
-static int device_count;
+static int device_count = 0;
 static oc_uuid_t *next_di;
 static oc_id_updated_t id_updated_callback;
 
@@ -54,22 +54,82 @@ static oc_id_updated_t id_updated_callback;
 /* Number of characters of OC_NAME_CON_RES */
 #define OC_NAMELEN_CON_RES 6
 
-#ifdef OC_DYNAMIC_ALLOCATION
 void
 oc_core_init(void)
 {
-  core_resources = (oc_resource_t *)calloc(OC_NUM_CORE_RESOURCES_PER_DEVICE,
-                                           sizeof(oc_resource_t));
+  oc_core_shutdown();
+
+#ifdef OC_DYNAMIC_ALLOCATION
+  core_resources = (oc_resource_t *)calloc(1, sizeof(oc_resource_t));
   if (!core_resources) {
     oc_abort("Insufficient memory");
   }
-  oc_device_info = (oc_device_info_t *)calloc(OC_NUM_CORE_RESOURCES_PER_DEVICE,
-                                              sizeof(oc_device_info_t));
-  if (!oc_device_info) {
-    oc_abort("Insufficient memory");
+
+  oc_device_info = NULL;
+#endif /* OC_DYNAMIC_ALLOCATION */
+}
+
+static void
+oc_core_free_resource_properties(oc_resource_t *core_resources_item)
+{
+  if (core_resources_item) {
+    if (oc_string_len(core_resources_item->name))
+      oc_free_string(&(core_resources_item->name));
+    if (oc_string_len(core_resources_item->uri))
+      oc_free_string(&(core_resources_item->uri));
+    if (oc_string_array_get_allocated_size(core_resources_item->types))
+      oc_free_string_array(&(core_resources_item->types));
   }
 }
+
+static void
+oc_core_free_device_info_properties(oc_device_info_t *oc_device_info_item)
+{
+
+  if (oc_device_info_item) {
+    if (oc_string_len(oc_device_info_item->name))
+      oc_free_string(&(oc_device_info_item->name));
+    if (oc_string_len(oc_device_info_item->icv))
+      oc_free_string(&(oc_device_info_item->icv));
+    if (oc_string_len(oc_device_info_item->dmv))
+      oc_free_string(&(oc_device_info_item->dmv));
+  }
+}
+
+void
+oc_core_shutdown(void)
+{
+  int i;
+  if (oc_string_len(oc_platform_info.mfg_name))
+    oc_free_string(&(oc_platform_info.mfg_name));
+
+#ifdef OC_DYNAMIC_ALLOCATION
+  if (oc_device_info) {
 #endif /* OC_DYNAMIC_ALLOCATION */
+    for (i = 0; i < device_count; ++i) {
+      oc_device_info_t *oc_device_info_item = &oc_device_info[i];
+      oc_core_free_device_info_properties(oc_device_info_item);
+    }
+#ifdef OC_DYNAMIC_ALLOCATION
+    free(oc_device_info);
+    oc_device_info = NULL;
+  }
+#endif /* OC_DYNAMIC_ALLOCATION */
+
+#ifdef OC_DYNAMIC_ALLOCATION
+  if (core_resources) {
+#endif /* OC_DYNAMIC_ALLOCATION */
+    for (i = 0; i < 1 + (OCF_D * device_count); ++i) {
+      oc_resource_t *core_resources_item = &core_resources[i];
+      oc_core_free_resource_properties(core_resources_item);
+    }
+#ifdef OC_DYNAMIC_ALLOCATION
+    free(core_resources);
+    core_resources = NULL;
+  }
+#endif /* OC_DYNAMIC_ALLOCATION */
+  device_count = 0;
+}
 
 void
 oc_core_encode_interfaces_mask(CborEncoder *parent,
@@ -117,8 +177,7 @@ oc_core_regen_unique_ids(int device)
     oc_uuid_to_str(&oc_platform_info.pi, pi, 37);
     id_updated_callback(device, di, piid, pi);
   }
-
-  oc_sec_dtls_update_psk_identity(device);
+  oc_tls_update_psk_identity(device);
 }
 #endif /* OC_SECURITY */
 
@@ -273,24 +332,23 @@ oc_core_add_new_device(const char *uri, const char *rt, const char *name,
   }
 #else  /* !OC_DYNAMIC_ALLOCATION */
   int new_num = 1 + OCF_D * (device_count + 1);
-  /* Initially we have already OC_NUM_CORE_RESOURCES_PER_DEVICE
-     allocated. See oc_core_init(). */
-  if (new_num > OC_NUM_CORE_RESOURCES_PER_DEVICE) {
-    core_resources =
-      (oc_resource_t *)realloc(core_resources, new_num * sizeof(oc_resource_t));
-    if (!core_resources) {
-      oc_abort("Insufficient memory");
-    }
-    oc_resource_t *device = &core_resources[new_num - OCF_D];
-    memset(device, 0, OCF_D * sizeof(oc_resource_t));
+  core_resources =
+    (oc_resource_t *)realloc(core_resources, new_num * sizeof(oc_resource_t));
 
-    oc_device_info = (oc_device_info_t *)realloc(
-      oc_device_info, (device_count + 1) * sizeof(oc_device_info_t));
-    if (!oc_device_info) {
-      oc_abort("Insufficient memory");
-    }
-    memset(&oc_device_info[device_count], 0, sizeof(oc_device_info_t));
+  if (!core_resources) {
+    oc_abort("Insufficient memory");
   }
+  oc_resource_t *device = &core_resources[new_num - OCF_D];
+  memset(device, 0, OCF_D * sizeof(oc_resource_t));
+
+  oc_device_info = (oc_device_info_t *)realloc(
+    oc_device_info, (device_count + 1) * sizeof(oc_device_info_t));
+
+  if (!oc_device_info) {
+    oc_abort("Insufficient memory");
+  }
+  memset(&oc_device_info[device_count], 0, sizeof(oc_device_info_t));
+
 #endif /* OC_DYNAMIC_ALLOCATION */
 
 #ifndef OC_SECURITY
@@ -349,7 +407,7 @@ oc_core_add_new_device(const char *uri, const char *rt, const char *name,
 
   device_count++;
 
-  return &oc_device_info[device_count];
+  return &oc_device_info[device_count - 1];
 }
 
 void
