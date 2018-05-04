35be05d6638367b2a90147e87dea02ee8f0247dc
Kush <kush.agrawal@samsung.com>
2018-05-04 10:59:57 +0530

    Easy Setup:Added samsung specific properties
     -Added dev conf properties for easy-setup
     -Added sc , cloud conf , wifi conf properties
     -Added samsung properties specific sample app for easy setup
    
    Change-Id: I3c8f022e96c6aa5f08c14c5dd4ae0feb42a07982
    Signed-off-by: Kush <kush.agrawal@samsung.com>

---
+++ apps/sc_easysetup_linux.c
@@ -0,0 +1,429 @@
+//******************************************************************
+//
+// Copyright 2015 Samsung Electronics All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+#include "oc_api.h"
+#include "port/oc_clock.h"
+
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include "samsung/sc_easysetup.h"
+
+#include "easysetup.h"
+
+static pthread_mutex_t mutex;
+static pthread_cond_t cv;
+static struct timespec ts;
+static int quit = 0;
+
+static double temp_C = 5.0, min_C = 0.0, max_C = 100.0, min_K = 273.15,
+              max_K = 373.15, min_F = 32, max_F = 212;
+typedef enum { C = 100, F, K } units_t;
+
+/**
+ * @var gIsSecured
+ * @brief Variable to check if secure mode is enabled or not.
+ */
+static bool gIsSecured = false;
+
+static sc_properties g_SCProperties ;
+
+static int
+app_init(void)
+{
+  int err = oc_init_platform("Samsung", NULL, NULL);
+
+  err |= oc_add_device("/oic/d", "oic.d.airconditioner", "[Floor A/C] Samsung", "ocf.1.0.0",
+                        "ocf.res.1.0.0", NULL, NULL);
+  return err;
+}
+
+static void
+get_temp(oc_request_t *request, oc_interface_mask_t interface, void *user_data)
+{
+  (void)user_data;
+  PRINT("[ES App] GET_temp:\n");
+  bool invalid_query = false;
+  double temp = temp_C;
+  units_t temp_units = C;
+  char *units;
+  int units_len = oc_get_query_value(request, "units", &units);
+  if (units_len != -1) {
+    if (units[0] == 'K') {
+      temp = temp_C + 273.15;
+      temp_units = K;
+    } else if (units[0] == 'F') {
+      temp = (temp_C / 100) * 180 + 32;
+      temp_units = F;
+    } else if (units[0] != 'C')
+      invalid_query = true;
+  }
+
+  oc_rep_start_root_object();
+  switch (interface) {
+  case OC_IF_BASELINE:
+    oc_process_baseline_interface(request->resource);
+    oc_rep_set_text_string(root, id, "home_thermostat");
+  /* fall through */
+  case OC_IF_A:
+  case OC_IF_S:
+    oc_rep_set_double(root, temperature, temp);
+    switch (temp_units) {
+    case C:
+      oc_rep_set_text_string(root, units, "C");
+      break;
+    case F:
+      oc_rep_set_text_string(root, units, "F");
+      break;
+    case K:
+      oc_rep_set_text_string(root, units, "K");
+      break;
+    }
+    break;
+  default:
+    break;
+  }
+
+  if (!invalid_query) {
+    oc_rep_set_array(root, range);
+    switch (temp_units) {
+    case C:
+      oc_rep_add_double(range, min_C);
+      oc_rep_add_double(range, max_C);
+      break;
+    case K:
+      oc_rep_add_double(range, min_K);
+      oc_rep_add_double(range, max_K);
+      break;
+    case F:
+      oc_rep_add_double(range, min_F);
+      oc_rep_add_double(range, max_F);
+      break;
+    }
+    oc_rep_close_array(root, range);
+  }
+
+  oc_rep_end_root_object();
+
+  if (invalid_query)
+    oc_send_response(request, OC_STATUS_FORBIDDEN);
+  else
+    oc_send_response(request, OC_STATUS_OK);
+}
+
+static void
+post_temp(oc_request_t *request, oc_interface_mask_t interface, void *user_data)
+{
+  (void)interface;
+  (void)user_data;
+  PRINT("[ES App] POST_temp:\n");
+  bool out_of_range = false;
+  double temp = -1;
+
+  oc_rep_t *rep = request->request_payload;
+  while (rep != NULL) {
+    switch (rep->type) {
+    case OC_REP_DOUBLE:
+      temp = rep->value.double_p;
+      break;
+    default:
+      break;
+    }
+    rep = rep->next;
+  }
+
+  if (temp < min_C || temp > max_C)
+    out_of_range = true;
+
+  temp_C = temp;
+
+  oc_rep_start_root_object();
+  oc_rep_set_text_string(root, id, "home_thermostat");
+  oc_rep_set_double(root, temperature, temp_C);
+  oc_rep_set_text_string(root, units, "C");
+  oc_rep_set_array(root, range);
+  oc_rep_add_double(range, min_C);
+  oc_rep_add_double(range, max_C);
+  oc_rep_close_array(root, range);
+  oc_rep_end_root_object();
+
+  if (out_of_range)
+    oc_send_response(request, OC_STATUS_FORBIDDEN);
+  else
+    oc_send_response(request, OC_STATUS_CHANGED);
+}
+
+void WiFiProvCbInApp(es_wifi_conf_data*eventData)
+{
+    printf("WiFiProvCbInApp IN\n");
+
+    if(eventData == NULL)
+    {
+        printf("ESWiFiProvData is NULL\n");
+        return ;
+    }
+
+    printf("SSID : %s\n", eventData->ssid);
+    printf("Password : %s\n", eventData->pwd);
+    printf("AuthType : %d\n", eventData->authtype);
+    printf("EncType : %d\n", eventData->enctype);
+
+    if(eventData->userdata != NULL)
+    {
+        sc_wifi_conf_properties*data = eventData->userdata;
+        printf("[SC] DiscoveryChannel : %d\n", data->discoveryChannel);
+    }
+
+    printf("WiFiProvCbInApp OUT\n");
+}
+
+void DevConfProvCbInApp(es_dev_conf_data*eventData)
+{
+    printf("[ES App] DevConfProvCbInApp IN\n");
+
+    if(eventData == NULL)
+    {
+        printf("[ES App] ESDevConfProvData is NULL\n");
+        return ;
+    }
+
+    if(eventData->userdata != NULL)
+    {
+        sc_dev_conf_properties*data = eventData->userdata;
+        for(int i = 0 ; i < data->numLocation ; ++i)
+        {
+            printf("[SC] Location : %s\n", data->location[i]);
+        }
+        printf("[SC] Register Mobile Device : %s\n", data->regMobileDev);
+        printf("[SC] Country : %s\n", data->country);
+        printf("[SC] Language : %s\n", data->language);
+        printf("[SC] GPS Location : %s\n", data->gpsLocation);
+        printf("[SC] UTC Date time : %s\n", data->utcDateTime);
+        printf("[SC] Regional time : %s\n", data->regionalDateTime);
+        printf("[SC] SSO List : %s\n", data->ssoList);
+    }
+
+    printf("[ES App] DevConfProvCbInApp OUT\n");
+}
+
+void CloudDataProvCbInApp(es_coap_cloud_conf_data *eventData)
+{
+    printf("[ES App] CloudDataProvCbInApp IN\n");
+
+    if(eventData == NULL)
+    {
+        printf("ESCloudProvData is NULL\n");
+        return ;
+    }
+
+    if (eventData->auth_code)
+    {
+        printf("AuthCode : %s\n", eventData->auth_code);
+    }
+
+    if (eventData->access_token)
+    {
+        printf("Access Token : %s\n", eventData->access_token);
+    }
+
+    if (eventData->auth_provider)
+    {
+        printf("AuthProvider : %s\n", eventData->auth_provider);
+    }
+
+    if (eventData->ci_server)
+    {
+        printf("CI Server : %s\n", eventData->ci_server);
+    }
+
+    if(eventData->userdata != NULL)
+    {
+        sc_coap_cloud_server_conf_properties *data = eventData->userdata;
+        printf("[SC] ClientID : %s\n", data->clientID);
+    }
+
+    printf("[ES App] CloudDataProvCbInApp OUT\n");
+}
+
+es_provisioning_callbacks_s gCallbacks = {
+    .wifi_prov_cb = &WiFiProvCbInApp,
+    .dev_conf_prov_cb = &DevConfProvCbInApp,
+    .cloud_data_prov_cb = &CloudDataProvCbInApp
+};
+
+void StartEasySetup()
+{
+    printf("[ES App] StartEasySetup IN\n");
+
+    es_connect_type resourcemMask = ES_WIFICONF_RESOURCE | ES_COAPCLOUDCONF_RESOURCE | ES_DEVCONF_RESOURCE;
+    if(es_init_enrollee(gIsSecured, resourcemMask, gCallbacks) != ES_OK)
+    {
+        printf("[ES App] Easy Setup Enrollee init error!!\n");
+        return;
+    }
+
+    printf("[ES App] ESInitEnrollee Success\n");
+
+    // Set callbacks for Vendor Specific Properties
+    es_set_callback_for_userdata(&ReadUserdataCb, &WriteUserdataCb);
+    printf("[ES App] StartEasySetup OUT\n");
+}
+
+void SetDeviceInfo()
+{
+    printf("[ES App] SetDeviceInfo IN\n");
+
+    es_device_property deviceProperty = {
+        {{WIFI_11G, WIFI_11N, WIFI_11AC, WiFi_EOF}, WIFI_5G},
+            {"Test Device"}
+    };
+
+    if(es_set_device_property(&deviceProperty) == ES_ERROR)
+        printf("[ES App] ESSetDeviceProperty Error\n");
+
+    // Set user properties if needed
+
+    memset(&g_SCProperties, 0, sizeof(sc_properties));
+    strncpy(g_SCProperties.deviceType, "deviceType", MAXLEN_STRING);
+    strncpy(g_SCProperties.deviceSubType, "deviceSubType", MAXLEN_STRING);
+    g_SCProperties.netConnectionState = NET_STATE_INIT;
+    g_SCProperties.discoveryChannel = WIFI_DISCOVERY_CHANNEL_INIT;
+    strncpy(g_SCProperties.regSetDev, "{\"wm\":\"00:11:22:33:44:55\",\"pm\":\"00:11:22:33:44:55\",\"bm\":\"00:11:22:33:44:55\",\"rk\":[\"VOICE\",\"EXTRA\",\"BTHIDPOWERON\"],\"sl\":[\"TV2MOBILE\",\"MOBILE2TV\",\"BTWAKEUP\",\"WOWLAN\",\"BTREMOTECON\",\"DLNADMR\"]}", MAXLEN_STRING);
+    strncpy(g_SCProperties.nwProvInfo, "{\"IMEI\":\"123456789012345 / 01\",\"IMSI\":\"123401234567890\",\"MCC_MNC\":\"100_10\",\"SN\":\"XY0123456XYZ\"}", MAXLEN_STRING);
+    strncpy(g_SCProperties.pnpPin, "pinNumber", MAXLEN_STRING);
+    strncpy(g_SCProperties.modelNumber, "Model Number", MAXLEN_STRING);
+    strncpy(g_SCProperties.esProtocolVersion, "2.0", MAXLEN_STRING);
+
+    if(set_sc_properties(&g_SCProperties) == ES_ERROR)
+        printf("SetSCProperties Error\n");
+	
+    printf("[ES App] SetDeviceInfo OUT\n");
+}
+
+void StopEasySetup()
+{
+    printf("[ES App] StopEasySetup IN\n");
+
+    if (es_terminate_enrollee() == ES_ERROR)
+    {
+        printf("ESTerminateEnrollee Failed!!\n");
+        return;
+    }
+
+    printf("[ES App] StopEasySetup OUT\n");
+}
+
+static void
+register_resources(void)
+{
+    printf("[ES App] register_resources IN\n");
+
+    oc_resource_t *temp = oc_new_resource("tempsensor", "/temp", 1, 0);
+    oc_resource_bind_resource_type(temp, "oic.r.temperature");
+    oc_resource_bind_resource_interface(temp, OC_IF_A);
+    oc_resource_bind_resource_interface(temp, OC_IF_S);
+    oc_resource_set_default_interface(temp, OC_IF_A);
+    oc_resource_set_discoverable(temp, true);
+    oc_resource_set_periodic_observable(temp, 1);
+    oc_resource_set_request_handler(temp, OC_GET, get_temp, NULL);
+    oc_resource_set_request_handler(temp, OC_POST, post_temp, NULL);
+    oc_add_resource(temp);
+
+#ifdef OC_SECURITY
+    gIsSecured = true;
+#else
+    gIsSecured = false;
+#endif
+
+    StartEasySetup();
+    SetDeviceInfo();
+
+    printf("[ES App] register_resources OUT\n");
+}
+
+static void
+signal_event_loop(void)
+{
+  pthread_mutex_lock(&mutex);
+  pthread_cond_signal(&cv);
+  pthread_mutex_unlock(&mutex);
+}
+
+static void
+handle_signal(int signal)
+{
+  (void)signal;
+  signal_event_loop();
+  quit = 1;
+}
+
+int
+main(void)
+{
+    int init;
+  struct sigaction sa;
+  sigfillset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sa.sa_handler = handle_signal;
+  sigaction(SIGINT, &sa, NULL);
+
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&cv, NULL);
+
+  static const oc_handler_t handler = {.init = app_init,
+                                       .signal_event_loop = signal_event_loop,
+                                       .register_resources =
+                                         register_resources };
+
+  oc_clock_time_t next_event;
+
+  oc_set_mtu_size(2048);
+  oc_set_max_app_data_size(8192);
+
+#ifdef OC_SECURITY
+  oc_storage_config("./smart_home_server_linux_creds");
+#endif /* OC_SECURITY */
+
+  init = oc_main_init(&handler);
+  if (init < 0)
+    return init;
+
+  while (quit != 1) {
+    next_event = oc_main_poll();
+    pthread_mutex_lock(&mutex);
+    if (next_event == 0) {
+      pthread_cond_wait(&cv, &mutex);
+    } else {
+      ts.tv_sec = (next_event / OC_CLOCK_SECOND);
+      ts.tv_nsec = (next_event % OC_CLOCK_SECOND) * 1.e09 / OC_CLOCK_SECOND;
+      pthread_cond_timedwait(&cv, &mutex, &ts);
+    }
+    pthread_mutex_unlock(&mutex);
+  }
+
+  oc_main_shutdown();
+
+  StopEasySetup();
+
+  printf("[ES App] Exit..\n");
+  return 0;
+}
+
---
+++ service/easy-setup/enrollee/include/samsung/sc_easysetup.h
@@ -0,0 +1,155 @@
+//******************************************************************
+//
+// Copyright 2015 Samsung Electronics All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#include "easysetup.h"
+#include "escommon.h"
+
+#ifndef EASYSETUPX_ENROLLEE_H__
+#define EASYSETUPX_ENROLLEE_H__
+
+#define MAXLEN_STRING 1000
+#define MAXNUM_LOCATION 20
+#define MAXLEN_DATE_TIME 33
+
+
+#define STR_SC_RSRVD_ES_VENDOR_NETCONNECTION_STATE  "x.com.samsung.ncs"
+#define STR_SC_RSRVD_ES_VENDOR_DISCOVERY_CHANNEL    "x.com.samsung.chn"
+#define STR_SC_RSRVD_ES_VENDOR_DEVICE_TYPE          "x.com.samsung.dt"
+#define STR_SC_RSRVD_ES_VENDOR_DEVICE_SUBTYPE       "x.com.samsung.sdt"
+#define STR_SC_RSRVD_ES_VENDOR_LOCATION             "x.com.samsung.location"
+#define STR_SC_RSRVD_ES_VENDOR_CLIENTID             "x.com.samsung.clientid"
+#define STR_SC_RSRVD_ES_VENDOR_REGISTER_MOBILE_DEV  "x.com.samsung.rmd"
+#define STR_SC_RSRVD_ES_VENDOR_REGISTER_SET_DEV     "x.com.samsung.rsd"
+#define STR_SC_RSRVD_ES_VENDOR_NETWORK_PROV_INFO    "x.com.samsung.npi"
+#define STR_SC_RSRVD_ES_VENDOR_ACCOUNT              "x.com.samsung.account"
+#define STR_SC_RSRVD_ES_VENDOR_SSO_LIST            "x.com.samsung.ssolist"
+#define STR_SC_RSRVD_ES_VENDOR_AAC                  "x.com.samsung.aac"
+#define STR_SC_RSRVD_ES_VENDOR_TNC_HEADER           "x.com.samsung.tcheader"
+#define STR_SC_RSRVD_ES_VENDOR_TNC_VERSION          "x.com.samsung.tcversion"
+#define STR_SC_RSRVD_ES_VENDOR_TNC_RESULT           "x.com.samsung.tcresult"
+#define STR_SC_RSRVD_ES_VENDOR_TNC_STATUS           "x.com.samsung.tcstatus"
+#define STR_SC_RSRVD_ES_VENDOR_REFRESH_TOKEN        "x.com.samsung.refreshtoken"
+#define STR_SC_RSRVD_ES_VENDOR_UID                  "x.com.samsung.uid"
+#define STR_SC_RSRVD_ES_VENDOR_BSSID                "x.com.samsung.bssid"
+#define STR_SC_RSRVD_ES_VENDOR_PNP_PIN              "x.com.samsung.pnppin"
+#define STR_SC_RSRVD_ES_VENDOR_MODEL_NUMBER         "x.com.samsung.modelnumber"
+#define STR_SC_RSRVD_ES_VENDOR_LANGUAGE             "x.com.samsung.language"
+#define STR_SC_RSRVD_ES_VENDOR_COUNTRY              "x.com.samsung.country"
+#define STR_SC_RSRVD_ES_VENDOR_GPSLOCATION          "x.com.samsung.gpslocation"
+#define STR_SC_RSRVD_ES_VENDOR_UTC_DATE_TIME        "x.com.samsung.datetime"
+#define STR_SC_RSRVD_ES_VENDOR_REGIONAL_DATE_TIME   "x.com.samsung.regionaldatetime"
+#define STR_SC_RSRVD_ES_VENDOR_ES_PROTOCOL_VERSION  "x.com.samsung.espv"
+
+#define WIFI_DISCOVERY_CHANNEL_INIT             -1
+
+/**
+ * @brief  Supported WIFI frequency like 2.4G and 5G
+ */
+typedef enum
+{
+    NET_STATE_INIT = -1,                /**< Init state **/
+    NET_STATE_WIRED_CONNECTED = 0,      /**< Wired connected **/
+    NET_STATE_WIRELESS_CONNECTED,       /**< Wireless connected **/
+    NET_STATE_NOT_CONNECTED             /**< Not connected, at all **/
+} NETCONNECTION_STATE;
+
+typedef struct sc_wifi_conf_properties
+{
+    int discoveryChannel;                   /**< Wi-Fi AP Channel used for fast discovery **/
+    char bssid[MAXLEN_STRING];              /**< Wi-Fi bssid information. **/
+} sc_wifi_conf_properties;
+
+typedef struct sc_tnc_info
+{
+    char header[MAXLEN_STRING]; /**< Terms & Conditions header **/
+    char version[MAXLEN_STRING];    /**< Terms & Conditions version **/
+}sc_tnc_info;
+
+typedef struct sc_dev_conf_properties
+{
+    int numLocation;
+    char location[MAXNUM_LOCATION][MAXLEN_STRING];  /**< Samsung-specific location-related information **/
+    char regMobileDev[MAXLEN_STRING];               /**< Samsung-specific mobile device information for 'register TV' **/
+    char account[MAXLEN_STRING];  /**< Samsung-specific account-related information **/
+    char ssoList[MAXLEN_STRING];            /**< Samsung-specific SSO list information which is registered in device **/
+    sc_tnc_info scTnCInfo;    /**< Samsung-specific Terms & Conditions information **/
+    char modelNumber[MAXLEN_STRING];                /**< Samsung-specific model number **/
+    char language[MAXLEN_STRING];                   /**< IETF language tag using ISO 639X **/
+    char country[MAXLEN_STRING];                    /**< ISO Country Code (ISO 3166-1 Alpha-2) **/
+    char gpsLocation[MAXLEN_STRING];                /**< GPS information of device. Longitude and latitude in json format **/
+    char utcDateTime[MAXLEN_DATE_TIME];             /**< UTC date time **/
+    char regionalDateTime[MAXLEN_DATE_TIME];        /**< Regional date time **/
+} sc_dev_conf_properties;
+
+typedef struct sc_coap_cloud_server_conf_properties
+{
+    char clientID[MAXLEN_STRING];               /**< Samsung-specific clientId for sign-up to IoT Cloud **/
+    char aac[MAXLEN_STRING];                    /**< Samsung-specific aac information **/
+    char tncResult[MAXLEN_STRING];              /**< Samsung-specific Terms & Conditions result **/
+    char refreshToken[MAXLEN_STRING];           /**< Samsung-specific refreshToken information. Indicate refresh token to be used if the access token is expired**/
+    char uid[MAXLEN_STRING];                    /**< Samsung-specific aac information. Indicate user ID corresponding to user account **/
+} sc_coap_cloud_server_conf_properties;
+
+typedef struct sc_properties
+{
+    NETCONNECTION_STATE netConnectionState; /**< A state of network connection **/
+    int discoveryChannel;                   /**< Wi-Fi AP Channel used for fast discovery **/
+    char deviceType[MAXLEN_STRING];         /**< Generated with Device Type + Icon Type **/
+    char deviceSubType[MAXLEN_STRING];      /**< Device Sub Category **/
+    int numLocation;
+    char location[MAXNUM_LOCATION][MAXLEN_STRING];  /**< Samsung-specific location-related information **/
+    char clientID[MAXLEN_STRING];           /**< Samsung-specific clientId for sign-up to IoT Cloud **/
+    char regMobileDev[MAXLEN_STRING];       /**< Samsung-specific mobile device information for 'register TV' **/
+    char regSetDev[MAXLEN_STRING];          /**< Samsung-specific set device information for 'register TV' **/
+    char nwProvInfo[MAXLEN_STRING];         /**< Samsung-specific network provisioning information for cellular network vendor **/
+    char account[MAXLEN_STRING];            /**< Samsung-specific account-related information **/
+    char ssoList[MAXLEN_STRING];            /**< Samsung-specific SSO list information which is registered in device **/
+    char aac[MAXLEN_STRING];                /**< Samsung-specific aac information **/
+    sc_tnc_info tncInfo;                      /**< Samsung-specific Terms & Conditions information **/
+    char tncResult[MAXLEN_STRING];          /**< Samsung-specific Terms & Conditions result **/
+    int tncStatus;                          /**< Samsung-specific Terms & Conditions status **/
+    char refreshToken[MAXLEN_STRING];       /**< Samsung-specific refreshToken information. Indicate refresh token to be used if the access token is expired**/
+    char uid[MAXLEN_STRING];                /**< Samsung-specific aac information. Indicate user ID corresponding to user account **/
+    char bssid[MAXLEN_STRING];              /**< Samsung-specific Wi-Fi bssid information. **/
+    char pnpPin[MAXLEN_STRING];             /**< Samsung-specific PnP Pin **/
+    char modelNumber[MAXLEN_STRING];                /**< Samsung-specific model number **/
+    char language[MAXLEN_STRING];                   /**< IETF language tag using ISO 639X **/
+    char country[MAXLEN_STRING];                    /**< ISO Country Code (ISO 3166-1 Alpha-2) **/
+    char gpsLocation[MAXLEN_STRING];                /**< GPS information of device. Longitude and latitude in json format **/
+    char utcDateTime[MAXLEN_DATE_TIME];             /**< UTC date time **/
+    char regionalDateTime[MAXLEN_DATE_TIME];        /**< Regional date time **/
+    char esProtocolVersion[MAXLEN_STRING];          /**< Samsung Easy Setup Protocol Version **/
+} sc_properties;
+
+void  ReadUserdataCb(oc_rep_t* payload, char* resourceType, void** userdata);
+void WriteUserdataCb(oc_rep_t* payload, char* resourceType);
+
+es_result_e set_sc_properties(const sc_properties *prop);
+
+es_result_e set_register_set_device(const char *regSetDevice);
+es_result_e set_network_prov_info(const char *nwProvInfo);
+es_result_e set_sc_tnc_info(sc_tnc_info *tncInfo);
+es_result_e set_sc_tnc_status(int status);
+es_result_e set_sc_net_connection_state(NETCONNECTION_STATE netConnectionState);
+es_result_e set_sc_pnp_pin(const char *pnp);
+
+es_result_e set_es_version_info(const char *esVersionInfo);
+
+#endif /* EASYSETUPX_ENROLLEE_H__ */
