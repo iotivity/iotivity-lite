b166a8d90ca987940b3f79bd035045316afdf77b
Abhishek Pandey <abhi.siso@samsung.com>
2018-05-07 15:32:25 +0530

    [Easy Setup] Coding rules fix
    - Fixed variable naming as per constrained code style.
    - Did some code cleanup of sample code.
    - Removed un-necessary function name prints from Debug logs.
    - Done formatting using ./tools/check-style script (using
      clang-format-3.9).
    - Fixed termination crash in easy setup app due to wrong termination order.
    - Avoided unused code building when Soft AP, Auto Wi-Fi Connection
      macro is off.
    
    Change-Id: I14e8fb6d9a42319ab77d3622c491baaa19482a56
    Signed-off-by: Abhishek Pandey <abhi.siso@samsung.com>

--- service/easy-setup/enrollee/include/easysetup.h
+++ service/easy-setup/enrollee/include/easysetup.h
@@ -19,8 +19,8 @@
 #ifndef EASYSETUP_ENROLLEE_H
 #define EASYSETUP_ENROLLEE_H
 
-#include "escommon.h"
-#include "ESEnrolleeCommon.h"
+#include "es_enrollee_common.h"
+#include "es_common.h"
 
 /**
  * @file
@@ -33,45 +33,58 @@ extern "C" {
 #endif // __cplusplus
 
 /**
- * A function pointer for registering a user-defined function to set user-specific properties to a
+ * A function pointer for registering a user-defined function to set
+ * user-specific properties to a
  * response going back to a client.
- * @param payload Represents a response. You can set a specific value with specific property key
- * to the payload. If a client receives the response and know the property key, then it can
+ * @param payload Represents a response. You can set a specific value with
+ * specific property key
+ * to the payload. If a client receives the response and know the property key,
+ * then it can
  * extract the value.
- * @param resource_type Used to distinguish which resource the received property belongs to.
+ * @param resource_type Used to distinguish which resource the received property
+ * belongs to.
  */
-typedef void (*es_write_userdata_cb) (oc_rep_t* payload, char* resource_type);
+typedef void (*es_write_userdata_cb)(oc_rep_t *payload, char *resource_type);
 
 /**
- * A function pointer for registering a user-defined function to parse user-specific properties
+ * A function pointer for registering a user-defined function to parse
+ * user-specific properties
  * from received POST request.
- * @param payload Represents a received POST request. If you know user-specific property key,
+ * @param payload Represents a received POST request. If you know user-specific
+ * property key,
  * then you can extract a corresponding value if it exists.
- * @param resource_type Used to distinguish which resource the received property belongs to
- * @param userdata User-specific data you want to deliver to desired users, i.e. application.
+ * @param resource_type Used to distinguish which resource the received property
+ * belongs to
+ * @param userdata User-specific data you want to deliver to desired users, i.e.
+ * application.
  * The user should know a data structure of passed userdata.
  */
-typedef void (*es_read_userdata_cb) (oc_rep_t* payload, char* resource_type, void** userdata);
+typedef void (*es_read_userdata_cb)(oc_rep_t *payload, char *resource_type,
+                                    void **userdata);
 
 /**
- * This function initializes the EasySetup. This API must be called prior to invoking any other API.
+ * This function initializes the EasySetup. This API must be called prior to
+ * invoking any other API.
  *
  * @param is_secured        True if the Enrollee is operating in secured mode.
- * @param resource_mask     Easy Setup Resource Type which application wants to create.
+ * @param resource_mask     Easy Setup Resource Type which application wants to
+ * create.
  *                          ES_WIFICONF_RESOURCE = 0x01,
  *                          ES_COAPCLOUDCONF_RESOURCE = 0x02,
  *                          ES_DEVCONF_RESOURCE = 0x04
- * @param callbacks         ESProvisioningCallbacks for updating Easy setup Resources' data to the
+ * @param callbacks         ESProvisioningCallbacks for updating Easy setup
+ * Resources' data to the
  *                          application
  * @return ::ES_OK on success, some other value upon failure.
  */
 es_result_e es_init_enrollee(bool is_secured, es_resource_mask_e resource_mask,
-                              es_provisioning_callbacks_s callbacks);
+                             es_provisioning_callbacks_s callbacks);
 
 /**
  * This function sets Device information.
  *
- * @param es_device_property Contains device information composed of WiFiConf Structure & DevConf
+ * @param es_device_property Contains device information composed of WiFiConf
+ * Structure & DevConf
  *                           Structure
  * @return ::ES_OK on success, some other value upon failure.
  *
@@ -107,9 +120,11 @@ es_result_e es_set_error_code(es_error_code es_err_code);
 es_result_e es_terminate_enrollee(void);
 
 /**
- * This function is to set two function pointer to handle user-specific properties in in-comming
+ * This function is to set two function pointer to handle user-specific
+ * properties in in-comming
  * POST request and to out-going response for GET or POST request.
- * If you register certain functions with this API, you have to handle oc_rep_t structure to
+ * If you register certain functions with this API, you have to handle oc_rep_t
+ * structure to
  * set and get properties you want.
  *
  * @param readcb a callback for parsing properties from POST request
@@ -120,11 +135,11 @@ es_result_e es_terminate_enrollee(void);
  * @see es_read_userdata_cb
  * @see es_write_userdata_cb
  */
-es_result_e es_set_callback_for_userdata(es_read_userdata_cb readcb, es_write_userdata_cb writecb);
+es_result_e es_set_callback_for_userdata(es_read_userdata_cb readcb,
+                                         es_write_userdata_cb writecb);
 
 #ifdef __cplusplus
 }
 #endif // __cplusplus
-
 
 #endif /* EASYSETUP_ENROLLEE_H */
