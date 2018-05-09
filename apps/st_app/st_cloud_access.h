77f12d7feb6d2f29987da7e89db0018dd272882c
Jaehong Jo <jaehong.jo@samsung.com>
2018-05-09 21:24:36 +0900

    Add sample app for easysetup & cloud access.
    Change-Id: I1a6d7d693a19ad4e31f362f10137e256f96f4aad
    Signed-off-by: Jaehyun Cho <jaehyun3.cho@samsung.com>
    Signed-off-by: Jaehong Jo <jaehong.jo@samsung.com>

---
+++ apps/st_app/st_cloud_access.h
@@ -0,0 +1,39 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#include "easysetup.h"
+#include <stdbool.h>
+
+typedef enum {
+  CLOUD_ACCESS_INITIALIZE,
+  CLOUD_ACCESS_PROCRESSING,
+  CLOUD_ACCESS_FINISH,
+  CLOUD_ACCESS_PING,
+  CLOUD_ACCESS_PING_FAIL,
+  CLOUD_ACCESS_FAIL,
+  CLOUD_ACCESS_DISCONNECTED
+} st_cloud_access_status_t;
+
+typedef void (*st_cloud_access_cb_t)(st_cloud_access_status_t status);
+
+bool st_cloud_access_start(es_coap_cloud_conf_data *cloud_info,
+                           oc_link_t *publish_resources, int device_index,
+                           st_cloud_access_cb_t cb);
+void st_cloud_access_stop(int device_index);
+
+st_cloud_access_status_t get_cloud_access_status(int device_index);
