973ca4b3ed3ee10f8ed255133d5ad5b1d52469ad
ol.beketov <ol.beketov@samsung.com>
2018-05-15 18:29:19 +0300

    MFG OTM
    mfg server sample added
    
    Change-Id: If045eac050e558d483fe082b62d843e13bd63072
    Signed-off-by: ol.beketov <ol.beketov@samsung.com>

--- security/oc_doxm.h
+++ security/oc_doxm.h
@@ -39,6 +39,7 @@ void oc_sec_doxm_init(void);
 void oc_sec_doxm_free(void);
 bool oc_sec_decode_doxm(oc_rep_t *rep, bool from_storage, int device);
 void oc_sec_encode_doxm(int device);
+void oc_sec_doxm_mfg(int device);
 oc_sec_doxm_t *oc_sec_get_doxm(int device);
 void oc_sec_doxm_default(int device);
 void get_doxm(oc_request_t *request, oc_interface_mask_t interface, void *data);
