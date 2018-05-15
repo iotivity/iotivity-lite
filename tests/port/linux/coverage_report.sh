99f60bf906486f88b822ee08206196f5f9e0d91e
towhid <t.islam@samsung.com>
2018-05-15 15:54:17 +0600

    Adding script to check sampleapp memory profile
    required tool:
    $sudo apt-get install valgrind
    $sudo apt-get install massif-visualizer
    
    usages:
    $./memory_profile.sh <sample_app_name>
    after sample app run and close successfully. the script
    will generate memory profile file on memory_profile/massif.out.*
    download and open with massif-visualizer to view memory usage.
    
    Bug: https://jira.iotivity.org/browse/LITE-14
    Change-Id: I82246c2c60565f68ba1fb336bced016a565e4ab9
    Signed-off-by: towhid <t.islam@samsung.com>

--- tests/port/linux/coverage_report.sh
+++ tests/port/linux/coverage_report.sh
@@ -15,7 +15,7 @@ cd $linux_dir
 sed -i "s/$match/$insert/" $file
 
 make clean
-make test
+make qa_test
 mkdir ${iotivity}/coverage_report
 lcov -c -d ../../ -o ${iotivity}/coverage_report/new_coverage.info && lcov --remove ${iotivity}/coverage_report/new_coverage.info 'port/unittest/*' '/usr/include/*' 'api/unittest/*' -o ${iotivity}/coverage_report/main_coverage.info
 
