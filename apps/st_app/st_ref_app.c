22df3f36507b780514f7d199f994ad70e46fb05f
Jaehyun Cho <jaehyun3.cho@samsung.com>
2018-05-28 20:35:16 +0900

    Modify Resource handling logic.
    - resource properties value will treated in app level.
    - registered cb usint st_register_resource_handler() will
      all cover about resource get/set logic.
    - if cb return false, resource manager need to handle
      rollback making payload.(oc_rep_reset can handle it)
    
    Change-Id: I5f8eedec9427d314b4b58b95974e0432aedadb25
    Signed-off-by: Jaehyun Cho <jaehyun3.cho@samsung.com>

--- apps/st_app/st_ref_app.c
+++ apps/st_app/st_ref_app.c
@@ -18,8 +18,123 @@
 
 #include "st_manager.h"
 #include "st_port.h"
+#include "st_resource_manager.h"
 
-// TODO: resource handling callbacks.
+static const char *switch_rsc_uri = "/capability/switch/main/0";
+static const char *switchlevel_rsc_uri = "/capability/switchLevel/main/0";
+static const char *color_temp_rsc_uri = "/capability/colorTemperature/main/0";
+
+static const char *power_prop_key = "power";
+static const char *dimming_prop_key = "dimmingSetting";
+static const char *ct_prop_key = "ct";
+
+static char power[10] = "on";
+
+static int dimmingSetting = 50;
+static int dimming_range[2] = { 0, 100 };
+static int dimming_step = 5;
+
+static int ct = 50;
+static int ct_range[2] = { 0, 100 };
+
+static void
+switch_resource_construct(void)
+{
+  oc_rep_set_text_string(root, power, power);
+}
+
+static void
+switchlevel_resource_construct(void)
+{
+  oc_rep_set_int(root, dimmingSetting, dimmingSetting);
+  oc_rep_set_int_array(root, range, dimming_range, 2);
+  oc_rep_set_int(root, step, dimming_step);
+}
+
+static void
+color_temp_resource_construct(void)
+{
+  oc_rep_set_int(root, ct, ct);
+  oc_rep_set_int_array(root, range, ct_range, 2);
+}
+
+static bool
+get_resource_handler(oc_request_t *request)
+{
+  if (strncmp(oc_string(request->resource->uri), switch_rsc_uri,
+              strlen(switch_rsc_uri)) == 0) {
+    switch_resource_construct();
+  } else if (strncmp(oc_string(request->resource->uri), switchlevel_rsc_uri,
+                     strlen(switchlevel_rsc_uri)) == 0) {
+    switchlevel_resource_construct();
+  } else if (strncmp(oc_string(request->resource->uri), color_temp_rsc_uri,
+                     strlen(color_temp_rsc_uri)) == 0) {
+    color_temp_resource_construct();
+  } else {
+    st_print_log("[ST_APP] invalid uri %s\n",
+                 oc_string(request->resource->uri));
+    return false;
+  }
+
+  return true;
+}
+static void
+switch_resource_change(oc_rep_t *rep)
+{
+  int len = 0;
+  char *m_power = NULL;
+  if (oc_rep_get_string(rep, power_prop_key, &m_power, &len)) {
+    strncpy(power, m_power, len);
+    power[len] = '\0';
+    st_print_log("[ST_APP]  %s : %s\n", oc_string(rep->name), power);
+
+    // TODO: device specific behavior.
+  }
+}
+
+static void
+switchlevel_resource_change(oc_rep_t *rep)
+{
+  if (oc_rep_get_int(rep, dimming_prop_key, &dimmingSetting)) {
+    st_print_log("[ST_APP]  %s : %d\n", oc_string(rep->name), dimmingSetting);
+
+    // TODO: device specific behavior.
+  }
+}
+
+static void
+color_temp_resource_change(oc_rep_t *rep)
+{
+  if (oc_rep_get_int(rep, ct_prop_key, &ct)) {
+    st_print_log("[ST_APP]  %s : %d\n", oc_string(rep->name), ct);
+
+    // TODO: device specific behavior.
+  }
+}
+
+static bool
+set_resource_handler(oc_request_t *request)
+{
+  if (strncmp(oc_string(request->resource->uri), switch_rsc_uri,
+              strlen(switch_rsc_uri)) == 0) {
+    switch_resource_change(request->request_payload);
+    switch_resource_construct();
+  } else if (strncmp(oc_string(request->resource->uri), switchlevel_rsc_uri,
+                     strlen(switchlevel_rsc_uri)) == 0) {
+    switchlevel_resource_change(request->request_payload);
+    switchlevel_resource_construct();
+  } else if (strncmp(oc_string(request->resource->uri), color_temp_rsc_uri,
+                     strlen(color_temp_rsc_uri)) == 0) {
+    color_temp_resource_change(request->request_payload);
+    color_temp_resource_construct();
+  } else {
+    st_print_log("[ST_APP] invalid uri %s\n",
+                 oc_string(request->resource->uri));
+    return false;
+  }
+
+  return true;
+}
 
 int
 main(void)
@@ -29,7 +144,9 @@ main(void)
     return -1;
   }
 
-  // TODO: callback registration.
+  st_register_resource_handler(get_resource_handler, set_resource_handler);
+
+  // TODO: callback registration. (ex. user confirm cb)
 
   if (st_manager_start() != 0) {
     st_print_log("[ST_APP] st_manager_start failed.\n");
