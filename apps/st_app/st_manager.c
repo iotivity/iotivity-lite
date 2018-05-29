c2f42022a966c5525f2c167145890d7012e6413d
Jaehong Jo <jaehong.jo@samsung.com>
2018-05-29 10:06:09 +0900

    Refactoring about st cloud access logic
    update cloud_access_status
    update message timeout
    update keepalive interval
    update refresh token
    
    Change-Id: Ib192fbe432d7dfad6ac37596d2d2a7406c585a9f
    Signed-off-by: Jaehong Jo <jaehong.jo@samsung.com>

--- apps/st_app/st_manager.c
+++ apps/st_app/st_manager.c
@@ -118,8 +118,6 @@ cloud_access_handler(st_cloud_access_status_t status)
     is_cloud_access_success = true;
   } else if (status == CLOUD_ACCESS_FAIL) {
     st_print_log("Cloud access failed!!!\n");
-  } else if (status == CLOUD_ACCESS_DISCONNECTED) {
-    st_print_log("Disconnected from cloud!\n");
     is_cloud_access_success = false;
   }
 }
