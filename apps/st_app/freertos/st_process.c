b164bbbca5d03772ffc3bf931dd4526b2a1321aa
Philippe Coval <p.coval@samsung.com>
2018-05-30 17:56:28 +0200

    freertos: Build st_app (with stubbed parts)
    IMHO, this should be refactored to split generic and platform parts,
    
    I think that files from:
      ./apps/st_app/${port}
    Should be relocated into:
      ./api/
    And then plaform specific code should be in:
      ./port/${port}
    Or eventually:
      ./extra/port/${port}
    if not used by lib internals,
    just to make app developer easier.
    
    Change-Id: I3b0a5e952f4678a3c2d3c568085df2575cc4eeda
    Bug: https://jira.iotivity.org/browse/LITE-8
    Origin: https://gerrit.iotivity.org/gerrit/#/c/25547/
    Signed-off-by: Philippe Coval <p.coval@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/25547
    Tested-by: IoTivity Jenkins <jenkins-daemon@iotivity.org>
    Reviewed-by: Uze Choi <uzchoi@samsung.com>

---
+++ apps/st_app/freertos/st_process.c
@@ -0,0 +1,80 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#include "oc_assert.h"
+#include "../st_process.h"
+#include "oc_api.h"
+
+typedef struct
+{
+  st_mutex_t mutex;
+  st_mutex_t app_mutex;
+  st_cond_t cv;
+  st_thread_t thread;
+  int quit;
+} st_process_data_t;
+
+static st_process_data_t g_process_data;
+
+static void *st_process_func(void *data);
+
+int
+st_process_init(void)
+{
+  oc_abort(__func__);
+  return 0;
+}
+
+int
+st_process_start(void)
+{
+  oc_abort(__func__);
+  return 0;
+}
+
+int
+st_process_stop(void)
+{
+  oc_abort(__func__);
+  return 0;
+}
+
+static void *
+st_process_func(void *data)
+{
+  oc_abort(__func__);
+  return NULL;
+}
+
+void
+st_process_signal(void)
+{
+  oc_abort(__func__);
+}
+
+void
+st_process_app_sync_lock(void)
+{
+  oc_abort(__func__);
+}
+
+void
+st_process_app_sync_unlock(void)
+{
+  oc_abort(__func__);
+}
---
+++ apps/st_app/freertos/st_ref_app.c
@@ -0,0 +1,94 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#include "oc_assert.h"
+#include "../st_manager.h"
+#include "../st_port.h"
+#include "../st_resource_manager.h"
+
+static const char *switch_rsc_uri = "/capability/switch/main/0";
+static const char *switchlevel_rsc_uri = "/capability/switchLevel/main/0";
+static const char *color_temp_rsc_uri = "/capability/colorTemperature/main/0";
+
+static const char *power_prop_key = "power";
+static const char *dimming_prop_key = "dimmingSetting";
+static const char *ct_prop_key = "ct";
+
+static char power[10] = "on";
+
+static int dimmingSetting = 50;
+static int dimming_range[2] = { 0, 100 };
+static int dimming_step = 5;
+
+static int ct = 50;
+static int ct_range[2] = { 0, 100 };
+
+static void
+switch_resource_construct(void)
+{
+  oc_abort(__func__);
+}
+
+static void
+switchlevel_resource_construct(void)
+{
+  oc_abort(__func__);
+}
+
+static void
+color_temp_resource_construct(void)
+{
+  oc_abort(__func__);
+}
+
+static bool
+get_resource_handler(oc_request_t *request)
+{
+  oc_abort(__func__);
+}
+static void
+switch_resource_change(oc_rep_t *rep)
+{
+  oc_abort(__func__);
+}
+
+static void
+switchlevel_resource_change(oc_rep_t *rep)
+{
+  oc_abort(__func__);
+}
+
+static void
+color_temp_resource_change(oc_rep_t *rep)
+{
+  oc_abort(__func__);
+}
+
+static bool
+set_resource_handler(oc_request_t *request)
+{
+  oc_abort(__func__);
+  return true;
+}
+
+int
+main(void)
+{
+  oc_abort(__func__);
+  return 0;
+}
