bcb4f8c79dd6c521ea293b92c6ce706dca7c7340
Jaehong Jo <jaehong.jo@samsung.com>
2018-05-31 23:34:41 +0900

    Fixed build error when SECURE=1 DYNAMIC=0
    Should be need to modify to right operation
    
    Change-Id: I955c0c0c6229d1ee0eefb1d35149b622a706d902
    Signed-off-by: Jaehong Jo <jaehong.jo@samsung.com>

--- security/oc_tls.c
+++ security/oc_tls.c
@@ -631,6 +631,7 @@ dtls_init_err:
 bool
 oc_sec_load_certs(int device)
 {
+#ifdef OC_DYNAMIC_ALLOCATION
   int i = 0, j = 0, ret = 0;
   for (i = 0; i < oc_core_get_num_devices(); i++) {
     mbedtls_x509_crt *cacert =
@@ -701,6 +702,9 @@ oc_sec_load_certs(int device)
     }
   }
   return true;
+#else
+  oc_abort("alloc failed");
+#endif
 tls_certs_load_err:
   OC_ERR("oc_tls: TLS initialization error");
   return false;
@@ -714,6 +718,7 @@ oc_sec_load_ca_cert(const unsigned char *ca_cert_buf, size_t ca_cet_buf_len)
     OC_ERR("oc_tls: empty ca cert buffer");
     goto tls_load_ca_cert_err;
   }
+#ifdef OC_DYNAMIC_ALLOCATION
   for (i = 0; i < oc_core_get_num_devices(); i++) {
     if (server_conf[i].ca_chain != NULL) {
       mbedtls_x509_crt_free(server_conf[i].ca_chain);
@@ -737,6 +742,9 @@ oc_sec_load_ca_cert(const unsigned char *ca_cert_buf, size_t ca_cet_buf_len)
 #endif /* OC_CLIENT */
   }
   return true;
+#else
+  oc_abort("alloc failed");
+#endif
 tls_load_ca_cert_err:
   OC_ERR("oc_tls: TLS initialization error");
   return false;
