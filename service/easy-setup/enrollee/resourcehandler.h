ba6d2b9df4144bfc1dc7ad535e8790136dbf43ed
Harish Kumara M <h.marappa@samsung.com>
2018-05-31 15:15:57 +0900

    More validation & reverting changes from oc_rep.h
    Moved changes of setting key:value where key is string
    type from oc_rep.h to es_util.h as these API requirement
    is specific to easysetup.
    
    Change-Id: I8b27095d823ca67b46c9eb84fcd0114876e6613b
    Signed-off-by: Harish Kumara M <h.marappa@samsung.com>

--- service/easy-setup/enrollee/resourcehandler.h
+++ service/easy-setup/enrollee/resourcehandler.h
@@ -31,7 +31,6 @@ es_result_e create_easysetup_resources(bool is_secured,
 void delete_easysetup_resources(void);
 es_result_e set_device_property(es_device_property *device_property);
 es_result_e set_enrollee_state(es_enrollee_state es_state);
-es_enrollee_state get_enrollee_state(void);
 es_result_e set_enrollee_err_code(es_error_code es_err_code);
 es_result_e set_callback_for_userdata(es_read_userdata_cb readcb,
                                       es_write_userdata_cb writecb,
