b9bba6bea081c87b98e3f7394b7e2123d5bb0b17
iot <rami.jung@samsung.com>
2018-06-06 14:37:21 +0900

    Fix crash in make test
    Add checking code when it does add pace
    https://jira.iotivity.org/browse/LITE-16
    
    Change-Id: Ic3441331093eacc890409b4ddc2d6fcf02652f90
    Signed-off-by: iot <rami.jung@samsung.com>

--- util/oc_mem_trace.c
+++ util/oc_mem_trace.c
@@ -69,6 +69,8 @@ oc_mem_trace_init(void)
 void
 oc_mem_trace_add_pace(const char *func, int size, int type, void *address)
 {
+  if(!mInfo.mem_log_list)
+    oc_mem_trace_init();
 
   if (type == MEM_TRACE_ALLOC) {
     mInfo.current += size;
