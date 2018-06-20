b2acae0b62c507055d421e9923fc50f60791a792
Anutosh <anutosh@graniteriverlabs.in>
2018-06-20 12:58:09 +0530

    UTC for TLS and Storage.
    Change-Id: Ib81ff4290f2cb33f86073f0322152f7e20f06ba7
    Signed-off-by: Anutosh <anutosh@graniteriverlabs.in>

---
+++ port/unittest/storagetest.cpp
@@ -0,0 +1,91 @@
+/******************************************************************
+ *
+ * Copyright 2018 GRANITE RIVER LABS All Rights Reserved.
+ *
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+#include <cstdlib>
+#include <string>
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+extern "C" {
+    #include "port/oc_storage.h"
+    #include <config.h>
+    }
+#define STORAGE_CONFIG_LONG "./IOTIVITY_CONSTRAINED_LITE_Sample_Test_devices_IOTIVITY_CONSTRAINED_LITE_Sample_Test_devices_IOTIVITY_CONSTRAINED_LITE_Sample_Test_devices_IOTIVITY_CONSTRAINED_LITE_Sample_Test_devices_IOTIVITY_CONSTRAINED_LITE_Sample_Test_devices_IOTIVITY_CONSTRAINED_LITE_Sample_Test_devices_IOTIVITY_CONSTRAINED_LITE_Sample_Test_devices_IOTIVITY_CONSTRAINED_LITE_Sample_Test_devices"
+#define STORAGE_CONFIG "./simpleserver_creds"
+//./simple_len_sert folder is not present
+//used to check the negative scenario
+#define STORAGE_CONFIG_N "./simple_len_sert"
+uint8_t buf[OC_MAX_APP_DATA_SIZE] = "uuuuuu";
+uint8_t buf2[OC_MAX_APP_DATA_SIZE] = "0000000000000000000000000000";
+char svr_tag[SVR_TAG_MAX];
+
+class TestStorage: public testing::Test
+{
+    protected:
+        virtual void SetUp()
+        {
+            oc_storage_config(STORAGE_CONFIG);
+        }
+        virtual void TearDown()
+        {
+          
+        }
+};
+
+TEST(TestStorage_config, StorageConfigTest_P)
+{
+    int ret = oc_storage_config(STORAGE_CONFIG);
+    EXPECT_EQ(0, ret);
+}
+
+TEST(TestStorage_config, StorageConfigTest_N)
+{
+    int ret = oc_storage_config("STORAGE_CONFIG_LONG");
+    ASSERT_EQ(-ENOENT, ret);
+}
+
+TEST_F(TestStorage, StorageReadWrite_P)
+{ 
+    long ret = oc_storage_write(STORAGE_CONFIG, buf, OC_MAX_APP_DATA_SIZE);
+    EXPECT_NE(0, ret);
+    long ret1 = oc_storage_read(STORAGE_CONFIG, buf2, OC_MAX_APP_DATA_SIZE);
+    EXPECT_NE(0, ret1);
+}
+
+TEST_F(TestStorage, StorageReadWrite_N)
+{ 
+    long ret = oc_storage_write(STORAGE_CONFIG_N, buf, OC_MAX_APP_DATA_SIZE);
+    EXPECT_EQ(-EINVAL, ret);
+    long ret1 = oc_storage_read(STORAGE_CONFIG_N, buf2, OC_MAX_APP_DATA_SIZE);
+    EXPECT_EQ(-EINVAL, ret1);
+}
+
+TEST_F(TestStorage, Oc_New_Resource_P)
+{
+    s_pResource = oc_new_resource(NULL, "/a/light", 2, 0);
+    std::cout << s_pResource;
+    EXPECT_NE(0, s_pResource) << "Properly created the new resource";
+}
+
+   TEST_F(TestStorage, New_Collection_Test_P)
+{
+    oc_resource_t *col =  oc_new_collection("roomlights","/lights",1,0);
+    EXPECT_NE(0, col) << "Properly collected the new resource";
+}
---
+++ security/unittest/SecurityTest.cpp
@@ -0,0 +1,129 @@
+/******************************************************************
+ *
+ * Copyright 2018 GRANITE RIVER LABS All Rights Reserved.
+ *
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+#include <cstdlib>
+#include "gtest/gtest.h"
+
+extern "C" {
+    #include "oc_acl.h"
+    #include "oc_api.h"
+    #include "oc_cred.h"
+    #include "oc_doxm.h"
+    #include "config.h"
+    #include "oc_uuid.h"
+    #include "oc_core_res.h"
+    #include "oc_ri.h"
+    #include "oc_pstat.h"
+}
+#define DEVICE_MAX_NUM 2
+#define INVALID_DEVICE_MAX_NUM 3
+#define DEVICE_NUM 1
+#define RESOURCE_URI "/LightResourceURI"
+#define DEVICE_URI "/oic/d"
+#define RESOURCE_TYPE "oic.r.light"
+#define DEVICE_TYPE "oic.d.light"
+#define RESOURCE_INTERFACE "oic.if.baseline"
+#define MANUFACTURER_NAME "Samsung"
+#define DEVICE_NAME "Table Lamp"
+#define OCF_SPEC_VERSION "ocf.1.0.0"
+#define OCF_DATA_MODEL_VERSION "ocf.res.1.0.0"
+#define OC_MAX_NUM_DEVICE 2
+
+class TestAclOperational: public testing::Test
+{
+    protected:
+        virtual void SetUp()
+        {
+            oc_sec_acl_init();
+            oc_init_platform(MANUFACTURER_NAME, NULL, NULL);
+            oc_add_device(DEVICE_URI, DEVICE_TYPE, DEVICE_NAME,
+                          OCF_SPEC_VERSION, OCF_DATA_MODEL_VERSION, NULL, NULL);
+        }
+        virtual void TearDown()
+        {
+        }
+};
+
+TEST_F(TestAclOperational, GetAclTest_P)
+{
+    oc_sec_acl_t *aclist;
+    aclist = oc_sec_get_acl(DEVICE_MAX_NUM);
+    EXPECT_NE(0, aclist) << "Able to get the value from the acl list";
+}
+
+TEST_F(TestAclOperational, GetAclTest_N)
+{
+    oc_sec_acl_t *aclist =oc_sec_get_acl(INVALID_BOUND) ;
+    EXPECT_NE(0, aclist) << "Failed to get the value from the acl list";
+}
+
+TEST_F(TestAclOperational, EncodeAclTest_P)
+{
+    bool errorCode_false =  oc_sec_encode_acl(DEVICE_NUM);
+    EXPECT_EQ(true, errorCode_false) << "Properly Encode the subject of the device";
+}
+
+TEST_F(TestAclOperational, GetCredsTest_P)
+{
+    oc_sec_creds_t *errorCode_false =  oc_sec_get_creds(DEVICE_NUM);
+    std::cout << errorCode_false->rowneruuid.id;
+    EXPECT_NE(0, errorCode_false) << "Properly get the credential";
+}
+
+TEST_F(TestAclOperational, GetDoxmTest_P)
+{
+    oc_sec_doxm_t *errorCode_false =  oc_sec_get_doxm(DEVICE_MAX_NUM);
+    EXPECT_NE(0, errorCode_false) << "Properly get the doxm";
+}
+
+TEST_F(TestAclOperational, Get_Device_Id_Test_P)
+{
+    oc_uuid_t *deviceuuid =  oc_core_get_device_id(DEVICE_NUM);
+    std::cout << "i am in UUID_get_device_id";
+    std::cout << deviceuuid->id;
+    EXPECT_NE(0, deviceuuid->id) << "Properly get the device id ";
+
+}
+
+TEST_F(TestAclOperational, Get_Device_Info_Test_P)
+{
+    oc_device_info_t *deviceinfo =  oc_core_get_device_info(DEVICE_NUM);
+    EXPECT_NE(0, deviceinfo) << "Properly get the device info";
+}
+
+TEST_F(TestAclOperational, Get_Resource_By_Index_Test_P)
+{
+    oc_resource_t *res =  oc_core_get_resource_by_index(OCF_CON,DEVICE_MAX_NUM);
+    EXPECT_NE(0, res) << "Properly get the device index";
+}
+
+TEST_F(TestAclOperational, Init_Platform_P)
+{
+  int _init_add = oc_init_platform("Samsung", NULL, NULL);
+  EXPECT_EQ(0, _init_add);
+}
+
+TEST_F(TestAclOperational, Get_Con_Res_P)
+{
+    bool announce = true;
+    oc_set_con_res_announced(announce);
+    bool get_announce = oc_get_con_res_announced();
+    EXPECT_EQ(true, get_announce);
+  }
