33e6c5c72f4c67356d98d234e25756c627c34061
Hanna Cha <hanna.cha@samsung.com>
2018-06-25 14:35:54 +0900

    Adding St_app_fw Unit tests
    covered part of
    - st_easy_setup.c
    - st_port.c
    - st_process.c
    - st_store.c
    - st_cloud_manager.c
    - st_data_manager.c
    
    Change-Id: Iad461a8a0afdc5f2ba5f4f6dbb07ba78b22d1063
    Signed-off-by: Hanna Cha <hanna.cha@samsung.com>

---
+++ service/st-app-fw/unittest/stmanagertest.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include <cstdlib>
+
+extern "C"{
+    #include "st_manager.h"
+    #include "st_process.h"
+    #include "st_port.h"
+}
+
+static bool otm_confirm_handler_test(void){}
+static void st_status_handler_test(st_status_t status)
+{
+    (void)status;
+}
+
+class TestSTManager: public testing::Test
+{
+    protected:
+        virtual void SetUp()
+        {
+
+        }
+
+        virtual void TearDown()
+        {
+
+        }
+};
+
+TEST_F(TestSTManager, st_manager_initialize)
+{
+    int ret = st_manager_initialize();
+    EXPECT_EQ(0, ret);
+    st_manager_deinitialize();
+}
+
+#ifdef OC_SECURITY
+TEST_F(TestSTManager, st_register_otm_confirm_handler)
+{
+    bool ret = st_register_otm_confirm_handler(otm_confirm_handler_test);
+    EXPECT_EQ(true, ret);
+    st_unregister_otm_confirm_handler();
+}
+#endif
+
+TEST_F(TestSTManager, st_register_status_handler)
+{
+    bool ret = st_register_status_handler(st_status_handler_test);
+    EXPECT_EQ(true, ret);
+    st_unregister_status_handler();
+}
