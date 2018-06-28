e9165025d2d09c5d22046e9626609c1a02b6dedd
Jaehyun Cho <jaehyun3.cho@samsung.com>
2018-06-28 16:17:44 +0900

    Add st_manager TC.
    st_manager_start is blocking function.
    So I made TC using thread for st_manager_start.
    
    Change-Id: I68467f8479fe2c29cf93a318efddc23032ca675c
    Signed-off-by: Jaehyun Cho <jaehyun3.cho@samsung.com>

--- service/st-app-fw/unittest/stmanagertest.cpp
+++ service/st-app-fw/unittest/stmanagertest.cpp
@@ -5,12 +5,24 @@ extern "C"{
     #include "st_manager.h"
     #include "st_process.h"
     #include "st_port.h"
+    void st_manager_quit(void);
 }
 
+#ifdef OC_SECURITY
 static bool otm_confirm_handler_test(void){}
+#endif
 static void st_status_handler_test(st_status_t status)
 {
     (void)status;
+}
+static
+void *st_manager_func(void *data)
+{
+    (void)data;
+    int ret = st_manager_start();
+    EXPECT_EQ(0, ret);
+
+    return NULL;
 }
 
 class TestSTManager: public testing::Test
@@ -31,6 +43,34 @@ TEST_F(TestSTManager, st_manager_initialize)
 {
     int ret = st_manager_initialize();
     EXPECT_EQ(0, ret);
+    st_manager_deinitialize();
+}
+
+TEST_F(TestSTManager, st_manager_start)
+{
+    int ret = st_manager_initialize();
+    EXPECT_EQ(0, ret);
+
+    st_thread_t t = st_thread_create(st_manager_func, "TEST", 0, NULL);
+    st_sleep(1);
+    st_manager_quit();
+    st_thread_destroy(t);
+    st_manager_stop();
+    st_manager_deinitialize();
+}
+
+TEST_F(TestSTManager, st_manager_reset)
+{
+    int ret = st_manager_initialize();
+    EXPECT_EQ(0, ret);
+
+    st_thread_t t = st_thread_create(st_manager_func, "TEST", 0, NULL);
+    st_sleep(1);
+    st_manager_reset();
+    st_sleep(1);
+    st_manager_quit();
+    st_thread_destroy(t);
+    st_manager_stop();
     st_manager_deinitialize();
 }
 
