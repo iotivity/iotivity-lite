51cd3f6fdcafe35b9642eb303f060c61cef6f2cf
Hanna Cha <hanna.cha@samsung.com>
2018-06-28 19:13:03 +0900

    Adding St-app-fw Unit test
    - st_fota_manager.c
    - st_cloud_manager.c
    - st_store.c
    - st_resource_manager.c
    
    - modified Makefile
    - added Copyright message
    
    Change-Id: Ie357ea6a8d7215206c9e9d34899abb389e5b55f4
    Signed-off-by: Hanna Cha <hanna.cha@samsung.com>

--- service/st-app-fw/unittest/stmanagertest.cpp
+++ service/st-app-fw/unittest/stmanagertest.cpp
@@ -1,3 +1,21 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
 #include <gtest/gtest.h>
 #include <cstdlib>
 
@@ -72,6 +90,12 @@ TEST_F(TestSTManager, st_manager_reset)
     st_thread_destroy(t);
     st_manager_stop();
     st_manager_deinitialize();
+
+}
+
+TEST_F(TestSTManager, st_manager_quit)
+{
+    st_manager_quit();
 }
 
 #ifdef OC_SECURITY
---
+++ service/st-app-fw/unittest/stresourcemanagertest.cpp
@@ -0,0 +1,113 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#include <gtest/gtest.h>
+#include <cstdlib>
+
+extern "C"{
+    #include "st_data_manager.h"
+    #include "st_resource_manager.h"
+    #include "oc_api.h"
+    #include "oc_ri.h"
+    #include "st_port.h"
+}
+
+static int device_index = 0;
+static bool
+resource_handler(oc_request_t *request)
+{
+    (void)request;
+    return true;
+}
+
+class TestSTResourceManager: public testing::Test
+{
+    protected:
+        virtual void SetUp()
+        {
+
+        }
+
+        virtual void TearDown()
+        {
+
+        }
+};
+
+TEST_F(TestSTResourceManager, st_register_resources)
+{
+    char *uri = "/capability/switch/main/0";
+    oc_resource_t *resource = NULL;
+    st_data_mgr_info_load();
+    st_register_resources(device_index);
+    resource = oc_ri_get_app_resource_by_uri(uri, strlen(uri), device_index);
+    EXPECT_STREQ(uri, oc_string(resource->uri));
+    st_data_mgr_info_free();
+}
+
+TEST_F(TestSTResourceManager, st_register_resource_handler)
+{
+    st_register_resource_handler(resource_handler, resource_handler);
+    // EXPECT_EQ(0, ret);
+}
+
+TEST_F(TestSTResourceManager, st_register_resource_handler_fail)
+{
+    st_register_resource_handler(NULL, NULL);
+    // EXPECT_EQ(-1, ret);
+}
+
+TEST_F(TestSTResourceManager, st_notify_back)
+{
+    // Given
+    char *uri = "/capability/switch/main/0";
+    oc_resource_t *resource = oc_new_resource(NULL, uri, 1, 0);
+    oc_resource_bind_resource_type(resource, "core.light");
+    oc_add_resource(resource);
+
+    // When
+    st_notify_back(uri);
+    oc_delete_resource(resource);
+
+    // Then
+    // EXPECT_EQ(0, ret);
+}
+
+TEST_F(TestSTResourceManager, st_notify_back_fail_null)
+{
+    // Given
+    char *uri = NULL;
+
+    // When
+    st_notify_back(uri);
+
+    // Then
+    // EXPECT_EQ(-1, ret);
+}
+
+TEST_F(TestSTResourceManager, st_notify_back_fail)
+{
+    // Given
+    char *uri = "/capability/switch/main/1";
+
+    // When
+    st_notify_back(uri);
+
+    // Then
+    // EXPECT_EQ(-1, ret);
+}
