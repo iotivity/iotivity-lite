8459e49e0b81b860ff5eafe5de1fca243f6bbf2d
Hanna Cha <hanna.cha@samsung.com>
2018-06-28 19:13:03 +0900

    Adding St-app-fw Unit test
    - st_fota_manager.c
    - st_cloud_manager.c
    - st_store.c
    - st_resource_manager.c
    
    - modified Makefile
    - added Copyright message
    
    Change-Id: Ie357ea6a8d7215206c9e9d34899abb389e5b55f4
    Signed-off-by: Hanna Cha <hanna.cha@samsung.com>

--- service/st-app-fw/unittest/stmanagertest.cpp
+++ service/st-app-fw/unittest/stmanagertest.cpp
@@ -1,3 +1,21 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
 #include <gtest/gtest.h>
 #include <cstdlib>
 
@@ -5,6 +23,7 @@ extern "C"{
     #include "st_manager.h"
     #include "st_process.h"
     #include "st_port.h"
+    void st_manager_quit(void);
 }
 
 static bool otm_confirm_handler_test(void){}
@@ -32,6 +51,11 @@ TEST_F(TestSTManager, st_manager_initialize)
     int ret = st_manager_initialize();
     EXPECT_EQ(0, ret);
     st_manager_deinitialize();
+}
+
+TEST_F(TestSTManager, st_manager_quit)
+{
+    st_manager_quit();
 }
 
 #ifdef OC_SECURITY
