14446e72de34349886ac7c1b2573e4449199ad54
Jaehyun Cho <jaehyun3.cho@samsung.com>
2018-06-29 18:48:28 +0900

    Add st_manager & st_easy_setup TC
    To test properly, It need to reset storage every TC and
    acl policy need to pass every resource. So I made for that
    kind of function in sttestcommon.h/c
    
    Change-Id: I4a3e357b96bc768e8e0333029f4c50042c9eb965
    Signed-off-by: Jaehyun Cho <jaehyun3.cho@samsung.com>

--- service/st-app-fw/unittest/stmanagertest.cpp
+++ service/st-app-fw/unittest/stmanagertest.cpp
@@ -5,15 +5,24 @@ extern "C"{
     #include "st_manager.h"
     #include "st_process.h"
     #include "st_port.h"
+    #include "sttestcommon.h"
     void st_manager_quit(void);
 }
+
+static st_mutex_t mutex = NULL;
+static st_cond_t cv = NULL;
 
 #ifdef OC_SECURITY
 static bool otm_confirm_handler_test(void){}
 #endif
 static void st_status_handler_test(st_status_t status)
 {
-    (void)status;
+    if (status == ST_STATUS_EASY_SETUP_DONE ||
+        status == ST_STATUS_EASY_SETUP_PROGRESSING) {
+        st_mutex_lock(mutex);
+        st_cond_signal(cv);
+        st_mutex_unlock(mutex);
+    }
 }
 static
 void *st_manager_func(void *data)
@@ -30,12 +39,17 @@ class TestSTManager: public testing::Test
     protected:
         virtual void SetUp()
         {
-
+            mutex = st_mutex_init();
+            cv = st_cond_init();
+            reset_storage();
         }
 
         virtual void TearDown()
         {
-
+            st_cond_destroy(cv);
+            st_mutex_destroy(mutex);
+            cv = NULL;
+            mutex = NULL;
         }
 };
 
@@ -51,8 +65,11 @@ TEST_F(TestSTManager, st_manager_start)
     int ret = st_manager_initialize();
     EXPECT_EQ(0, ret);
 
+    st_register_status_handler(st_status_handler_test);
     st_thread_t t = st_thread_create(st_manager_func, "TEST", 0, NULL);
-    st_sleep(1);
+
+    ret = test_wait_until(mutex, cv, 5);
+    EXPECT_EQ(0, ret);
     st_manager_quit();
     st_thread_destroy(t);
     st_manager_stop();
@@ -64,10 +81,16 @@ TEST_F(TestSTManager, st_manager_reset)
     int ret = st_manager_initialize();
     EXPECT_EQ(0, ret);
 
+    st_register_status_handler(st_status_handler_test);
     st_thread_t t = st_thread_create(st_manager_func, "TEST", 0, NULL);
+    ret = test_wait_until(mutex, cv, 5);
+    EXPECT_EQ(0, ret);
+
     st_sleep(1);
+
     st_manager_reset();
-    st_sleep(1);
+    ret = test_wait_until(mutex, cv, 5);
+    EXPECT_EQ(0, ret);
     st_manager_quit();
     st_thread_destroy(t);
     st_manager_stop();
