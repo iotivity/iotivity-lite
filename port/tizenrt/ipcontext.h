eba85fb2356dfb85baa63f8c551dcb46b890b27d
pilli manikanta <p.manikanta@partner.samsung.com>
2018-07-03 09:22:45 +0530

    [WIP]-TizenRT:Working on stapp and ipadapter
    Change-Id: If4dd23246ecc55b1085f9058cc5cae2c8d7ce15f
    Signed-off-by: pilli manikanta <p.manikanta@partner.samsung.com>

--- port/tizenrt/ipcontext.h
+++ port/tizenrt/ipcontext.h
@@ -19,8 +19,9 @@
 #ifndef IPCONTEXT_H
 #define IPCONTEXT_H
 
-#include <stdint.h>
+#include "oc_endpoint.h"
 #include <pthread.h>
+#include <stdint.h>
 #include <sys/socket.h>
 
 #ifdef OC_TCP
@@ -54,6 +55,7 @@ typedef struct tcp_context_t {
 
 typedef struct ip_context_t {
   struct ip_context_t *next;
+  OC_LIST_STRUCT(eps);
 #ifdef OC_IPV6
   struct sockaddr_storage mcast;
   struct sockaddr_storage server;
