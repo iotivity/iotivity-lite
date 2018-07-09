3da12fa68d005a25330f5f566d2a9d06c5e232dc
iot <rami.jung@samsung.com>
2018-07-09 20:08:28 +0900

    change return values of APIs for app developers
    make  them using st_error_t from st_types.h
    
    Change-Id: Ia58b0373609442cfa88fb1d1011273dd667e60c7
    Signed-off-by: iot <rami.jung@samsung.com>

--- service/st-app-fw/include/st_manager.h
+++ service/st-app-fw/include/st_manager.h
@@ -17,7 +17,9 @@
  ****************************************************************************/
 
 /**
-  @brief ST application framework manager API for managing st-app-fw module.
+  @brief ST application framework manager APIs for managing st-app-fw module.
+    calling sequence is supposed to be
+    st_manager_initialize()->st_manager_start()->st_manager_stop()->st_manager_deinitialize()
   @file
 */
 
@@ -58,60 +60,98 @@ typedef void (*st_status_cb_t)(st_status_t status);
 
 /**
   @brief A function to initialize ST application framework.
-  @return ST_ERROR_NONE if initilaize success or return regarding errors.
+  @return st_error_t An enumeration of possible outcomes.
+  @retval ST_ERROR_NONE if successful.
+  @retval ST_ERROR_STACK_ALREADY_INITIALIZED
+    if st_manager_initialize() is already called.
+  @retval ST_ERROR_STACK_RUNNING
+    if st_manager_initialize() and st_manager_start() are already called.
+  @retval ST_ERROR_OPERATION_FAILED
+    if there is an internal failure while excuting this function.
 */
 st_error_t st_manager_initialize(void);
 
 /**
   @brief A function to start ST application framework. This function will
-     start iotivity-lite stack which include network thread and resources
-     registration. Also it will process Easy Setup and Cloud registration
-     steps. This function will loop until st_status is ST_STATUS_QUIT.
-  @return ST_ERROR_NONE if start success or return regarding errors.
+    start iotivity-lite stack which include network thread and resources
+    registration. Also it will process Easy Setup and Cloud registration
+    steps. This function will loop until st_status is ST_STATUS_QUIT.
+  @return st_error_t An enumeration of possible outcomes.
+  @retval ST_ERROR_NONE if successful.
+  @retval ST_ERROR_STACK_NOT_INITIALIZED
+    st_manager_initialize() isn't executed.
+    this function is supposed to be called right after st_manager_initialize().
+  @retval ST_ERROR_STACK_RUNNING st_manager_start() is already called.
+  @retval ST_ERROR_OPERATION_FAILED
+    if there is an internal failure while excuting this function.
 */
 st_error_t st_manager_start(void);
 
 /**
   @brief A function to reset ST application framework. This function will
-     reset all db files such as security and change into user language
-     instead of internal type. After finish this function, ST application
-     framework will return to ST_STATUS_INIT status.
-  @return ST_ERROR_NONE if reset success or return regarding errors.
+    reset all db files such as security and change into user language
+    instead of internal type. After finish this function, ST application
+    framework will return to ST_STATUS_INIT status.
+  @return st_error_t An enumeration of possible outcomes.
+  @retval ST_ERROR_NONE if successful.
+  @retval ST_ERROR_STACK_NOT_INITIALIZED
+    st_manager_initialize() isn't executed yet.
 */
 st_error_t st_manager_reset(void);
 
 /**
   @brief A function to stop ST application framework. This function will
-     stop iotivity-lite stack internally which includes network thread and
-     resources registration. Also it will stop Easy Setup and Cloud registration
-     steps also. if they are under progress.
-  @return ST_ERROR_NONE if stop success or return regarding errors.
+    stop iotivity-lite stack internally which includes network thread and
+    resources registration. Also it will stop Easy Setup and Cloud registration
+    steps, if they are under progress.
+  @return st_error_t An enumeration of possible outcomes.
+  @retval ST_ERROR_NONE if successful.
+  @retval ST_ERROR_STACK_NOT_INITIALIZED
+    st_manager_initialize() and st_manager_start() aren't executed yet.
+  @retval ST_ERROR_STACK_NOT_STARTED
+  st_manager_start() isn't executed yet.
 */
 st_error_t st_manager_stop(void);
 
 /**
   @brief A function to deinitialize ST application framework.
-  @return ST_ERROR_NONE if de-initialize success or return regarding errors.
+  @return st_error_t An enumeration of possible outcomes.
+  @retval ST_ERROR_NONE if successful.
+  @retval ST_ERROR_STACK_NOT_INITIALIZED
+    st_manager_initialize() isn't executed yet,
+    st_manager_deinitialize() is supposed to be called right after
+    st_manager_initialize() or st_manager_stop().
+  @retval ST_ERROR_STACK_RUNNING
+    status under st_manager_start(),
+    st_manager_deinitialize() is supposed to be called right after
+    st_manager_initialize() or st_manager_stop().
 */
 st_error_t st_manager_deinitialize(void);
 
 /**
-  @brief A function for register registration of the user
+  @brief A function for registration of the user
      confirm handler for OCF OTM(Ownership Transfer Method).
   @param cb Callback function to require OTM confirm.
-  @return Returns true if success.
+  @return bool Description of result.
+  @retval true if successful.
+  @retval false Input parameter is NULL or it is called under unsecure build.
+     unsecure enviroment doesn't support OTM confirm.
+
 */
 bool st_register_otm_confirm_handler(st_otm_confirm_cb_t cb);
 
 /**
-  @brief A function for unregister OTM confirm handler
+  @brief A function for unregistering OTM confirm handler.
 */
 void st_unregister_otm_confirm_handler(void);
 
 /**
-  @brief A function for register ST application framework status handler
+  @brief A function for registering ST application framework status handler
   @param cb Callback function to return the ST application framework status.
-  @return Returns true if success.
+  @return bool Description of result.
+  @retval true if successful.
+  @retval false Input parameter is NULL or it is already registered.
+
 */
 bool st_register_status_handler(st_status_cb_t cb);
 
