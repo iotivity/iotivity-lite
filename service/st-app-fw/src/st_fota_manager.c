5c8e9f3cfdd18abfbac256331488144182b81598
iot <rami.jung@samsung.com>
2018-07-09 20:08:28 +0900

    change return values of APIs for app developers
    Change-Id: Ia58b0373609442cfa88fb1d1011273dd667e60c7
    Signed-off-by: iot <rami.jung@samsung.com>

--- service/st-app-fw/src/st_fota_manager.c
+++ service/st-app-fw/src/st_fota_manager.c
@@ -45,38 +45,47 @@ st_fota_manager_stop()
   fota_deinit();
 }
 
-int
+st_error_t
 st_fota_set_state(fota_state_t state)
 {
-  return fota_set_state(state);
+  return (fota_set_state(state) == 0) ? ST_ERROR_NONE
+                                      : ST_ERROR_OPERATION_FAILED;
 }
 
-int
+st_error_t
 st_fota_set_fw_info(const char *ver, const char *uri)
 {
-  return fota_set_fw_info(ver, uri);
+  return (fota_set_fw_info(ver, uri) == 0) ? ST_ERROR_NONE
+                                           : ST_ERROR_INVALID_PARAMETER;
 }
 
-int
+st_error_t
 st_fota_set_result(fota_result_t result)
 {
-  return fota_set_result(result);
+  return (fota_set_result(result) == 0) ? ST_ERROR_NONE
+                                        : ST_ERROR_INVALID_PARAMETER;
 }
 
-bool
+st_error_t
 st_register_fota_cmd_handler(st_fota_cmd_cb_t cb)
 {
-  if (!cb || g_st_fota_cmd_cb) {
-    st_print_log("Failed to register fota cmd handler\n");
-    return false;
+  if (!cb) {
+    st_print_log("Failed to register fota cmd handler - invalid parameter\n");
+    return ST_ERROR_INVALID_PARAMETER;
+  }
+  if (g_st_fota_cmd_cb) {
+    st_print_log("Failed to register fota cmd handler - already registered\n");
+    return ST_ERROR_OPERATION_FAILED;
   }
 
   g_st_fota_cmd_cb = cb;
-  return true;
+  return ST_ERROR_NONE;
 }
 
-void
+st_error_t
 st_unregister_fota_cmd_handler(void)
 {
   g_st_fota_cmd_cb = NULL;
+  // other return values would be added
+  return ST_ERROR_NONE;
 }
