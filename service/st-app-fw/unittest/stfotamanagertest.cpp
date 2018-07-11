97d8f8bd2eeb6fded37c97b13ebbde85fbf5a05b
Hanna Cha <hanna.cha@samsung.com>
2018-07-11 15:46:24 +0900

    ST_APP_FW Unit test Modifications
    - stporttest.cpp, steasysetuptest.cpp : made dependency to json file
    - stfotamanagertest.cpp : added handler test
    - stresourcemanagertest.cpp : modified some test because of changed return type, modified waitForEvent not to wait from when resource is discovered
    
    Change-Id: I77671edeb0d31e1ce12a91a0d9a9387f656f206c
    Signed-off-by: Hanna Cha <hanna.cha@samsung.com>

--- service/st-app-fw/unittest/stfotamanagertest.cpp
+++ service/st-app-fw/unittest/stfotamanagertest.cpp
@@ -22,6 +22,8 @@
 extern "C"{
     #include "st_fota_manager.h"
     #include "oc_ri.h"
+    #include "oc_api.h"
+    #include "sttestcommon.h"
     extern int st_fota_manager_start(void);
     extern void st_fota_manager_stop(void);
 }
@@ -169,4 +171,145 @@ TEST_F(TestSTFotaManager, st_unregister_fota_cmd_handler)
 
     // Then
     EXPECT_TRUE(ret);
+}
+
+#define OC_RSRVD_FIRMWARE_URI "/firmware"
+#define MAX_WAIT_TIME 10
+#define DEVICE_URI "/oic/d"
+#define DEVICE_TYPE "oic.d.light"
+#define MANUFACTURER_NAME "Samsung"
+#define DEVICE_NAME "Table Lamp"
+#define DEVICE_NUM 0
+#define OCF_SPEC_VERSION "core.1.1.0"
+#define OCF_DATA_MODEL_VERSION "res.1.1.0"
+
+class TestSTFotaManagerHandler: public testing::Test
+{
+    public:
+        static oc_handler_t handler;
+        static pthread_mutex_t mutex;
+        static pthread_cond_t cv;
+        static bool isResourceDiscovered;
+        static bool isCallbackReceived;
+        static oc_endpoint_t *LightEndpoint;
+        static oc_status_t status;
+
+        static oc_discovery_flags_t onResourceDiscovered(const char *di, const char *uri,
+                oc_string_array_t types, oc_interface_mask_t interfaces,
+                oc_endpoint_t *endpoint, oc_resource_properties_t bm, void *user_data)
+        {
+            (void)di;
+            (void)types;
+            (void)interfaces;
+            (void)bm;
+            (void)user_data;
+            std::string discoveredResourceUri = std::string(uri);
+            if (discoveredResourceUri.compare(OC_RSRVD_FIRMWARE_URI) == 0)
+            {
+                PRINT("Switch Resource Discovered...\n");
+                LightEndpoint = endpoint;
+                isResourceDiscovered = true;
+                return OC_STOP_DISCOVERY;
+            }
+
+            oc_free_server_endpoints(endpoint);
+            return OC_CONTINUE_DISCOVERY;
+        }
+
+        static void onPostResponse(oc_client_response_t *data)
+        {
+            EXPECT_EQ(OC_STATUS_CHANGED, data->code); 
+            isCallbackReceived = true;
+        }
+
+        static void waitForEvent(int waitTime)
+        {
+            oc_clock_time_t next_event;
+            (void)next_event;
+            while (waitTime && !isCallbackReceived && !isResourceDiscovered)
+            {
+                PRINT("Waiting for callback....\n");
+                next_event = oc_main_poll();
+                sleep(1);
+                waitTime--;
+            }
+        }
+
+        static int appInit(void)
+        {
+            int result = oc_init_platform(MANUFACTURER_NAME, NULL, NULL);
+            result |= oc_add_device(DEVICE_URI, DEVICE_TYPE, DEVICE_NAME,
+                                    OCF_SPEC_VERSION, OCF_DATA_MODEL_VERSION, NULL, NULL);
+            return result;
+        }
+
+        static void signalEventLoop(void)
+        {
+            pthread_mutex_lock(&mutex);
+            pthread_cond_signal(&cv);
+            pthread_mutex_unlock(&mutex);
+        }
+
+        static void registerResources(void)
+        {
+            st_fota_manager_start();
+        }
+
+    protected:
+        virtual void SetUp()
+        {
+
+        }
+
+        virtual void TearDown()
+        {
+
+        }
+
+        static void SetUpTestCase(){
+            
+            handler.init = appInit;
+            handler.signal_event_loop = signalEventLoop;
+            handler.register_resources = registerResources;
+
+            oc_main_init(&handler);
+            get_wildcard_acl_policy();
+            ASSERT_TRUE(oc_do_ip_discovery(NULL, onResourceDiscovered, NULL)) << "oc_do_ip_discovery() returned failure.";
+
+            waitForEvent(MAX_WAIT_TIME);
+            ASSERT_TRUE(isResourceDiscovered) << " Unable to discover Switch Resource";
+        }
+        
+        static void TearDownTestCase()
+        {
+            st_fota_manager_stop();
+            oc_main_shutdown();
+            reset_storage();
+        }
+};
+
+bool TestSTFotaManagerHandler::isCallbackReceived = false;
+bool TestSTFotaManagerHandler::isResourceDiscovered = false;
+oc_endpoint_t *TestSTFotaManagerHandler::LightEndpoint = nullptr;
+oc_handler_t TestSTFotaManagerHandler::handler;
+pthread_mutex_t TestSTFotaManagerHandler::mutex;
+pthread_cond_t TestSTFotaManagerHandler::cv;
+
+TEST_F(TestSTFotaManagerHandler, fota_cmd_handler)
+{
+    bool init_success, post_success = false;
+    isCallbackReceived = false;
+    isResourceDiscovered = false;
+
+    init_success = oc_init_post(OC_RSRVD_FIRMWARE_URI, LightEndpoint, NULL, onPostResponse, LOW_QOS, NULL);
+    oc_rep_start_root_object();
+    oc_rep_set_text_string(root, update, "Init");
+    oc_rep_end_root_object();
+    post_success = oc_do_post();
+
+    EXPECT_TRUE(init_success);
+    EXPECT_TRUE(post_success);
+
+    waitForEvent(MAX_WAIT_TIME);
+    EXPECT_TRUE(isCallbackReceived);
 }
