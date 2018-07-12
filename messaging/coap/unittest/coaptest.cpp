aa0e74e3bae98a577ed61f6eb426ab3d203a53da
towhid <t.islam@samsung.com>
2018-07-12 17:56:30 +0600

    Added unit TC for messaging and coap
    Added more unit TC to improve coverage report
    
    Change-Id: Ib05e8976208af8aab4d7097fa2abca80da8ac455
    Signed-off-by: towhid <t.islam@samsung.com>

---
+++ messaging/coap/unittest/coaptest.cpp
@@ -0,0 +1,48 @@
+#include <cstdlib>
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "coap.h"
+#include "oc_api.h"
+#include "oc_endpoint.h"
+}
+
+class TestCoap: public testing::Test
+{
+    protected:
+        virtual void SetUp()
+        {
+
+        }
+
+        virtual void TearDown()
+        {
+
+        }
+};
+
+
+TEST_F(TestCoap, CoapGetMidTest_P)
+{
+    uint16_t mid = coap_get_mid();
+    EXPECT_TRUE(mid) << "Failed to get mid transaction";
+}
+
+
+TEST_F(TestCoap, CoapUDPInitMessageTest_P)
+{
+    coap_packet_t ack[1];
+    uint16_t mid = coap_get_mid();
+    coap_udp_init_message(ack, COAP_TYPE_ACK, 0, mid);
+}
+
+TEST_F(TestCoap, CoapSerializeMessageTest_P)
+{
+    coap_packet_t ack[1];
+    uint16_t mid = coap_get_mid();
+    coap_udp_init_message(ack, COAP_TYPE_ACK, 0, mid);
+    oc_message_t *message = oc_internal_allocate_outgoing_message();
+    size_t size = coap_serialize_message(ack, message->data);
+
+    EXPECT_TRUE(size) << "Failed to get mid transaction";
+}
---
+++ messaging/coap/unittest/enginetest.cpp
@@ -0,0 +1,38 @@
+#include <cstdlib>
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "engine.h"
+#include "oc_api.h"
+#include "oc_endpoint.h"
+#include "oc_signal_event_loop.h"
+#define delete pseudo_delete
+#include "oc_core_res.h"
+#undef delete
+}
+
+class TestEngine: public testing::Test
+{
+    protected:
+        virtual void SetUp()
+        {
+
+        }
+
+        virtual void TearDown()
+        {
+
+        }
+};
+
+TEST_F(TestEngine, CoapInitEngineTest_P)
+{
+    coap_init_engine();
+}
+
+TEST_F(TestEngine, CoapReceiveTest_P)
+{
+    coap_init_engine();
+    oc_message_t *message = oc_internal_allocate_outgoing_message();
+    coap_receive(message);
+}
