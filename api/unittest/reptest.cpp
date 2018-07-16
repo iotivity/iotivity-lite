e3fda2b056b3497e7516034c135ca255b5fb1566
Anutosh <anutosh@graniteriverlabs.in>
2018-07-16 17:56:15 +0530

    UTC for rep
    Change-Id: Ia98b59873b65711cbecc71fe146b4d91cf351f2e
    Signed-off-by: Anutosh <anutosh@graniteriverlabs.in>

--- api/unittest/reptest.cpp
+++ api/unittest/reptest.cpp
@@ -23,10 +23,34 @@
 #include "gtest/gtest.h"
 extern "C" {
     #include "oc_rep.h"
+    #include "oc_rep.h"
 }
+#define OC_MAX_APP_DATA_SIZE (2048)
 
 TEST(TestRep, OCRepFinalizeTest_P)
 {
     int repSize = oc_rep_finalize();
     EXPECT_NE(repSize, -1);
 }
+
+TEST(TestRep, RepNewTest_P)
+{
+    uint8_t *buf;    
+    buf = malloc(OC_MAX_APP_DATA_SIZE);
+    oc_rep_new(buf, OC_MAX_APP_DATA_SIZE);
+    EXPECT_NE(buf, NULL);
+}
+
+TEST(TestRep, ParseRepTest_P)
+{
+    uint8_t *buf;
+    oc_rep_t *rep;
+    int ret;
+    int parse;
+
+    buf = malloc(OC_MAX_APP_DATA_SIZE);
+    ret = oc_storage_read("obt_state", buf, OC_MAX_APP_DATA_SIZE);
+    parse = oc_parse_rep(buf, ret, &rep);
+    EXPECT_EQ(parse, 0);
+}
+
