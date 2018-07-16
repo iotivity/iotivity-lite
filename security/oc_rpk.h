b2690ae5b0a8ff9c72c4a933d9d532dfd6aaed99
Aleksey Volkov <a.volkov@samsung.com>
2018-07-16 12:43:29 +0300

    Raw public key API doxygen
    Doxygen config and comments added for
    Raw public key OTM functions
    
    Change-Id: I20ac74c15b23b43bf98ea95e30b6dff7cc80a0bb
    Signed-off-by: Aleksey Volkov <a.volkov@samsung.com>

--- security/oc_rpk.h
+++ security/oc_rpk.h
@@ -21,10 +21,42 @@
 #ifndef OC_RPK_H
 #define OC_RPK_H
 
+/**
+ * Function pointer for getting peer's public key and token for raw public key OTM.
+ * Callback is expected to copy key and token to the pointers provided.
+ *
+ * Function provides pointers to be filled with key and token binary values and their length.
+ *
+ * @param cpubkey pointer to peer's public key
+ * @param cpubkey_len peer's public key length
+ * @param token pointer to security token
+ * @param token_len security token length
+ */
 typedef void (*oc_sec_get_cpubkey_and_token)(uint8_t *cpubkey, int *cpubkey_len, uint8_t *token, int *token_len);
+
+/**
+ * Function to setting RPK peer's public key and token callback from user.
+ *
+ * @param cpubkey_and_token_cb implementation of cpubkey and token callback.
+ */
 void oc_sec_set_cpubkey_and_token_load(oc_sec_get_cpubkey_and_token cpubkey_and_token_cb);
 
+/**
+ * Function pointer for getting peer's public key and token for raw public key OTM.
+ * Callback is expected to copy key buffer to the pointer provided and set the buffer length.
+ *
+ * @param priv_key pointer to own private key buffer
+ * @param priv_key_len own private key buffer length
+ * @param pub_key pointer to own public key buffer
+ * @param pub_key_len own public key buffer length
+ */
 typedef void (*oc_sec_get_own_key)(uint8_t *priv_key, int *priv_key_len, uint8_t *pub_key, int *pub_key_len);
+
+/**
+ * Function to setting RPK own private key callback from user.
+ *
+ * @param ownkey_cb implementation of own key callback.
+ */
 void oc_sec_set_own_key_load(oc_sec_get_own_key ownkey_cb);
 
 #endif /* OC_RPK_H */
