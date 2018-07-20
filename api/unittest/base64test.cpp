5a0f4809a16cd2e57abc7dd5265b48671ab519b7
Mushfiqul Islam Antu <i.mushfiq@samsung.com>
2018-07-20 18:42:04 +0600

    Add UTC for Base64
    6 new UTC added
    
    Change-Id: I31971a1002542faa53d5e774ceb2f1a7f69d9086
    Signed-off-by: Mushfiqul Islam Antu <i.mushfiq@samsung.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/26473
    Tested-by: IoTivity Jenkins <jenkins-daemon@iotivity.org>
    Reviewed-by: Uze Choi <uzchoi@samsung.com>

---
+++ api/unittest/base64test.cpp
@@ -0,0 +1,86 @@
+/******************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+#include <cstdlib>
+#include "gtest/gtest.h"
+
+extern "C" {
+    #include "oc_base64.h"
+}
+
+#define MAX_OUTPUT_BUFFER_LENGTH 1024
+
+TEST(TestBase64, EncodingTest_P)
+{
+    uint8_t inputData[] = "123456";
+    uint8_t outputData[MAX_OUTPUT_BUFFER_LENGTH];
+    int outputLength, inputLength = 6;
+    outputLength = MAX_OUTPUT_BUFFER_LENGTH;
+    int ret = oc_base64_encode(inputData,inputLength, outputData, outputLength);
+    ASSERT_NE(ret, -1);
+    EXPECT_EQ(ret, inputLength * 4 / 3);
+}
+
+TEST(TestBase64, EmptyEncodingTest_N)
+{
+    uint8_t inputData[] = "123456";
+    uint8_t outputData[MAX_OUTPUT_BUFFER_LENGTH];
+    int outputLength, inputLength = 6;
+    outputLength = 0;
+    int ret = oc_base64_encode(inputData,inputLength, outputData, outputLength);
+    ASSERT_EQ(ret, -1);
+}
+
+TEST(TestBase64, EncodingWithPaddingTest_P)
+{
+    uint8_t inputData[] = "1234567";
+    uint8_t outputData[MAX_OUTPUT_BUFFER_LENGTH];
+    int outputLength, inputLength = 7;
+    outputLength = MAX_OUTPUT_BUFFER_LENGTH;
+    int ret = oc_base64_encode(inputData,inputLength, outputData, outputLength);
+    ASSERT_NE(ret, -1);
+    EXPECT_EQ(ret, (inputLength / 3 ) * 4 + 4 * ((inputLength % 3) > 0));
+}
+
+TEST(TestBase64, DecodingTest_P)
+{
+    uint8_t inputData[] = "12345678";
+    int inputLength = 8;
+    int ret = oc_base64_decode(inputData,inputLength);
+    ASSERT_NE(ret, -1);
+    EXPECT_EQ(ret, inputLength * 3 / 4);
+}
+
+TEST(TestBase64, PaddedDecodingTest_P)
+{
+    uint8_t inputData[] = "12345+/=";
+    int inputLength = 8;
+    int ret = oc_base64_decode(inputData,inputLength);
+    ASSERT_NE(ret, -1);
+    EXPECT_EQ(ret, (inputLength * 3 - 3)/ 4);
+}
+
+TEST(TestBase64, InvalidDecodingTest_N)
+{
+    uint8_t inputData[] = "12345@#$";
+    int inputLength = 8;
+    int ret = oc_base64_decode(inputData,inputLength);
+    ASSERT_EQ(ret, -1);
+}
