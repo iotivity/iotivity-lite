680922d502143a59c10c8e9bca1ccdca0435ff52
Jaehong Jo <jaehong.jo@samsung.com>
2018-07-24 08:56:11 +0900

    Adding RD Client TC for normal case
    Change-Id: I918a42b1a0516daf87f5da11657769e90603b270
    Signed-off-by: Jaehong Jo <jaehong.jo@samsung.com>

--- service/resource-directory/client/unittest/rd_client_test.cpp
+++ service/resource-directory/client/unittest/rd_client_test.cpp
@@ -19,20 +19,64 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-  #include "rd_client.h"
+#include "oc_api.h"
+#include "oc_endpoint.h"
+#include "rd_client.h"
 }
 
 class TestRDClient: public testing::Test
 {
-  protected:
-    virtual void SetUp()
+  public:
+    static oc_handler_t s_handler;
+    static oc_endpoint_t s_endpoint;
+
+    static void onPostResponse(oc_client_response_t *data)
+    {
+      (void) data;
+    }
+
+    static int appInit(void)
+    {
+      int result = oc_init_platform("Samsung", NULL, NULL);
+      result |= oc_add_device("/oic/d", "oic.d.light", "Jaehong's Light",
+                              "ocf.1.0.0", "ocf.res.1.0.0", NULL, NULL);
+      return result;
+    }
+
+    static void signalEventLoop(void)
     {
     }
 
-    virtual void TearDown()
+  protected:
+    static void SetUpTestCase()
     {
+      s_handler.init = &appInit;
+      s_handler.signal_event_loop = &signalEventLoop;
+      int ret = oc_main_init(&s_handler);
+      ASSERT_EQ(0, ret);
+
+      oc_string_t ep_str;
+      oc_new_string(&ep_str, "coap://224.0.1.187:5683", 23);
+      oc_string_to_endpoint(&ep_str, &s_endpoint, NULL);
+      oc_free_string(&ep_str);
+    }
+
+    static void TearDownTestCase()
+    {
+      oc_main_shutdown();
     }
 };
+oc_handler_t TestRDClient::s_handler;
+oc_endpoint_t TestRDClient::s_endpoint;
+
+TEST_F(TestRDClient, rd_publish_p)
+{
+  // When
+  bool ret = rd_publish(&s_endpoint, NULL, 0, onPostResponse, LOW_QOS, NULL);
+
+  // Then
+  EXPECT_TRUE(ret);
+}
 
 TEST_F(TestRDClient, rd_publish_f)
 {
@@ -58,6 +102,15 @@ TEST_F(TestRDClient, rd_publish_with_device_id_f)
   EXPECT_FALSE(ret);
 }
 
+TEST_F(TestRDClient, rd_publish_all_p)
+{
+  // When
+  bool ret = rd_publish_all(&s_endpoint, 0, onPostResponse, LOW_QOS, NULL);
+
+  // Then
+  EXPECT_TRUE(ret);
+}
+
 TEST_F(TestRDClient, rd_publish_all_f)
 {
   // Given
@@ -68,6 +121,15 @@ TEST_F(TestRDClient, rd_publish_all_f)
 
   // Then
   EXPECT_FALSE(ret);
+}
+
+TEST_F(TestRDClient, rd_delete_p)
+{
+  // When
+  bool ret = rd_delete(&s_endpoint, NULL, 0, onPostResponse, LOW_QOS, NULL);
+
+  // Then
+  EXPECT_TRUE(ret);
 }
 
 TEST_F(TestRDClient, rd_delete_f)
