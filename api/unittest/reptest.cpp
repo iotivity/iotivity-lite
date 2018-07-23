3d2de8f090828d5c742e019af086eb9a2d555026
Mushfiqul Islam Antu <i.mushfiq@samsung.com>
2018-07-23 20:49:13 +0600

    Add UTC for reptest
    retest lacked some codecovereage, this commit
    adds to code coverage
    
    Change-Id: I31971a1002542faa53d5e774ceb2f1a7f69d9088
    Signed-off-by: Mushfiqul Islam Antu <i.mushfiq@samsung.com>

--- api/unittest/reptest.cpp
+++ api/unittest/reptest.cpp
@@ -30,3 +30,106 @@ TEST(TestRep, OCRepFinalizeTest_P)
     int repSize = oc_rep_finalize();
     EXPECT_NE(repSize, -1);
 }
+
+TEST(TestRep, OCRepGetDoubleTest_N)
+{
+    oc_rep_t rep;
+    char key[] = "speed";
+    double *value = NULL;
+
+    bool isFailure = oc_rep_get_double(&rep, key, value);
+    ASSERT_FALSE(isFailure);
+}
+
+TEST(TestRep, OCRepGetIntArrayTest_N)
+{
+    oc_rep_t rep;
+    char key[] = "speed";
+    int **value = NULL;
+    int *size = NULL;
+
+    bool isFailure = oc_rep_get_int_array(&rep, key, value, size);
+    ASSERT_FALSE(isFailure);
+}
+
+TEST(TestRep, OCRepGetStringArrayTest_N)
+{
+    oc_rep_t rep;
+    char key[] = "speed";
+    int *size = NULL;
+    oc_string_array_t  *value = NULL;
+
+    bool isFailure = oc_rep_get_string_array(&rep, key, value, size);
+    ASSERT_FALSE(isFailure);
+}
+
+TEST(TestRep, OCRepGetDoubleTest_P)
+{
+    oc_rep_t rep;
+    char key[] = "speed";
+    double value = 10;
+    oc_string_t name;
+    name.size = 6;
+    name.ptr = key;
+    rep.name = name;
+    rep.type = OC_REP_DOUBLE;
+    rep.value.double_p = value;
+    double retrievedValue = 0;
+
+    bool isSuccess = oc_rep_get_double(&rep, key, &retrievedValue);
+    ASSERT_TRUE(isSuccess);
+    ASSERT_EQ(value, retrievedValue);
+}
+
+TEST(TestRep, OCRepGetIntArrayTest_P)
+{
+    oc_rep_t rep;
+    char key[] = "speed";
+    int value[] = {1, 2};
+    oc_array_t intArray;
+    int size = 2;    
+    intArray.size = size;
+    intArray.ptr = value;
+    oc_string_t name;
+    name.size = 6;
+    name.ptr = key;
+    rep.name = name;
+    rep.type = OC_REP_INT_ARRAY;
+    rep.value.array = intArray;
+    int **retrievedValue = NULL;
+
+    bool isFailure = oc_rep_get_int_array(&rep, key, retrievedValue, &size);
+    ASSERT_FALSE(isFailure);
+}
+
+TEST(TestRep, OCRepGetObjectTest_P)
+{
+    oc_rep_t rep;
+    char key[] = "speed";
+    oc_string_t name;
+    name.size = 6;
+    name.ptr = key;
+    oc_rep_t  *value;
+    rep.name = name;
+    rep.type = OC_REP_OBJECT;
+    rep.value.object = value;
+
+    bool isSuccess = oc_rep_get_object(&rep, key, &value);
+    ASSERT_TRUE(isSuccess);
+}
+
+TEST(TestRep, OCRepGetObjectArrayTest_P)
+{
+    oc_rep_t rep;
+    char key[] = "speed";
+    oc_string_t name;
+    name.size = 6;
+    name.ptr = key;
+    oc_rep_t  *value;
+    rep.name = name;
+    rep.type = OC_REP_OBJECT_ARRAY;
+    rep.value.object_array = value;
+
+    bool isSuccess = oc_rep_get_object_array(&rep, key, &value);
+    ASSERT_TRUE(isSuccess);
+}
