45634192819286d5f5fa5f7fecdf583d6c762256
agrkush <kush.agrawal@samsung.com>
2018-07-25 20:37:36 +0530

    Added doxygen comments for oc_ri.h
    Formatting changes in sc_easy_setup.h
    
    Change-Id: Ic359587c226b29d5105bea2b377dc87934b136f2
    Signed-off-by: agrkush <kush.agrawal@samsung.com>

--- include/oc_ri.h
+++ include/oc_ri.h
@@ -14,6 +14,11 @@
 // limitations under the License.
 */
 
+/**
+ *@brief APIs of Iotivity-constrained for resource interface.
+ *@file
+ */
+
 #ifndef OC_RI_H
 #define OC_RI_H
 
@@ -25,6 +30,9 @@
 
 typedef enum { OC_GET = 1, OC_POST, OC_PUT, OC_DELETE } oc_method_t;
 
+/**
+ *@brief Types of resource properties.
+ */
 typedef enum {
   OC_DISCOVERABLE = (1 << 0),
   OC_OBSERVABLE = (1 << 1),
@@ -32,6 +40,9 @@ typedef enum {
   OC_PERIODIC = (1 << 6),
 } oc_resource_properties_t;
 
+/**
+ *@brief Types of status codes.
+ */
 typedef enum {
   OC_STATUS_OK = 0,
   OC_STATUS_CREATED,
@@ -61,12 +72,18 @@ typedef struct oc_separate_response_s oc_separate_response_t;
 
 typedef struct oc_response_buffer_s oc_response_buffer_t;
 
+/**
+ *@brief Structure to store oc respnse details.
+ */
 typedef struct
 {
   oc_separate_response_t *separate_response;
   oc_response_buffer_t *response_buffer;
 } oc_response_t;
 
+/**
+ *@brief Masked value of interfaces.
+ */
 typedef enum {
   OC_IF_BASELINE = 1 << 1,
   OC_IF_LL = 1 << 2,
@@ -77,6 +94,9 @@ typedef enum {
   OC_IF_S = 1 << 7,
 } oc_interface_mask_t;
 
+/**
+ *@brief Types of core resources.
+ */
 typedef enum {
   OCF_P = 0,
   OCF_RES,
@@ -96,6 +116,9 @@ typedef enum {
 
 typedef struct oc_resource_s oc_resource_t;
 
+/**
+ *@brief Structure to store oc request details.
+ */
 typedef struct
 {
   oc_endpoint_t *origin;
@@ -106,15 +129,27 @@ typedef struct
   oc_response_t *response;
 } oc_request_t;
 
+/**
+ *A function pointer for registering a callback to handle request
+ *@param oc_request_t oc_request structure.
+ *@param oc_interface_mask_t oc_interface_mask  structure.
+ *@param user data.
+ */
 typedef void (*oc_request_callback_t)(oc_request_t *, oc_interface_mask_t,
                                       void *);
 
+/**
+ *@brief Structure to store oc request handler details.
+ */
 typedef struct oc_request_handler_s
 {
   oc_request_callback_t cb;
   void *user_data;
 } oc_request_handler_t;
 
+/**
+ *@brief Structure to store OC Resource details.
+ */
 struct oc_resource_s
 {
   struct oc_resource_s *next;
@@ -136,6 +171,9 @@ struct oc_resource_s
 typedef struct oc_link_s oc_link_t;
 typedef struct oc_collection_s oc_collection_t;
 
+/**
+ *@brief Types of return value of event callback.
+ */
 typedef enum {
   OC_EVENT_DONE = 0,
   OC_EVENT_CONTINUE
@@ -143,6 +181,9 @@ typedef enum {
 
 typedef oc_event_callback_retval_t (*oc_trigger_t)(void *);
 
+/**
+ *@brief Structure to store event callback data.
+ */
 typedef struct oc_event_callback_s
 {
   struct oc_event_callback_s *next;
@@ -151,14 +192,32 @@ typedef struct oc_event_callback_s
   void *data;
 } oc_event_callback_t;
 
+/**
+ *@brief A function to initialize resource interface.
+ */
 void oc_ri_init(void);
 
+/**
+ *@brief A function to deinitialize resource interface.
+ */
 void oc_ri_shutdown(void);
 
+/**
+ *@brief A function to add callback to be trigerred after particular interval.
+ *@param cb_data data to be passed to callback.
+ *@param event_callback callback function.
+ *@param ticks time interval.
+ */
 void oc_ri_add_timed_event_callback_ticks(void *cb_data,
                                           oc_trigger_t event_callback,
                                           oc_clock_time_t ticks);
 
+/**
+ *@brief A macro to add callback to be trigerred after particular interval.
+ *@param cb_data data to be passed to callback.
+ *@param event_callback callback function.
+ *@param ticks time interval.
+ */
 #define oc_ri_add_timed_event_callback_seconds(cb_data, event_callback,        \
                                                seconds)                        \
   do {                                                                         \
@@ -167,19 +226,61 @@ void oc_ri_add_timed_event_callback_ticks(void *cb_data,
       (oc_clock_time_t)seconds *(oc_clock_time_t)OC_CLOCK_SECOND);             \
   } while (0)
 
+/**
+ *@brief A function to delete event callback.
+ *@param cb_data data to be passed to callback.
+ *@param event_callback callback function.
+ */
 void oc_ri_remove_timed_event_callback(void *cb_data,
                                        oc_trigger_t event_callback);
 
+/**
+ *@brief A function to fetch coap status code.
+ *@param key oc_status code.
+ *@return int coap status code.
+ */
 int oc_status_code(oc_status_t key);
 
+/**
+ *@brief A function to fetch resource with help of uri.
+ *@param uri uri of resource.
+ *@param uri_len length of uri.
+ *@param device device.
+ *@return resource OC Resource.
+ */
 oc_resource_t *oc_ri_get_app_resource_by_uri(const char *uri, int uri_len,
                                              int device);
 
+/**
+ *@brief A function to fetch list of resources present in app.
+ *@return resource OC Resource.
+ */
 oc_resource_t *oc_ri_get_app_resources(void);
 
 #ifdef OC_SERVER
+
+/**
+ *@brief A function to allocate memory for particular resource.
+ *@return resource OC Resource.
+ */
 oc_resource_t *oc_ri_alloc_resource(void);
+
+/**
+ *@brief A function to add resource to list of app resources.
+ *@param resource OC Resource.
+ *@return bool Result of add operation.
+ *@retval true if add is successful.
+ *@retval false if any input parameter is NULL.
+ */
 bool oc_ri_add_resource(oc_resource_t *resource);
+
+/**
+ *@brief A function to delete resource from app resources list.
+ *@param resource OC Resource.
+ *@return bool Result of delete operation.
+ *@retval true if delete is successful.
+ *@retval false if any input parameter is NULL.
+ */
 bool oc_ri_delete_resource(oc_resource_t *resource);
 
 #ifdef OC_MAX_NUM_COLLECTIONS
@@ -187,14 +288,44 @@ bool oc_ri_delete_resource(oc_resource_t *resource);
 #endif /* OC_MAX_NUM_COLLECTIONS */
 #endif /* OC_SERVER */
 
+/**
+ *@brief A function to free OC Resource properties.
+ *@param resource OC Resource.
+ */
 void oc_ri_free_resource_properties(oc_resource_t *resource);
 
+/**
+ *@brief A function to fetch key value pair in query at particular index.
+ *@param query query.
+ *@param query_len query length.
+ *@param key variable to store key.
+ *@param key_len variable to store key length.
+ *@param value variable to store value related to key.
+ *@param value_len variable to store value length.
+ *@param n index to be fetched.
+ *@return int  next index in query..
+ */
 int oc_ri_get_query_nth_key_value(const char *query, int query_len, char **key,
                                   int *key_len, char **value, int *value_len,
                                   int n);
+
+/**
+ *@brief A function to fetch the value of particular key related to query.
+ *@param query query.
+ *@param query_len query length.
+ *@param key key for which value is required.
+ *@param value variable to store value related to key.
+ *@return int value length.
+ */
 int oc_ri_get_query_value(const char *query, int query_len, const char *key,
                           char **value);
 
+/**
+ *@brief A function to get the masked value of particular interface.
+ *@param iface interface for which mask is required.
+ *@param if_len interface length
+ *@return oc_interface_mask_t masked value of the interface.
+ */
 oc_interface_mask_t oc_ri_get_interface_mask(char *iface, int if_len);
 
 #endif /* OC_RI_H */
--- service/easy-setup/enrollee/include/samsung/sc_easysetup.h
+++ service/easy-setup/enrollee/include/samsung/sc_easysetup.h
@@ -27,7 +27,7 @@
 #include "es_common.h"
 
 /**
- * @brief  WIFI discovery channel init value.
+ *@brief  WIFI discovery channel init value.
  */
 #define WIFI_DISCOVERY_CHANNEL_INIT -1
 
@@ -42,8 +42,8 @@ typedef enum {
 } sc_networkstate_t;
 
 /**
-  @brief Structure to store samsung specific wifi configuration properties.
-*/
+ *@brief Structure to store samsung specific wifi configuration properties.
+ */
 typedef struct
 {
   /**< Wi-Fi AP Channel used for fast discovery **/
@@ -53,8 +53,8 @@ typedef struct
 } sc_wifi_conf_properties;
 
 /**
-  @brief Structure to store terms and conditions information.
-*/
+ *@brief Structure to store terms and conditions information.
+ */
 typedef struct
 {
   /**< Terms & Conditions header **/
@@ -64,8 +64,8 @@ typedef struct
 } sc_tnc_info;
 
 /**
-  @brief Structure to store samsung specific device configuration properties.
-*/
+ *@brief Structure to store samsung specific device configuration properties.
+ */
 typedef struct
 {
   /**< Samsung-specific location-related information **/
@@ -93,8 +93,8 @@ typedef struct
 } sc_dev_conf_properties;
 
 /**
-  @brief Structure to store samsung specific cloud configuration properties.
-*/
+ *@brief Structure to store samsung specific cloud configuration properties.
+ */
 typedef struct
 {
   /**< Samsung-specific clientId for sign-up to IoT Cloud **/
@@ -116,8 +116,8 @@ typedef struct
 } sc_cloud_server_conf_properties;
 
 /**
-  @brief Structure to store samsung specific properties.
-*/
+ *@brief Structure to store samsung specific properties.
+ */
 typedef struct
 {
   /**< A state of network connection **/
@@ -180,103 +180,103 @@ typedef struct
 } sc_properties;
 
 /**
-  @brief A macro to set network connection state for samsung specific properties.
-  @param state Network Connection State
-*/
+ *@brief A macro to set network connection state for samsung specific properties.
+ *@param state Network Connection State
+ */
 #define sc_prop_set_net_conn_state(sc_prop, state)                             \
   sc_prop.net_conn_state = state
 /**
-  @brief A macro to set registered device for samsung specific properties.
-  @param reg_set_dev Information of registered device
-*/
+ *@brief A macro to set registered device for samsung specific properties.
+ *@param reg_set_dev Information of registered device
+ */
 #define sc_prop_set_reg_device(sc_prop, reg_set_dev)                           \
   oc_new_string(&sc_prop.reg_set_dev, reg_set_dev)
 /**
-  @brief A macro to set network provisioning information for samsung specific properties.
-  @param net_prov_info Network Provisioning Information
-*/
+ *@brief A macro to set network provisioning information for samsung specific properties.
+ *@param net_prov_info Network Provisioning Information
+ */
 #define sc_prop_set_net_prov_info(sc_prop, net_prov_info)                      \
   oc_new_string(&sc_prop.net_prov_info, net_prov_info)
 /**
-  @brief A macro to set terms and conditions information for samsung specific properties.
-  @param tnc_info Terms and conditions information
-*/
+ *@brief A macro to set terms and conditions information for samsung specific properties.
+ *@param tnc_info Terms and conditions information
+ */
 #define sc_prop_set_tnc_info(sc_prop, tnc_info) sc_prop.tnc_info = tnc_info
 /**
-  @brief A macro to set terms and conditions status for samsung specific properties.
-  @param state Status of terms and conditions information which is given to mediator.
-*/
+ *@brief A macro to set terms and conditions status for samsung specific properties.
+ *@param state Status of terms and conditions information which is given to mediator.
+ */
 #define sc_prop_set_tnc_status(sc_prop, tnc_status)                            \
   sc_prop.tnc_status = tnc_status
 /**
-  @brief A macro to set pnp pin for samsung specific properties.
-  @param state Network Connection State
-*/
+ *@brief A macro to set pnp pin for samsung specific properties.
+ *@param state Network Connection State
+ */
 #define sc_prop_set_pnp_pin(sc_prop, pnp_pin)                                  \
   oc_new_string(&sc_prop.pnp_pin, pnp_pin)
 /**
-  @brief A macro to set easy setup protocol version for samsung specific properties.
-  @param es_protocol_ver Easy Setup protocol version.
-*/
+ *@brief A macro to set easy setup protocol version for samsung specific properties.
+ *@param es_protocol_ver Easy Setup protocol version.
+ */
 #define sc_prop_set_es_protocol_ver(sc_prop, es_protocol_ver)                  \
   oc_new_string(&sc_prop.es_protocol_ver, es_protocol_ver)
 
 // user data callbacks to set to easysetup
 /**
-  @brief A callback funtion for parsing samsung specific user properties from POST request.
- * @param payload Represents a received POST request. If you know user-specific
- * property key,
- * then you can extract a corresponding value if it exists.
- * @param resource_type Used to distinguish which resource the received property
- * belongs to
- * @param user_data User-specific data you want to deliver to desired users,
- * i.e.application.
-*/
+ *@brief A callback funtion for parsing samsung specific user properties from POST request.
+ *@param payload Represents a received POST request. If you know user-specific
+ *property key,
+ *then you can extract a corresponding value if it exists.
+ *@param resource_type Used to distinguish which resource the received property
+ *belongs to
+ *@param user_data User-specific data you want to deliver to desired users,
+ *i.e.application.
+ */
 void sc_read_userdata_cb(oc_rep_t *payload, char *resource_type,
                          void **user_data);
 /**
-  @brief A callback funtion for putting samsung specific user properties to a response to be sent.
- * @param payload Represents a response. You can set a specific value with
- * specific property key
- * to the payload. If a client receives the response and know the property key,
- * then it can
- * extract the value.
- * @param resource_type Used to distinguish which resource the received property
- * belongs to.
-*/
+ *@brief A callback funtion for putting samsung specific user properties to a response to be sent.
+ *@param payload Represents a response. You can set a specific value with
+ *specific property key
+ *to the payload. If a client receives the response and know the property key,
+ *then it can
+ *extract the value.
+ *@param resource_type Used to distinguish which resource the received property
+ *belongs to.
+ */
 void sc_write_userdata_cb(oc_rep_t *payload, char *resource_type);
 /**
-  @brief A callback funtion for freeing allocated memory of samsung specific user data in
-  * s_wifi_conf_data, es_dev_conf_data and es_coap_cloud_conf_data.
- * @param user_data User-specific data free up it's memory.
- * @param resource_type Used to distinguish which resource user data
- * beongs to.
-*/
+ *@brief A callback funtion for freeing allocated memory of samsung specific user data in
+ *s_wifi_conf_data, es_dev_conf_data and es_coap_cloud_conf_data.
+ *@param user_data User-specific data free up it's memory.
+ *@param resource_type Used to distinguish which resource user data
+ *beongs to.
+ */
 void sc_free_userdata(void *user_data, char *resource_type);
 
 /**
-  @brief A function to get samsung specific properties.
-  @return sc_properties
-*/
+ *@brief A function to get samsung specific properties.
+ *@return sc_properties
+ */
 sc_properties *get_sc_properties(void);
 /**
-  @brief A function to set samsung specific properties.
-  @param prop samsung specfic properties structure.
-  @return es_result_e Result of set operation.
-  @retval ES_OK if set is successful.
-  @retval ES_ERROR if input parameter is NULL.
-*/
+ *@brief A function to set samsung specific properties.
+ *@param prop samsung specfic properties structure.
+ *@return es_result_e Result of set operation.
+ *@retval ES_OK if set is successful.
+ *@retval ES_ERROR if input parameter is NULL.
+ */
 es_result_e set_sc_properties(sc_properties *prop);
 /**
-  @brief A function reset samsung specific properties.
-  @return es_result_e Result of reset operation.
-  @retval ES_OK if reset is successful.
-*/
+ *@brief A function reset samsung specific properties.
+ *@return es_result_e Result of reset operation.
+ *@retval ES_OK if reset is successful.
+ */
 es_result_e reset_sc_properties(void);
 
 /**
-  @brief Struture to store provisioning information targets.
-*/
+ *@brief Struture to store provisioning information targets.
+ */
 typedef struct
 {
   oc_string_t target_di;
@@ -285,8 +285,8 @@ typedef struct
 } sec_provisioning_info_targets;
 
 /**
-  @brief The "/sec/provisioninginfo" resource data structure node.
-*/
+ *@brief The "/sec/provisioninginfo" resource data structure node.
+ */
 typedef struct
 {
   sec_provisioning_info_targets *targets;
@@ -296,31 +296,31 @@ typedef struct
 } sec_provisioning_info;
 
 /**
-  @brief A function to initialize samsung specific provisioning info resource.
-  @param prov_info structure to store provisioning info resource.
-  @return es_result_e Result of init operation.
-  @retval ES_OK if initialization is successful.
-  @retval ES_ERROR if memory allocation failed for resource.
-*/
+ *@brief A function to initialize samsung specific provisioning info resource.
+ *@param prov_info structure to store provisioning info resource.
+ *@return es_result_e Result of init operation.
+ *@retval ES_OK if initialization is successful.
+ *@retval ES_ERROR if memory allocation failed for resource.
+ */
 es_result_e init_provisioning_info_resource(sec_provisioning_info *prov_info);
 /**
-  @brief A function to set properties for provisioning info resource.
-  @param prov_info provisioning info resource.
-  @return es_result_e Result of set operation.
-  @retval ES_OK if set is successful.
-  @retval ES_ERROR if input parameter is NULL.
-*/
+ *@brief A function to set properties for provisioning info resource.
+ *@param prov_info provisioning info resource.
+ *@return es_result_e Result of set operation.
+ *@retval ES_OK if set is successful.
+ *@retval ES_ERROR if input parameter is NULL.
+ */
 es_result_e set_sec_prov_info(sec_provisioning_info *prov_info);
 /**
-  @brief A function to deinitialize samsung specific provisioning info resource.
-  @return es_result_e Result of deinit operation.
-  @retval ES_OK if deinit is successful.
-*/
+ *@brief A function to deinitialize samsung specific provisioning info resource.
+ *@return es_result_e Result of deinit operation.
+ *@retval ES_OK if deinit is successful.
+ */
 es_result_e deinit_provisioning_info_resource(void);
 
 /**
-  @brief The "/sec/accesspointlist" resource data structure node.
-*/
+ *@brief The "/sec/accesspointlist" resource data structure node.
+ */
 typedef struct sec_accesspoint_s
 {
   oc_string_t channel;
@@ -334,24 +334,24 @@ typedef struct sec_accesspoint_s
 } sec_accesspoint;
 
 /**
- * A function pointer for registering a callback to fetch
-    access point scanned list information.
+ *A function pointer for registering a callback to fetch
+ *access point scanned list information.
  * @param ap_list accesspoint resource structure.
  */
 typedef void (*get_ap_scan_list)(sec_accesspoint **ap_list);
 
 /**
-  @brief A function to initialize samsung specific accesspointlist resource
-  @param prop .
-  @return es_result_e Result of get operation.
-  @retval ES_OK if set is successful.
-  @retval ES_ERROR if input parameter is NULL
+ *@brief A function to initialize samsung specific accesspointlist resource
+ *@param prop .
+ *@return es_result_e Result of get operation.
+ *@retval ES_OK if set is successful.
+ *@retval ES_ERROR if input parameter is NULL
 */
 es_result_e init_accesspointlist_resource(get_ap_scan_list cb);
 /**
-  @brief A function to deinitialize samsung specific accesspointlist resource
-  @return es_result_e Result of deinit operation.
-  @retval ES_OK if deinit is successful..
-*/
+ *@brief A function to deinitialize samsung specific accesspointlist resource
+ *@return es_result_e Result of deinit operation.
+ *@retval ES_OK if deinit is successful..
+ */
 es_result_e deinit_accesspointlist_resource(void);
 #endif /* SC_EASYSETUP_H */
