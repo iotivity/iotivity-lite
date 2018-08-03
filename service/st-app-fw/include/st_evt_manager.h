4fb90101e6e28af7e7465a4d345dd664555bdfc2
iot <rami.jung@samsung.com>
2018-08-03 14:17:12 +0900

    Apply State based transaction
    It allows to work  as based on state & event transaction
    with build option STATE_MODEL=1
    
    Change-Id: I7adce94c9c058c14128ceb4fa486b3e28d79f165
    Signed-off-by: Hanna Cha <hanna.cha@samsung.com>
    Signed-off-by: iot <rami.jung@samsung.com>

--- service/st-app-fw/include/st_evt_manager.h
+++ service/st-app-fw/include/st_evt_manager.h
@@ -15,19 +15,31 @@
  * language governing permissions and limitations under the License.
  *
  ****************************************************************************/
+#ifndef ST_EVT_MANAGER_H
+#define ST_EVT_MANAGER_H
 
-#ifndef ST_PROCESS_H
-#define ST_PROCESS_H
+#include "st_types.h"
+#include <stdbool.h>
 
-#include "st_port.h"
+typedef enum {
+  ST_EVT_INIT = 0,
+  ST_EVT_START,  // developer
+  ST_EVT_STOP,   // developer
+  ST_EVT_DEINIT, // developer
+  ST_EVT_RUN,
+  ST_EVT_START_EASYSETUP, // no more needed
+  ST_EVT_START_WIFI_CONNECT,
+  ST_EVT_RETRY_WIFI_CONNECT,
+  ST_EVT_START_CLOUDMANAGER,
+  ST_EVT_RESET, // developer
+  ST_EVT_MAX
+} st_evt;
 
-int st_process_init(void);
-int st_process_start(void);
-int st_process_stop(void);
-int st_process_destroy(void);
+void st_evt_init(void);
+void st_evt_deinit(void);
 
-void st_process_signal(void);
-void st_process_app_sync_lock(void);
-void st_process_app_sync_unlock(void);
+bool st_evt_is_in_queue(void);
+st_evt st_evt_pop(void);
+void st_evt_push(st_evt evt);
 
-#endif /* ST_PROCESS_H */
+#endif
