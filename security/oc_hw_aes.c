a11bdf11947612cb2177f5ede941a7b3150691d8
Oleksii Beketov <ol.beketov@samsung.com>
2018-08-06 19:12:58 +0300

    Enable hardware AES
    Use hardware AES implementation (draft)
    
    Change-Id: Iab65d3479c999cddd30563e47b694a48c4f4284a
    Signed-off-by: Oleksii Beketov <ol.beketov@samsung.com>

---
+++ security/oc_hw_aes.c
@@ -0,0 +1,49 @@
+#include "mbedtls/ctr_drbg.h"
+#include "dna_crypto.h"
+
+#ifdef ST_MBEDTLS_HW_AES
+#define MBEDTLS_AES_SETKEY_ENC_ALT
+#define MBEDTLS_AES_SETKEY_DEC_ALT
+#define MBEDTLS_AES_ENCRYPT_ALT
+#define MBEDTLS_AES_DECRYPT_ALT
+#endif ST_MBEDTLS_HW_AES
+
+#ifdef MBEDTLS_AES_SETKEY_ENC_ALT
+int mbedtls_aes_setkey_enc( mbedtls_aes_context *ctx, const unsigned char *key,
+                    unsigned int keybits )
+{
+    ctx->rk = ctx->buf;
+    memcpy (ctx->rk, key, 16);
+    return 0;
+}
+#endif /* MBEDTLS_AES_SETKEY_ENC_ALT */
+
+#ifdef MBEDTLS_AES_SETKEY_DEC_ALT
+int mbedtls_aes_setkey_dec( mbedtls_aes_context *ctx, const unsigned char *key,
+                    unsigned int keybits )
+{
+    ctx->rk = ctx->buf;
+    memcpy (ctx->rk, key, 16);
+    return 0;
+}
+#endif /* MBEDTLS_AES_SETKEY_DEC_ALT */
+
+#ifdef MBEDTLS_AES_ENCRYPT_ALT
+int mbedtls_internal_aes_encrypt( mbedtls_aes_context *ctx,
+                                  const unsigned char input[16],
+                                  unsigned char output[16] )
+{
+    dna_aes_encrypt(input, 16, NULL, ctx->rk, DNA_AES_KEY_128BIT, DNA_NO_PADDING, output);
+    return 0;
+}
+#endif /* MBEDTLS_AES_ENCRYPT_ALT */
+
+#ifdef MBEDTLS_AES_DECRYPT_ALT
+int mbedtls_internal_aes_decrypt( mbedtls_aes_context *ctx,
+                                  const unsigned char input[16],
+                                  unsigned char output[16] )
+{
+    dna_aes_decrypt(input, 16, NULL, ctx->rk, DNA_AES_KEY_128BIT, DNA_NO_PADDING, output);
+    return 0;
+}
+#endif /* MBEDTLS_AES_DECRYPT_ALT */
