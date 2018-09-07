a2ebc4dba4c20304244f01d6bbd12f789e20530c
Anutosh <anutosh@graniteriverlabs.in>
2018-09-07 13:10:29 +0530

    Certification APP
    Change-Id: I2ac187aae5f440d686376cacdf9550134d9c39ad
    Signed-off-by: Anutosh <anutosh@graniteriverlabs.in>

---
+++ include/Certification.h
@@ -0,0 +1,26 @@
+#define RESOURCE_NAME "lightbulb"
+#define RESOURCE_URI "/light/"
+#define NUMRESOURCESTYPES 1
+#define DEVICE 0
+#define RESOURCE_LIGHT_TYPE "core.light"
+#define MAX_LIGHT_RESOURCE_COUNT 4
+#define NDEVICE 1
+#define MAX_STRING 65
+#define FAN_INVISIBLE_URI "/device/fan-invisible"
+#define MAX_URI_LENGTH (30)
+
+extern char g_3DPrinter_RESOURCE_ENDPOINT[];
+extern char g_AudioControls_RESOURCE_ENDPOINT[];
+extern int g_AudioControls_nr_resource_types;
+extern int quit;
+pthread_mutex_t mutex;
+pthread_cond_t cv;
+struct timespec ts;
+extern int g_3DPrinter_nr_resource_types;
+
+int convert_if_string(char *interface_name);
+void register_resources();
+void signal_event_loop(void);
+void signal_event_loop(void);
+void handle_signal(int signal);
+int app_init(void);
