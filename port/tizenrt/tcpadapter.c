f41680950d425c10b9d15cd81bfab760029803b8
Chan-yeol Park <chanyeol.park@samsung.com>
2018-09-20 20:24:46 +0900

    Add TCP receive timeout to avoid infinte loop.
    iotivity-constrained reads TCP body message based on the length in
    tcp header but this could not be matched rarely so we could face
    unexpected infinite wait to complete reading the whole body message.
    
    Change-Id: I526ff30446c6ff8d287511797b0bc4ec084cac14
    Signed-off-by: Chan-yeol Park <chanyeol.park@samsung.com>

--- port/tizenrt/tcpadapter.c
+++ port/tizenrt/tcpadapter.c
@@ -352,6 +352,13 @@ oc_tcp_receive_message(ip_context_t *dev, fd_set *fds, oc_message_t *message)
     ret_with_code(TCP_STATUS_NONE);
   }
 
+  // Set RECV TIMEOUT
+  struct timeval timeout;
+  timeout.tv_sec = 30;
+  timeout.tv_usec = 0;
+
+  setsockopt(session->sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+
   // receive message.
   size_t total_length = 0;
   size_t want_read = DEFAULT_RECEIVE_SIZE;
