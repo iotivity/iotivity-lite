d66e79397aa7c18d40600729baf988fd42e40885
George Nash <george.nash@intel.com>
2018-09-27 14:59:01 -0700

    Enable passing user data through callbacks
    This commit does two things. It enables passing the
    a generic object when a call back is set. Tested using
    the client/server samples. The test code was not part
    of this commit.
    
    It also adds the callback struct to a map based
    on the jobject callback class. This way if a callback
    is un-registered we can use the map to release
    global references and free memory. So far i have
    not implemented any un-register calls so that
    is still not fully tested.
    
    A lot of small name changes.
    
    Change-Id: I967f9f41035e74d94affe0c1ede4c6771e65db60
    Signed-off-by: George Nash <george.nash@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/27081
    Tested-by: IoTivity Jenkins <jenkins-daemon@iotivity.org>
    Reviewed-by: Larry Sachs <larry.j.sachs@intel.com>
    Reviewed-by: Rick Bell <richard.s.bell@intel.com>

--- swig/apps/java_lite_simple_server/src/java_lite_simple_server/PutLight.java
+++ swig/apps/java_lite_simple_server/src/java_lite_simple_server/PutLight.java
@@ -6,9 +6,8 @@ import org.iotivity.RequestHandler;
 public class PutLight implements RequestHandler {
 
     @Override
-    public void handler(OCRequest request, int interfaces, Object user_data) {
+    public void handler(OCRequest request, int interfaces, Object userData) {
         System.out.println("Inside the PutLight RequestHandler");
-        new PostLight().handler(request, interfaces, user_data);
+        new PostLight().handler(request, interfaces, userData);
     }
-
 }
