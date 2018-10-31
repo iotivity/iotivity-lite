c429ad90b9494984305cddc4834f9067a6b5a4fe
Larry Sachs <larry.j.sachs@intel.com>
2018-10-31 13:09:42 -0700

    Build clock wrapper for android clock
    The build_swig script has been modified to take a command line
    argument allowing for oc_clock_android.i to be used to build
    the clock wrapper for android.
    
    ./build_swig.sh android
    
    Change-Id: Iab52c0ad9c1fee6cbc9c7f9ef7d063e7353c4898
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

--- swig/java_lang/build_swig.sh
+++ swig/java_lang/build_swig.sh
@@ -10,6 +10,10 @@ if [ "$#" -ge 1 ] && [ "$1" = "linux" ]
 then
   echo Building wrapper for linux clock
   swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../../port/ -o oc_clock_wrap.c ../swig_interfaces/oc_clock_linux.i
+elif [ "$#" -ge 1 ] && [ "$1" = "android" ]
+then
+  echo Building wrapper for android clock
+  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../../port/ -o oc_clock_wrap.c ../swig_interfaces/oc_clock_android.i
 else
   echo Building wrapper for windows clock
   swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../../port/ ../swig_interfaces/oc_clock.i
---
+++ swig/swig_interfaces/oc_clock_android.i
@@ -0,0 +1,14 @@
+/* File oc_clock.i */
+%module OCClock
+%include "stdint.i"
+#define OC_DYNAMIC_ALLOCATION
+%include "../../port/android/config.h"
+%{
+#include "../../port/oc_clock.h"
+%}
+
+%rename(clockInit) oc_clock_init;
+%rename(clockTime) oc_clock_time;
+%rename(clockSeconds) oc_clock_seconds;
+%rename(clockWait) oc_clock_wait;
+%include "../../port/oc_clock.h"
