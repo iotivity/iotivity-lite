22095af95ad99284c3156937ced6639d67b2fb5b
Larry Sachs <larry.j.sachs@intel.com>
2018-10-31 10:39:16 -0700

    Fixes for Android build
    Change-Id: Ib7c5ab74ab957b082ec55b403ca5a443adc9b2b8
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

--- swig/java_lang/build-iotivity-lite.sh
+++ swig/java_lang/build-iotivity-lite.sh
@@ -7,7 +7,7 @@ rm -rf ../iotivity-lite-eclipse-project/bin
 mkdir ../iotivity-lite-eclipse-project/bin
 
 # compile swig generated java files and custom java files
-javac  -sourcepath ../iotivity-lite-eclipse-project/src  -d ../iotivity-lite-eclipse-project/bin  ../iotivity-lite-eclipse-project/src/org/iotivity/*.java
+javac -source 1.7 -target 1.7 -sourcepath ../iotivity-lite-eclipse-project/src  -d ../iotivity-lite-eclipse-project/bin  ../iotivity-lite-eclipse-project/src/org/iotivity/*.java
 
 # create jar file
 jar -cfv iotivity-lite.jar -C ../iotivity-lite-eclipse-project/bin  .
