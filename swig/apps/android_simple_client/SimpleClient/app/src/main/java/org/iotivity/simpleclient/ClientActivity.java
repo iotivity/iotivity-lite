cbe171380ba3304feb68f744b8725f3de25d83cf
Larry Sachs <larry.j.sachs@intel.com>
2018-11-07 12:58:03 -0800

    Android SimpleClient using SWIG
    Change-Id: I8280b84f1a894c00824ec464c1c5763906e0977f
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/27417
    Tested-by: IoTivity Jenkins <jenkins-daemon@iotivity.org>
    Reviewed-by: George Nash <george.nash@intel.com>
    Reviewed-by: Rick Bell <richard.s.bell@intel.com>

---
+++ swig/apps/android_simple_client/SimpleClient/app/src/main/java/org/iotivity/simpleclient/ClientActivity.java
@@ -0,0 +1,106 @@
+package org.iotivity.simpleclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.iotivity.OCClock;
+import org.iotivity.OCMain;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ClientActivity extends Activity {
+
+    static {
+        System.loadLibrary("c++_shared");
+        System.loadLibrary("iotivity-lite-jni");
+    }
+
+    public final Lock lock = new ReentrantLock();
+    public Condition cv = lock.newCondition();
+
+    public TextView mConsoleTextView;
+    public ScrollView mScrollView;
+
+    private static final String TAG = ClientActivity.class.getSimpleName();
+
+    private boolean quit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mConsoleTextView = (TextView) findViewById(R.id.consoleTextView);
+        mConsoleTextView.setMovementMethod(new ScrollingMovementMethod());
+        mScrollView = (ScrollView) findViewById(R.id.scrollView);
+        mScrollView.fullScroll(View.FOCUS_DOWN);
+
+        if (savedInstanceState == null) {
+            // start first time only
+//            OCStorage.storage_config("./simpleclient_creds"); // TODO: no security yet
+            MyInitHandler handler = new MyInitHandler(this);
+            int initReturn = OCMain.mainInit(handler);
+            if (initReturn < 0) {
+                Log.e(TAG, "Error in mainInit return code = " + initReturn);
+                return;
+            }
+
+            new Thread(new Runnable() {
+                public void run() {
+                    eventLoop();
+                }
+            }).start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        quit = true;
+        Log.d(TAG, "Calling main_shutdown.");
+        OCMain.mainShutdown();
+        super.onDestroy();
+    }
+
+    public void msg(final String text) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                mConsoleTextView.append("\n");
+                mConsoleTextView.append(text);
+                mScrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+        Log.i(TAG, text);
+    }
+
+    public void printLine() {
+        msg("------------------------------------------------------------------------");
+    }
+
+    private void eventLoop() {
+        while (!quit) {
+            long nextEvent = OCMain.mainPoll();
+            lock.lock();
+            try {
+                if (nextEvent == 0) {
+                    cv.await();
+                } else {
+                    long now = OCClock.clockTime();
+                    // nextEvent and now are in microseconds
+                    long timeToWait = (nextEvent - now) * 1000;
+                    cv.awaitNanos(timeToWait);
+                }
+            } catch (InterruptedException e) {
+                Log.d(TAG, e.getMessage());
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
---
+++ swig/apps/android_simple_client/SimpleClient/app/src/main/java/org/iotivity/simpleclient/MyDiscoveryHandler.java
@@ -0,0 +1,123 @@
+package org.iotivity.simpleclient;
+
+import org.iotivity.OCDiscoveryFlags;
+import org.iotivity.OCDiscoveryHandler;
+import org.iotivity.OCEndpoint;
+import org.iotivity.OCInterfaceMask;
+import org.iotivity.OCMain;
+import org.iotivity.OCQos;
+import org.iotivity.OCResourcePropertiesMask;
+
+import java.util.Arrays;
+
+public class MyDiscoveryHandler implements OCDiscoveryHandler {
+
+    private static final String TAG = MyDiscoveryHandler.class.getSimpleName();
+
+    private ClientActivity activity;
+
+    public MyDiscoveryHandler(ClientActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public OCDiscoveryFlags handler(String anchor, String uri, String[] types, int interfaceMask, OCEndpoint endpoint,
+                                    int resourcePropertiesMask, Object userData) {
+        activity.msg("DiscoveryHandler:");
+        activity.msg("\tanchor: " + anchor);
+        activity.msg("\turi: " + uri);
+        activity.msg("\ttypes: " + Arrays.toString(types));
+
+        String interfacesFound = "";
+        if ((interfaceMask & OCInterfaceMask.S) > 0) {
+            interfacesFound += "S";
+        }
+        if ((interfaceMask & OCInterfaceMask.A) > 0) {
+            if (!interfacesFound.isEmpty()) {
+                interfacesFound += " | ";
+            }
+            interfacesFound += "A";
+        }
+        if ((interfaceMask & OCInterfaceMask.RW) > 0) {
+            if (!interfacesFound.isEmpty()) {
+                interfacesFound += " | ";
+            }
+            interfacesFound += "RW";
+        }
+        if ((interfaceMask & OCInterfaceMask.R) > 0) {
+            if (!interfacesFound.isEmpty()) {
+                interfacesFound += " | ";
+            }
+            interfacesFound += "R";
+        }
+        if ((interfaceMask & OCInterfaceMask.B) > 0) {
+            if (!interfacesFound.isEmpty()) {
+                interfacesFound += " | ";
+            }
+            interfacesFound += "B";
+        }
+        if ((interfaceMask & OCInterfaceMask.LL) > 0) {
+            if (!interfacesFound.isEmpty()) {
+                interfacesFound += " | ";
+            }
+            interfacesFound += "LL";
+        }
+        if ((interfaceMask & OCInterfaceMask.BASELINE) > 0) {
+            if (!interfacesFound.isEmpty()) {
+                interfacesFound += " | ";
+            }
+            interfacesFound += "BASELINE";
+        }
+        activity.msg("\tinterfaces: " + interfacesFound);
+
+        String propertiesFound = "";
+        if ((resourcePropertiesMask & OCResourcePropertiesMask.OC_PERIODIC) > 0) {
+            propertiesFound += "PERIODIC";
+        }
+        if ((resourcePropertiesMask & OCResourcePropertiesMask.OC_SECURE) > 0) {
+            if (!propertiesFound.isEmpty()) {
+                propertiesFound += " | ";
+            }
+            propertiesFound += "SECURE";
+        }
+        if ((resourcePropertiesMask & OCResourcePropertiesMask.OC_OBSERVABLE) > 0) {
+            if (!propertiesFound.isEmpty()) {
+                propertiesFound += " | ";
+            }
+            propertiesFound += "OBSERVABLE";
+        }
+        if ((resourcePropertiesMask & OCResourcePropertiesMask.OC_DISCOVERABLE) > 0) {
+            if (!propertiesFound.isEmpty()) {
+                propertiesFound += " | ";
+            }
+            propertiesFound += "DISCOVERABLE";
+        }
+        activity.msg("\tresource properties: " + propertiesFound);
+
+        for (String type : types) {
+            if (type.equals("core.light")) {
+                Light light = new Light();
+                light.serverEndpoint = endpoint;
+                light.serverUri = uri;
+                activity.msg("\tResource " + light.serverUri + " hosted at endpoint(s):");
+                OCEndpoint ep = endpoint;
+                while (ep != null) {
+                    String[] endpointStr = new String[1];
+                    OCMain.endpointToString(ep, endpointStr);
+                    activity.msg("\t\tendpoint: " + endpointStr[0]);
+                    activity.msg("\t\t\tendpoint.device " + ep.getDevice());
+                    activity.msg("\t\t\tendpoint.flags " + ep.getFlags());
+                    activity.msg("\t\t\tendpoint.interfaceIndex " + ep.getInterfaceIndex());
+                    activity.msg("\t\t\tendpoint.version " + ep.getVersion().toString());
+                    ep = ep.getNext();
+                }
+                activity.printLine();
+                GetLightResponseHandler responseHandler = new GetLightResponseHandler(activity);
+                OCMain.doGet(light.serverUri, light.serverEndpoint, null, responseHandler, OCQos.LOW_QOS, light);
+                return OCDiscoveryFlags.OC_STOP_DISCOVERY;
+            }
+        }
+        OCMain.freeServerEndpoints(endpoint);
+        return OCDiscoveryFlags.OC_CONTINUE_DISCOVERY;
+    }
+}
