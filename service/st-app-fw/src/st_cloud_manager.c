2dbd8ce88c1333d40dfd203d4970caeb228e09ca
Jaehong Jo <jaehong.jo@samsung.com>
2018-11-13 16:04:27 +0900

    Changed status to reconnect when retry
    If the connection is lost during the retry connect
    due to overrun or error, the connection is tried again.
    Because of this, stack can send a duplicate request to the cloud.
    To solve this problem, added status when the retry connect.
    
    Change-Id: I619cda35602dca9967895a5f068f5237e03aa571
    Signed-off-by: Jaehong <jaehong.jo@samsung.com>

--- service/st-app-fw/src/st_cloud_manager.c
+++ service/st-app-fw/src/st_cloud_manager.c
@@ -120,17 +120,16 @@ session_event_handler(const oc_endpoint_t *endpoint, oc_session_state_t state)
     context = context->next;
   }
 
-  if (!context ||
-      context->cloud_manager_status == CLOUD_MANAGER_RE_CONNECTING ||
+  if (!context || context->cloud_manager_status & CLOUD_MANAGER_RE_CONNECTING ||
       0 != oc_endpoint_compare(endpoint, &context->cloud_ep))
     return;
 
   if (state == OC_SESSION_DISCONNECTED) {
     if (context->cloud_manager_status == CLOUD_MANAGER_FINISH) {
       oc_remove_delayed_callback(context, send_ping);
-      context->cloud_manager_status = CLOUD_MANAGER_RE_CONNECTING;
       oc_set_delayed_callback(context, callback_handler, 0);
     }
+    context->cloud_manager_status |= CLOUD_MANAGER_RE_CONNECTING;
 
     cloud_start_process(context);
   }
@@ -221,6 +220,7 @@ is_retry_over(st_cloud_context_t *context)
   oc_connectivity_end_session(&context->cloud_ep);
 #endif /* OC_TCP */
   context->retry_count = 0;
+  context->cloud_manager_status |= CLOUD_MANAGER_RE_CONNECTING;
   cloud_start_process(context);
   return true;
 }
@@ -269,9 +269,9 @@ error_handler(st_cloud_context_t *context, oc_rep_t* res_payload,
   switch (code) {
   case CI_TOKEN_EXPIRED:
     oc_remove_delayed_callback(context, callback);
-    if (context->cloud_manager_status != CLOUD_MANAGER_RE_CONNECTING)
+    if (!(context->cloud_manager_status & CLOUD_MANAGER_RE_CONNECTING))
       context->retry_count = 0;
-    context->cloud_manager_status = CLOUD_MANAGER_RE_CONNECTING;
+    context->cloud_manager_status |= CLOUD_MANAGER_RE_CONNECTING;
     oc_set_delayed_callback(context, refresh_token,
                             session_timeout[context->retry_count]);
     break;
@@ -279,8 +279,6 @@ error_handler(st_cloud_context_t *context, oc_rep_t* res_payload,
     oc_remove_delayed_callback(context, callback);
     context->cloud_manager_status = CLOUD_MANAGER_RESET;
     oc_set_delayed_callback(context, callback_handler, 0);
-    break;
-  case CI_INTERNAL_SERVER_ERROR:
     break;
   case CI_AUTHORIZATION_FAILED:
   case CI_CERTIFICATE_FAILED:
@@ -290,6 +288,10 @@ error_handler(st_cloud_context_t *context, oc_rep_t* res_payload,
     context->cloud_manager_status = CLOUD_MANAGER_FAIL;
     es_set_state(ES_STATE_FAILED_TO_REGISTER_TO_CLOUD);
     oc_set_delayed_callback(context, callback_handler, 0);
+    break;
+  case CI_INTERNAL_SERVER_ERROR:
+  default:
+    context->cloud_manager_status |= CLOUD_MANAGER_RE_CONNECTING;
     break;
   }
 }
@@ -385,6 +387,7 @@ sign_in_handler(oc_client_response_t *data)
 
   oc_remove_delayed_callback(context, sign_in);
   context->retry_count = 0;
+  context->cloud_manager_status &= ~CLOUD_MANAGER_RE_CONNECTING;
 
   if (context->cloud_manager_status == CLOUD_MANAGER_SIGNED_UP ||
       context->cloud_manager_status == CLOUD_MANAGER_SIGNED_IN) {
