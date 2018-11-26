a834c8dd260e71e0ebf6df9319bba10af18bf124
Jaehyun Cho <jaehyun3.cho@samsung.com>
2018-11-26 17:38:26 +0900

    Extract queueing logics.
    st_status_queue use queue push/pop logic into same file.
    queue logic is common logic and it can be used another place
    also. So common queueing logics are extracted from st_status_queue.
    
    Change-Id: I7e0b1c188c66f6de234d1439e0d5cca888d58201
    Signed-off-by: Jaehyun Cho <jaehyun3.cho@samsung.com>

---
+++ service/st-app-fw/include/st_queue.h
@@ -0,0 +1,62 @@
+/****************************************************************************
+ *
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+#ifndef ST_QUEUE_H
+#define ST_QUEUE_H
+
+#include "st_port.h"
+#include "util/oc_list.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// signaling is disabled due to some environment doesn't work.
+#ifndef Q_SIGNAL_DISABLE
+#define Q_SIGNAL_DISABLE
+#endif /* Q_SIGNAL_DISABLE */
+
+typedef void *(*st_queue_add_handler_t)(void *value);
+typedef void (*st_queue_free_handler_t)(void *item);
+
+typedef struct st_queue
+{
+  OC_LIST_STRUCT(queue);
+  st_mutex_t mutex;
+#ifndef Q_SIGNAL_DISABLE
+  st_cond_t cv;
+#endif /* Q_SIGNAL_DISABLE */
+  st_queue_add_handler_t add_handler;
+  st_queue_free_handler_t free_handler;
+} st_queue_t;
+
+st_queue_t *st_queue_initialize(st_queue_add_handler_t add_handler,
+                                st_queue_free_handler_t free_handler);
+int st_queue_deinitialize(st_queue_t *queue);
+int st_queue_push(st_queue_t *queue, void *value);
+void *st_queue_pop(st_queue_t *queue);
+void *st_queue_get_head(st_queue_t *queue);
+int st_queue_wait(st_queue_t *queue);
+int st_queue_free_item(st_queue_t *queue, void *item);
+int st_queue_free_all_items(st_queue_t *queue);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ST_QUEUE_H */
