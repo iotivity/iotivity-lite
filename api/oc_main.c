6e9ea24feb7f55bd978fb3492190ebc2b1d801d7
Kishen Maloor <kishen.maloor@intel.com>
2018-11-30 19:51:10 -0800

    oc_sp: add security profiles resource
    This change adds the /oic/sec/sp resource for storing and
    responding to requests for supported security profiles and the
    currently selected security profile.
    
    The resource handler echoes the most current (Dec 2018)
    OIDs for the baseline, black, blue and purple security profiles
    as specified for OCF 2.0.
    
    The implementation also handles a new API for pre-configuring the
    "manufacturer default" values for the /oic/sec/sp resource.
    
    Change-Id: Iec90f6277c59677f2fec9db1342966fa10e455d4
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/27671

--- api/oc_main.c
+++ api/oc_main.c
@@ -36,6 +36,9 @@
 #include "security/oc_store.h"
 #include "security/oc_svr.h"
 #include "security/oc_tls.h"
+#ifdef OC_PKI
+#include "security/oc_sp.h"
+#endif /* OC_PKI */
 #endif /* OC_SECURITY */
 
 #ifdef OC_MEMORY_TRACE
@@ -195,6 +198,9 @@ oc_main_init(const oc_handler_t *handler)
     oc_sec_load_doxm(device);
     oc_sec_load_cred(device);
     oc_sec_load_acl(device);
+#ifdef OC_PKI
+    oc_sec_load_sp(device);
+#endif /* OC_PKI */
     oc_sec_load_unique_ids(device);
   }
 #endif
@@ -237,6 +243,9 @@ oc_main_shutdown(void)
   oc_sec_cred_free();
   oc_sec_doxm_free();
   oc_sec_pstat_free();
+#ifdef OC_PKI
+  oc_sec_sp_free();
+#endif /* OC_PKI */
   oc_tls_shutdown();
 #endif /* OC_SECURITY */
 
