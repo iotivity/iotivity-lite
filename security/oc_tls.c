efef37f79dc19790b03491e63612478efac9d0c2
Oleksii Beketov <ol.beketov@samsung.com>
2018-12-05 19:06:44 +0200

    Reset session on unexpected hello
    Reset session on unexpected client hello message
    (draft)
    
    Change-Id: I3d5af05ec32d9b3d94247094d13b69f76729e9f4
    Signed-off-by: Oleksii Beketov <ol.beketov@samsung.com>

--- security/oc_tls.c
+++ security/oc_tls.c
@@ -1530,6 +1530,12 @@ read_application_data(oc_tls_peer_t *peer)
     int ret = 0;
     do {
       ret = mbedtls_ssl_handshake_step(&peer->ssl_ctx);
+      if (ret == MBEDTLS_ERR_SSL_BAD_HS_CLIENT_HELLO &&
+          peer->ssl_ctx.state != MBEDTLS_SSL_HELLO_REQUEST &&
+          peer->ssl_ctx.state !=  MBEDTLS_SSL_CLIENT_HELLO) {
+        mbedtls_ssl_session_reset(&peer->ssl_ctx);
+        return;
+      }
       if (peer->ssl_ctx.state == MBEDTLS_SSL_CLIENT_CHANGE_CIPHER_SPEC ||
           peer->ssl_ctx.state == MBEDTLS_SSL_SERVER_CHANGE_CIPHER_SPEC) {
         memcpy(peer->master_secret, peer->ssl_ctx.session_negotiate->master,
