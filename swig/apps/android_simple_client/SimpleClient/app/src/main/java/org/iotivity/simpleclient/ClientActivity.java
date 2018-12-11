7ef1a5529238d41daf27cb8fd603c668308f830c
Larry Sachs <larry.j.sachs@intel.com>
2018-12-11 11:53:39 -0800

    Add Security for Android
    Change-Id: I2537681bdbd1b0c5111dd780ee67b901defe428e
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

--- swig/apps/android_simple_client/SimpleClient/app/src/main/java/org/iotivity/simpleclient/ClientActivity.java
+++ swig/apps/android_simple_client/SimpleClient/app/src/main/java/org/iotivity/simpleclient/ClientActivity.java
@@ -10,7 +10,9 @@ import android.widget.TextView;
 
 import org.iotivity.OCClock;
 import org.iotivity.OCMain;
+import org.iotivity.OCStorage;
 
+import java.io.File;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -43,7 +45,18 @@ public class ClientActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) {
             // start first time only
-//            OCStorage.storage_config("./simpleclient_creds"); // TODO: no security yet
+            File credsDir = new File(getExternalFilesDir(null), "simpleclient_creds");
+            Log.i(TAG, "Credentials directory is " + credsDir.getAbsolutePath());
+            if (!credsDir.exists()) {
+                boolean mkDirResult = credsDir.mkdir();
+                if (mkDirResult) {
+                    Log.i(TAG, "Created credentials directory " + credsDir.getAbsolutePath());
+                } else {
+                    Log.e(TAG, "Failed to create credentials directory " + credsDir.getAbsolutePath());
+                }
+            }
+            OCStorage.storageConfig(credsDir.getAbsolutePath());
+
             MyInitHandler handler = new MyInitHandler(this);
             int initReturn = OCMain.mainInit(handler);
             if (initReturn < 0) {
