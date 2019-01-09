59d65e1010399c9b8a8ea66396181344ebcf5faa
Larry Sachs <larry.j.sachs@intel.com>
2019-01-09 12:52:36 -0800

    Android On Boarding Tool
    Not yet complete
    Do not merge
    
    Change-Id: Ic444e612d875beec9477a7c8981e8827f0789a40
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ swig/apps/android_on_boarding_tool/OnBoardingTool/app/src/main/java/org/iotivity/onboardingtool/AcePropertiesHelper.java
@@ -0,0 +1,18 @@
+package org.iotivity.onboardingtool;
+
+import org.iotivity.OCSecurityAce;
+
+public class AcePropertiesHelper {
+
+    private static final String TAG = AcePropertiesHelper.class.getSimpleName();
+
+    private OnBoardingActivity activity;
+
+    public AcePropertiesHelper(OnBoardingActivity activity) {
+        this.activity = activity;
+    }
+
+    public void getProperties(OCSecurityAce ace) {
+        // TODO...
+    }
+}
