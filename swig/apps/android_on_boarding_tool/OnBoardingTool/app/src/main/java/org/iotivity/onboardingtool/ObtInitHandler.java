112ef399d94677cdefad14749e87a202747822e7
Larry Sachs <larry.j.sachs@intel.com>
2019-01-09 12:52:36 -0800

    Android On Boarding Tool
    Does not work
    Do not merge
    
    Change-Id: Ic444e612d875beec9477a7c8981e8827f0789a40
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ swig/apps/android_on_boarding_tool/OnBoardingTool/app/src/main/java/org/iotivity/onboardingtool/ObtInitHandler.java
@@ -0,0 +1,46 @@
+package org.iotivity.onboardingtool;
+
+import android.util.Log;
+
+import org.iotivity.OCMain;
+import org.iotivity.OCMainInitHandler;
+
+public class ObtInitHandler implements OCMainInitHandler {
+
+    private static final String TAG = ObtInitHandler.class.getSimpleName();
+
+    private OnBoardingActivity activity;
+
+    public ObtInitHandler(OnBoardingActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public int initialize() {
+        Log.d(TAG, "inside ObtInitHandler.initialize()");
+        int ret = OCMain.initPlatform("OCF");
+        ret |= OCMain.addDevice("/oic/d", "oic.d.sensor", "OBT", "ocf.1.0.0", "ocf.res.1.0.0");
+        return ret;
+    }
+
+    @Override
+    public void registerResources() {
+        Log.d(TAG, "inside ObtInitHandler.registerResources()");
+    }
+
+    @Override
+    public void requestEntry() {
+        Log.d(TAG, "inside ObtInitHandler.requestEntry()");
+    }
+
+    @Override
+    public void signalEventLoop() {
+        Log.d(TAG, "inside ObtInitHandler.signalEventLoop()");
+        activity.lock.lock();
+        try {
+            activity.cv.signalAll();
+        } finally {
+            activity.lock.unlock();
+        }
+    }
+}
