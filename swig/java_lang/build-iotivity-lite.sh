1c98f0115ab04d1147a617908758ddfafaa2701a
George Nash <george.nash@intel.com>
2019-02-21 14:38:30 -0800

    Renaming project properties
    The iotiviy-lite-java project was named iotivity-lite
    with the project now being named iotivity-lite instead
    of iotivity-constraned this caused name conflicts when
    importing both the native project and java project at
    the same time.
    
    Also renamed the folder iotivity-lite-eclipse-project simply
    to iotivity-lite-java to match the project name when imported
    into eclipse. It is also possible to build the code outside
    eclipse. In fact out scripts already do this.
    
    Update the scripts to reflect the folder name change.
    
    Change-Id: I557f8d9be583b6f9c980c4b7f35d5c7945c1084b
    Signed-off-by: George Nash <george.nash@intel.com>
    Reviewed-on: https://gerrit.iotivity.org/gerrit/29294
    Reviewed-by: Larry Sachs <larry.j.sachs@intel.com>
    Tested-by: IoTivity Jenkins <jenkins-daemon@iotivity.org>
    Reviewed-by: Rick Bell <richard.s.bell@intel.com>

--- swig/java_lang/build-iotivity-lite.sh
+++ swig/java_lang/build-iotivity-lite.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # remove existing .class files
-rm -rf ../iotivity-lite-eclipse-project/bin
+rm -rf ../iotivity-lite-java/bin
 
 # create .class files directory
-mkdir ../iotivity-lite-eclipse-project/bin
+mkdir ../iotivity-lite-java/bin
 
 # compile swig generated java files and custom java files
-javac -source 1.7 -target 1.7 -sourcepath ../iotivity-lite-eclipse-project/src  -d ../iotivity-lite-eclipse-project/bin  ../iotivity-lite-eclipse-project/src/org/iotivity/*.java
+javac -source 1.7 -target 1.7 -sourcepath ../iotivity-lite-java/src  -d ../iotivity-lite-java/bin  ../iotivity-lite-java/src/org/iotivity/*.java
 
 # create jar file
-jar -cfv iotivity-lite.jar -C ../iotivity-lite-eclipse-project/bin  .
+jar -cfv iotivity-lite.jar -C ../iotivity-lite-java/bin  .
 
 # if building for Android, copy jar to Android libs directory
 if [ "$#" -ge 1 ] && [ "$1" = "android" ]
--- swig/java_lang/build_swig.sh
+++ swig/java_lang/build_swig.sh
@@ -2,33 +2,33 @@
 
 rm *.c
 #rm *.cxx
-rm ../iotivity-lite-eclipse-project/src/org/iotivity/*.java
+rm ../iotivity-lite-java/src/org/iotivity/*.java
 
-swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../../include/ ../swig_interfaces/oc_obt.i
+swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../../include/ ../swig_interfaces/oc_obt.i
 
-swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../../include/ ../swig_interfaces/oc_uuid.i
+swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../../include/ ../swig_interfaces/oc_uuid.i
 
-swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../../include/ ../swig_interfaces/oc_collection.i
+swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../../include/ ../swig_interfaces/oc_collection.i
 
 if [ "$#" -ge 1 ] && [ "$1" = "linux" ]
 then
   echo Building wrapper for linux clock
-  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../.. -I../../include/ -I../../port/linux ../swig_interfaces/oc_api.i
-  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -D__linux__ -I../.. -I../../port/linux -o oc_clock_wrap.c ../swig_interfaces/oc_clock.i
+  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../.. -I../../include/ -I../../port/linux ../swig_interfaces/oc_api.i
+  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -D__linux__ -I../.. -I../../port/linux -o oc_clock_wrap.c ../swig_interfaces/oc_clock.i
 elif [ "$#" -ge 1 ] && [ "$1" = "android" ]
 then
   echo Building wrapper for android clock
-  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../.. -I../../include/ -I../../port/android ../swig_interfaces/oc_api.i
-  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -D__linux__ -I../.. -I../../port/android -o oc_clock_wrap.c ../swig_interfaces/oc_clock.i
+  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../.. -I../../include/ -I../../port/android ../swig_interfaces/oc_api.i
+  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -D__linux__ -I../.. -I../../port/android -o oc_clock_wrap.c ../swig_interfaces/oc_clock.i
 else
   echo Building wrapper for windows clock
-  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../.. -I../../include/ -I../../port/windows ../swig_interfaces/oc_api.i
-  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../.. -I../../port/windows ../swig_interfaces/oc_clock.i
+  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../.. -I../../include/ -I../../port/windows ../swig_interfaces/oc_api.i
+  swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../.. -I../../port/windows ../swig_interfaces/oc_clock.i
 fi
 
-#swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ ../swig_interfaces/oc_ri.i
-swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../../port/ ../swig_interfaces/oc_storage.i
+#swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ ../swig_interfaces/oc_ri.i
+swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../../port/ ../swig_interfaces/oc_storage.i
 
-#swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-eclipse-project/src/org/iotivity/ -I../../security/ ../swig_interfaces/oc_acl.i
+#swig -java -package org.iotivity -outcurrentdir -outdir ../iotivity-lite-java/src/org/iotivity/ -I../../security/ ../swig_interfaces/oc_acl.i
 
-cp ../oc_java/*.java ../iotivity-lite-eclipse-project/src/org/iotivity/
+cp ../oc_java/*.java ../iotivity-lite-java/src/org/iotivity/
