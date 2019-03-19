c80f173a55d32e7e31ac6792aa733420c9925a50
Larry Sachs <larry.j.sachs@intel.com>
2019-03-19 14:54:19 -0700

    Java classes wrapping SWIG generated Java code
    Also includes sample apps modified to use the new Java classes.
    
    This is only beginning...
    
    Change-Id: I783d6fa51b0cc03d3967f7b09fc3baa20d591287
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ swig/apps/oc/java_oc_simple_server/src/java_oc_simple_server/MyInitHandler.java
@@ -0,0 +1,55 @@
+package java_oc_simple_server;
+
+import org.iotivity.*;
+import org.iotivity.oc.*;
+
+public class MyInitHandler implements OCMainInitHandler {
+
+    private OcPlatform platform;
+    private OcDevice device;
+    private Light light;
+
+    public MyInitHandler(OcPlatform platform) {
+        this.platform = platform;
+    }
+
+    @Override
+    public int initialize() {
+        System.out.println("inside MyInitHandler.initialize()");
+
+        int ret = platform.platformInit();
+        if (ret >= 0) {
+            device = new OcDevice("/oic/d", "oic.d.light", "Lamp", "ocf.1.0.0", "ocf.res.1.0.0");
+            ret |= platform.addDevice(device);
+        }
+
+        light = new Light("John's Light");
+
+        return ret;
+    }
+
+    @Override
+    public void registerResources() {
+        System.out.println("inside MyInitHandler.registerResources()");
+
+        String[] resourceTypes = new String[] { "core.light", "core.brightlight" };
+        int[] interfaceMasks = new int[] { OCInterfaceMask.RW };
+
+        OcResource resource = new OcResource(null, "/a/light", resourceTypes, interfaceMasks);
+
+        resource.setDefaultInterfaceMask(OCInterfaceMask.RW);
+        resource.setDiscoverable(true);
+        resource.setObservable(true);
+        resource.setPeriodicObservable(1);
+        resource.setGetRequestHandler(new GetLight(light));
+        resource.setPutRequestHandler(new PutLight(light));
+        resource.setPostRequestHandler(new PostLight(light));
+
+        device.addResource(resource);
+    }
+
+    @Override
+    public void requestEntry() {
+        System.out.println("inside MyInitHandler.requestEntry()");
+    }
+}
---
+++ swig/apps/oc/java_oc_simple_server/src/java_oc_simple_server/PostLight.java
@@ -0,0 +1,44 @@
+package java_oc_simple_server;
+
+import org.iotivity.*;
+
+public class PostLight implements OCRequestHandler {
+
+    private Light light;
+
+    public PostLight(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void handler(OCRequest request, int interfaces) {
+        System.out.println("Inside the PostLight RequestHandler");
+        System.out.println("POST LIGHT:");
+        OCRepresentation rep = request.getRequest_payload();
+        while (rep != null) {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("Key: " + rep.getName());
+            System.out.println("Type: " + rep.getType());
+            switch (rep.getType()) {
+            case OC_REP_BOOL:
+                light.setState(rep.getValue().getBool());
+                System.out.println("value: " + light.getState());
+                break;
+            case OC_REP_INT:
+                light.setPower(rep.getValue().getInteger());
+                System.out.println("value: " + light.getPower());
+                break;
+            case OC_REP_STRING:
+                light.setName(rep.getValue().getString());
+                System.out.println("value: " + light.getName());
+                break;
+            default:
+                System.out.println("NOT YET HANDLED VALUE");
+                OCMain.sendResponse(request, OCStatus.OC_STATUS_BAD_REQUEST);
+            }
+            System.out.println("-----------------------------------------------------");
+            rep = rep.getNext();
+        }
+        OCMain.sendResponse(request, OCStatus.OC_STATUS_CHANGED);
+    }
+}
