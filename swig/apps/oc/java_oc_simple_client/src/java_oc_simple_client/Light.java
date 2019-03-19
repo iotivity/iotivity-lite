875e30877e846be591cfb9ca8f054ba7ac6f70d5
Larry Sachs <larry.j.sachs@intel.com>
2019-03-19 14:54:19 -0700

    Java classes wrapping SWIG generated Java code
    Also includes sample apps modified to use the new Java classes.
    
    This is only beginning...
    
    Change-Id: I783d6fa51b0cc03d3967f7b09fc3baa20d591287
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ swig/apps/oc/java_oc_simple_client/src/java_oc_simple_client/Light.java
@@ -0,0 +1,55 @@
+package java_oc_simple_client;
+
+import org.iotivity.OCEndpoint;
+
+public class Light {
+
+    private String name;
+    private long power;
+    private boolean state;
+    private OCEndpoint serverEndpoint;
+    private String serverUri;
+
+    public Light() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPower() {
+        return power;
+    }
+
+    public void setPower(long power) {
+        this.power = power;
+    }
+
+    public boolean getState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+
+    public OCEndpoint getServerEndpoint() {
+        return serverEndpoint;
+    }
+
+    public void setServerEndpoint(OCEndpoint serverEndpoint) {
+        this.serverEndpoint = serverEndpoint;
+    }
+
+    public String getServerUri() {
+        return serverUri;
+    }
+
+    public void setServerUri(String serverUri) {
+        this.serverUri = serverUri;
+    }
+}
