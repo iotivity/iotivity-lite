cde17edaf557b6862673d9e474c5c7f7e637e041
Jozef Kralik <jojo.lwin@gmail.com>
2019-03-30 09:54:04 +0100

    [LITE-43] Implement Cloud service
    Provides API for connect a device to the Cloud Interface.
    
    Cloud Sample apps/cloud_linux.c
    Cloud API: service/cloud/include/cloud.h
    
    Limitations:
     * Cloud always try to connect to the Cloud Interface
     * Delays are not configurable (ping, publish resources, ...)
     * Sid isn't verified
     * Cloud doesn't use expires in - refresh token is called after
       sign in and every 30 minutes
    
    Change-Id: I3faf440f0fd00648cf0e01f50763d80c785cd734
    Signed-off-by: Jozef Kralik <jojo.lwin@gmail.com>

---
+++ service/resource-directory/client/include/rd_client.h
@@ -0,0 +1,69 @@
+/****************************************************************************
+ *
+ * Copyright 2019 Jozef Kralik  All Rights Reserved.
+ * Copyright 2018 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
+/**
+  @brief Resource Directory API of IoTivity-constrained for client.
+  @file
+*/
+
+#ifndef RD_CLIENT_H
+#define RD_CLIENT_H
+
+#include "oc_client_state.h"
+#include "oc_ri.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Resource Directory URI used to Discover RD and Publish resources.*/
+#define OC_RSRVD_RD_URI "/oic/rd"
+
+/**
+  @brief Publish RD resource to Resource Directory.
+  @param endpoint The endpoint of the RD.
+  @param links This is the resource which we need to register to RD.
+   If null, oic/p and oic/d resources will be published.
+  @param device_index Index of the device for an unique identifier.
+  @param handler To refer to the request sent out on behalf of calling this API.
+  @param qos Quality of service.
+  @param user_data The user data passed from the registration function.
+  @return Returns true if success.
+*/
+bool rd_publish(oc_endpoint_t *endpoint, oc_link_t *links, size_t device_index,
+                oc_response_handler_t handler, oc_qos_t qos, void *user_data);
+
+/**
+  @brief Delete RD resource from Resource Directory.
+  @param endpoint The endpoint of the RD.
+  @param links This is the resource which we need to delete to RD.
+  @param device_index Index of the device for an unique identifier.
+  @param handler To refer to the request sent out on behalf of calling this API.
+  @param qos Quality of service.
+  @param user_data The user data passed from the registration function.
+  @return Returns true if success.
+*/
+bool rd_delete(oc_endpoint_t *endpoint, oc_link_t *links, size_t device_index,
+               oc_response_handler_t handler, oc_qos_t qos, void *user_data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RD_CLIENT_H */
