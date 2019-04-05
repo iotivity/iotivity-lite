b6e295e35251f89b5eac5d141248e75c63d3aba3
sachinsk <sachin@graniteriverlabs.in>
2019-04-05 19:13:42 +0530

    CertificationApp
    Integrated relevant parts of 'iotivity-lite/apps/cloud_certification_tests.c'
    with CertificationApp
    
    Change-Id: Iedec3ff00ec85e7353911dc609415c7c46521ed8
    Signed-off-by: Anutosh <anutosh@graniteriverlabs.in>
    Signed-off-by: sachinsk <sachin@graniteriverlabs.in>

---
+++ tests/automation/ctt/run_taf_agent.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get update --fix-missing
+sudo apt-get install libuv1.dev
+sudo apt-get install libxml2-dev
+
+killall -9 tafagent 
+killall -9 CertificationApp
+cd ../../../port/linux
+make cleanall
+make TCP=1
+cd -
+cp ../../../port/linux/discover_device .
+cp ../../../port/linux/CertificationApp .
+
+rm -rf CertificationApp_creds
+mkdir CertificationApp_creds
+#include system configured UV lib path
+export LD_LIBRARY_PATH=/usr/local/lib
+make
+./tafagent server_config.txt 1 6 13 2.0.2 192.168.3.66 2 10 1 6
