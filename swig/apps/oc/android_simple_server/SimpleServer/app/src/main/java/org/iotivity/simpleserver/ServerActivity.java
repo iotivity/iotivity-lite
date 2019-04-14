d4c9c87ab04e013c58bac84b5eda3c2b31837246
Larry Sachs <larry.j.sachs@intel.com>
2019-04-14 14:56:36 -0700

    Android Client/Server Apps using Java O-O classes
    Updates client/server apps to use Java O-O wrappers
    for SWIG generated code.
    
    Change-Id: I87209ac132c91d863bd221732a16ab27a28dd0d7
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ swig/apps/oc/android_simple_server/SimpleServer/app/src/main/java/org/iotivity/simpleserver/ServerActivity.java
@@ -0,0 +1,61 @@
+package org.iotivity.simpleserver;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.iotivity.oc.OcPlatform;
+
+public class ServerActivity extends AppCompatActivity {
+
+    private static final String TAG = ServerActivity.class.getSimpleName();
+
+    private TextView mConsoleTextView;
+    private ScrollView mScrollView;
+
+    private OcPlatform obtPlatform;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mConsoleTextView = (TextView) findViewById(R.id.consoleTextView);
+        mConsoleTextView.setMovementMethod(new ScrollingMovementMethod());
+        mScrollView = (ScrollView) findViewById(R.id.scrollView);
+        mScrollView.fullScroll(View.FOCUS_DOWN);
+
+        if (savedInstanceState == null) {
+            // start first time only
+            obtPlatform = OcPlatform.getInstance();
+            MyInitHandler handler = new MyInitHandler(this, obtPlatform);
+            obtPlatform.systemInit(handler);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "Calling Shutdown.");
+        obtPlatform.systemShutdown();
+        super.onDestroy();
+    }
+
+    public void msg(final String text) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                mConsoleTextView.append("\n");
+                mConsoleTextView.append(text);
+                mScrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+        Log.i(TAG, text);
+    }
+
+    public void printLine() {
+        msg("------------------------------------------------------------------------");
+    }
+}
