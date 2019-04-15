61645cd3beae516ddc4c03e81a9044e175229867
George Nash <george.nash@intel.com>
2019-04-15 14:18:20 -0700

    Move oc_endpoint.h into its own interface file
    Change-Id: Idc2a5410719d7268223a4cd207ee69bec82e7e9e
    Signed-off-by: George Nash <george.nash@intel.com>

--- swig/apps/java_onboarding_tool/src/java_onboarding_tool/OwnedDeviceHandler.java
+++ swig/apps/java_onboarding_tool/src/java_onboarding_tool/OwnedDeviceHandler.java
@@ -1,6 +1,7 @@
 package java_onboarding_tool;
 
 import org.iotivity.OCEndpoint;
+import org.iotivity.OCEndpointUtil;
 import org.iotivity.OCMain;
 import org.iotivity.OCObtDiscoveryHandler;
 import org.iotivity.OCUuidUtil;
@@ -14,7 +15,7 @@ public class OwnedDeviceHandler implements OCObtDiscoveryHandler {
         System.out.println("\nDiscovered owned device: "+ deviceId + " at:");
         while (endpoints != null) {
             String[] endpointStr = new String[1];
-            OCMain.endpointToString(endpoints, endpointStr);
+            OCEndpointUtil.toString(endpoints, endpointStr);
             System.out.println(endpointStr[0]);
             endpoints = endpoints.getNext();
         }
