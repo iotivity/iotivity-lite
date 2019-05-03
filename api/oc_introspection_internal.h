a2ab72f627ca11f8ea9e7ffe06f78bb88ac353e1
George Nash <george.nash@intel.com>
2019-05-03 16:56:31 -0700

    Rework of oc_introspection.h
    This adds the oc_set_introspection_data function to
    the oc_introspection.h header. This enables setting
    the introspection data by passing in an array of bytes
    instead of specifying a file or endoding the bytes in
    the introspeciton.dat.h header file.
    
    To keep this backwards compatible with the existing
    oc_set_introspection_file the code assumes that the source of
    introspection data is from a file. If no file is specified and
    oc_set_introspection_data is not called the code will default
    to using the introspection IDD_<deivce_number> from oc_storage.
    This is the only behavior what was changed from the priviously
    used behavior which was to default to a file named
    server_introspection.dat.
    
    As a side in the windows  build OC_IDD_FILE is set as the
    default.
    
    Users can get the same behavior as building the code without
    OC_IDD_FILE not defined by including server_introspection.dat.h
    in the program and calling
    oc_set_intersection_data(0,
                             introspection_Data,
                             introspection_data_size);
    
    The function oc_create_introspection_resource as well as a new
    function oc_introspection_init were placed in a new internal
    header file oc_introspection_internal.h. Both functions are
    only expected to be called by the framework and there is no
    need to expose these functions to end users as public APIs.
    
    Change-Id: I9ec452afbf96510c82557eeb1b8b79a8949eeb25
    Signed-off-by: George Nash <george.nash@intel.com>

---
+++ api/oc_introspection_internal.h
@@ -0,0 +1,41 @@
+/*
+// Copyright (c) 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#ifndef OC_INTROSPECTION_INTERNAL_H
+#define OC_INTROSPECTION_INTERNAL_H
+
+#define MAX_INTROSPECT_FILENAME_LENGTH 128
+
+typedef enum {
+    OC_INTROSPECT_FILE = 0,
+    OC_INTROSPECT_BYTE_ARRAY
+} oc_introspect_source_t;
+
+typedef struct oc_introspect_info_t {
+    oc_introspect_source_t source;
+    char filename[MAX_INTROSPECT_FILENAME_LENGTH];
+} oc_introspect_info_t;
+
+void oc_introspection_init(void);
+
+/**
+@brief Creation of the oic.wk.introspection resource.
+
+@param device index of the device to which the resource is to be created
+*/
+void oc_create_introspection_resource(size_t device);
+
+#endif /* OC_INTROSPECTION_INTERNAL_H */
