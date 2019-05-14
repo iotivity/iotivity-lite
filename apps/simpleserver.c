307041e1b49706e96e5a8ab93e19a88b2dfd1fc5
sungmok.shin <sungmok.shin@lge.com>
2019-05-14 11:42:24 +0900

    Comparing porting layer between linux and webos
    Change-Id: I274f9e983217f7a2c5f38b3fc684cfaacc156634
    Signed-off-by: sungmok.shin <sungmok.shin@lge.com>

--- apps/simpleserver.c
+++ apps/simpleserver.c
@@ -20,6 +20,17 @@
 #include <signal.h>
 #include <stdio.h>
 
+#include <luna-service2/lunaservice.h>
+#include <glib.h>
+#include <pbnjson.h>
+#include <PmLogLib.h>
+
+static LSHandle *pLsHandle = NULL;
+static GMainLoop *mainloop = NULL;
+
+PmLogContext gLogContext;
+PmLogContext gLogLibContext;
+
 pthread_mutex_t mutex;
 pthread_cond_t cv;
 struct timespec ts;
@@ -33,10 +44,10 @@ oc_string_t name;
 static int
 app_init(void)
 {
-  int ret = oc_init_platform("Intel", NULL, NULL);
-  ret |= oc_add_device("/oic/d", "oic.d.light", "Lamp", "ocf.1.0.0",
-                       "ocf.res.1.0.0", NULL, NULL);
-  oc_new_string(&name, "John's Light", 12);
+  int ret = oc_init_platform("webOS", NULL, NULL);
+  ret |= oc_add_device("/oic/d", "oic.d.light", "webOS Light", "ocf.2.0.0",
+                       "ocf.res.1.3.0,ocf.sh.1.3.0", NULL, NULL);
+  oc_new_string(&name, "webOS Light", 11);
   return ret;
 }
 
@@ -53,9 +64,13 @@ get_light(oc_request_t *request, oc_interface_mask_t iface_mask, void *user_data
     oc_process_baseline_interface(request->resource);
   /* fall through */
   case OC_IF_RW:
-    oc_rep_set_boolean(root, state, state);
-    oc_rep_set_int(root, power, power);
-    oc_rep_set_text_string(root, name, oc_string(name));
+    oc_rep_set_boolean(root, value, state);
+    //oc_rep_set_int(root, power, power);
+    oc_rep_set_text_string(root, n, oc_string(name));
+    break;
+  case OC_IF_A:
+    oc_rep_set_boolean(root, value, state);
+    oc_rep_set_text_string(root, n, oc_string(name));
     break;
   default:
     break;
@@ -109,16 +124,17 @@ put_light(oc_request_t *request, oc_interface_mask_t iface_mask,
 static void
 register_resources(void)
 {
-  oc_resource_t *res = oc_new_resource(NULL, "/a/light", 2, 0);
-  oc_resource_bind_resource_type(res, "core.light");
-  oc_resource_bind_resource_type(res, "core.brightlight");
-  oc_resource_bind_resource_interface(res, OC_IF_RW);
-  oc_resource_set_default_interface(res, OC_IF_RW);
+  oc_resource_t *res = oc_new_resource("ssm", "/binaryswitch", 1, 0);
+  oc_resource_bind_resource_type(res, "oic.r.switch.binary");
+  oc_resource_bind_resource_interface(res, OC_IF_BASELINE);
+  oc_resource_bind_resource_interface(res, OC_IF_A);
+  oc_resource_set_default_interface(res, OC_IF_A);
   oc_resource_set_discoverable(res, true);
   oc_resource_set_periodic_observable(res, 1);
   oc_resource_set_request_handler(res, OC_GET, get_light, NULL);
   oc_resource_set_request_handler(res, OC_PUT, put_light, NULL);
   oc_resource_set_request_handler(res, OC_POST, post_light, NULL);
+  oc_set_con_res_announced(false);
   oc_add_resource(res);
 }
 
@@ -141,6 +157,35 @@ handle_signal(int signal)
 int
 main(void)
 {
+    struct timespec timeout;
+    LSError lserror;
+    LSErrorInit(&lserror);
+    (void) PmLogGetContext("OCSERVERBASICOPS", &gLogContext);
+    (void) PmLogGetContext("OCSERVERBASICOPS-LIB", &gLogLibContext);
+    PmLogSetLibContext(gLogLibContext);
+
+    mainloop = g_main_loop_new(NULL, FALSE);
+
+    // Initialize g_main_loop
+    if (!mainloop) {
+        PRINT("Failed to create main loop");
+        return 0;
+    }
+
+    PRINT("simpleserver is starting...\n");
+
+    if (!LSRegister("org.ocf.webossample.simpleserver", &pLsHandle, &lserror)) {
+        PRINT("Failed to register LS Handle");
+        LSErrorLog(gLogContext, "LS_SRVC_ERROR", &lserror);
+        return 0;
+    }
+
+    if (!LSGmainAttach(pLsHandle, mainloop, &lserror)) {
+        PRINT("Failed to attach main loop: %s", &lserror);
+        LSErrorLog(gLogContext, "LS_SRVC_ATTACH_ERROR", &lserror);
+        return 0;
+    }
+
   int init;
   struct sigaction sa;
   sigfillset(&sa.sa_mask);
