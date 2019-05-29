2cafe1ff44d7b96f77204fac33b6cb7d1adf08a9
Larry Sachs <larry.j.sachs@intel.com>
2019-05-29 14:41:28 -0700

    Apps for Media Control Resource
    Simple Client/Server apps showing new Media Control Resource.
    
    Change-Id: Ifa46f394c7290ed87fa96abcccf7ddfdd034d441
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ swig/apps/oc/java_oc_simple_media_client/src/java_oc_simple_media_client/GetMediaControlResourceResponseHandler.java
@@ -0,0 +1,54 @@
+package java_oc_simple_media_client;
+
+import org.iotivity.*;
+import org.iotivity.oc.*;
+
+public class GetMediaControlResourceResponseHandler implements OCResponseHandler {
+
+    private MediaControlResource mediaControlResource;
+
+    public GetMediaControlResourceResponseHandler(MediaControlResource mediaControlResource) {
+        this.mediaControlResource = mediaControlResource;
+    }
+
+    @Override
+    public void handler(OCClientResponse response) {
+        OCRepresentation rep = response.getPayload();
+        while (rep != null) {
+            switch (rep.getType()) {
+            case OC_REP_BOOL:
+                if ("playstate".equalsIgnoreCase(rep.getName())) {
+                    mediaControlResource.setPlayState(rep.getValue().getBool());
+                }
+                break;
+            case OC_REP_INT:
+                break;
+            case OC_REP_STRING:
+                if ("name".equalsIgnoreCase(rep.getName())) {
+                    mediaControlResource.setName(rep.getValue().getString());
+                }
+                if ("mediaaction".equalsIgnoreCase(rep.getName())) {
+                    mediaControlResource.setCurrentAction(rep.getValue().getString());
+                }
+                break;
+            case OC_REP_STRING_ARRAY:
+                if ("mediaactions".equalsIgnoreCase(rep.getName())) {
+                    mediaControlResource.setAllowedActions(OCRep.ocArrayToStringArray(rep.getValue().getArray()));
+                }
+                break;
+            case OC_REP_DOUBLE:
+                if ("mediaspeed".equalsIgnoreCase(rep.getName())) {
+                    mediaControlResource.setSpeed(rep.getValue().getDouble());
+                }
+                if ("medialocation".equalsIgnoreCase(rep.getName())) {
+                    mediaControlResource.setLocation(rep.getValue().getDouble());
+                }
+                break;
+
+            default:
+                break;
+            }
+            rep = rep.getNext();
+        }
+    }
+}
