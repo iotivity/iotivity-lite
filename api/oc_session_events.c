9f1f55e88e86c8a3b959964ceb15093808e9b65e
Kishen Maloor <kishen.maloor@intel.com>
2019-06-09 17:10:37 -0700

    oc_session_events: clear deadlock
    This change clears a deadlock arising in free_session_state_delayed()
    from the invocation of oc_session_end_event() while freeing
    TCP session state. The problem was fixed by synchronizing access
    only to the session list and excluding the rest of the code block
    in free_session_state_delayed() which is where a call to
    oc_session_end_event() is made.
    
    Further, oc_tls was updated to call oc_handle_session() directly rather
    than using the session_events process as routines in oc_tls always execute
    on the main (background) thread.
    
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Change-Id: I7bdbec84acae463dee9bed9bbe4868a022430fde

--- api/oc_session_events.c
+++ api/oc_session_events.c
@@ -17,6 +17,7 @@
 #include "oc_session_events.h"
 #include "oc_config.h"
 
+#include "api/oc_session_events_internal.h"
 #include "oc_api.h"
 #include "oc_buffer.h"
 #include "oc_signal_event_loop.h"
@@ -28,65 +29,62 @@
 #include "messaging/coap/observe.h"
 #endif /* OC_SERVER */
 
-#define SESSION_STATE_FREE_DELAY_SECS (3)
-
 #ifdef OC_TCP
 OC_LIST(session_start_events);
 OC_LIST(session_end_events);
 
-static void
-oc_handle_session(oc_endpoint_t *endpoint, oc_session_state_t state)
-{
-  (void)endpoint;
-  (void)state;
-  if (state == OC_SESSION_DISCONNECTED) {
-#ifdef OC_SECURITY
-    if (endpoint->flags & SECURED && endpoint->flags & TCP) {
-      oc_tls_remove_peer(endpoint);
-    }
-#endif /* OC_SECURITY */
-  }
-#ifdef OC_SESSION_EVENTS
-  handle_session_event_callback(endpoint, state);
-#endif /* OC_SESSION_EVENTS */
-}
+static int SESSION_STATE_FREE_DELAY_SECS;
+static bool session_end_ref = false;
 
 static oc_event_callback_retval_t
 free_session_state_delayed(void *data)
 {
   (void)data;
-  oc_network_event_handler_mutex_lock();
-  oc_endpoint_t *session_event =
-    (oc_endpoint_t *)oc_list_pop(session_end_events);
-  while (session_event != NULL) {
-    oc_handle_session(session_event, OC_SESSION_DISCONNECTED);
-    oc_free_endpoint(session_event);
-    session_event = oc_list_pop(session_end_events);
-  }
-  oc_network_event_handler_mutex_unlock();
+  session_end_ref = true;
+  oc_endpoint_t *session_event = NULL;
+  do {
+    oc_network_event_handler_mutex_lock();
+    session_event = (oc_endpoint_t *)oc_list_pop(session_end_events);
+    oc_network_event_handler_mutex_unlock();
+    if (session_event) {
+      oc_handle_session(session_event, OC_SESSION_DISCONNECTED);
+      oc_free_endpoint(session_event);
+    }
+  } while (oc_list_length(session_end_events) > 0);
+  session_end_ref = false;
   return OC_EVENT_DONE;
+}
+
+bool
+oc_session_events_is_ongoing(void)
+{
+  return session_end_ref;
+}
+
+void
+oc_session_events_set_event_delay(int secs)
+{
+  SESSION_STATE_FREE_DELAY_SECS = secs;
 }
 
 static void
 oc_process_session_event(void)
 {
-  oc_network_event_handler_mutex_lock();
-
-  oc_endpoint_t *session_event =
-    (oc_endpoint_t *)oc_list_pop(session_start_events);
-
-  while (session_event != NULL) {
-    oc_handle_session(session_event, OC_SESSION_CONNECTED);
-    oc_free_endpoint(session_event);
-    session_event = oc_list_pop(session_start_events);
-  }
+  oc_endpoint_t *session_event = NULL;
+  do {
+    oc_network_event_handler_mutex_lock();
+    session_event = (oc_endpoint_t *)oc_list_pop(session_start_events);
+    oc_network_event_handler_mutex_unlock();
+    if (session_event) {
+      oc_handle_session(session_event, OC_SESSION_CONNECTED);
+      oc_free_endpoint(session_event);
+    }
+  } while (oc_list_length(session_start_events) > 0);
 
   if (oc_list_length(session_end_events) > 0) {
     oc_set_delayed_callback(NULL, &free_session_state_delayed,
                             SESSION_STATE_FREE_DELAY_SECS);
   }
-
-  oc_network_event_handler_mutex_unlock();
 }
 
 OC_PROCESS(oc_session_events, "");
@@ -140,3 +138,20 @@ oc_session_end_event(oc_endpoint_t *endpoint)
   _oc_signal_event_loop();
 }
 #endif /* OC_TCP */
+
+void
+oc_handle_session(oc_endpoint_t *endpoint, oc_session_state_t state)
+{
+  (void)endpoint;
+  (void)state;
+  if (state == OC_SESSION_DISCONNECTED) {
+#ifdef OC_SECURITY
+    if (endpoint->flags & SECURED && endpoint->flags & TCP) {
+      oc_tls_remove_peer(endpoint);
+    }
+#endif /* OC_SECURITY */
+  }
+#ifdef OC_SESSION_EVENTS
+  handle_session_event_callback(endpoint, state);
+#endif /* OC_SESSION_EVENTS */
+}
