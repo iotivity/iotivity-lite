8e77092c353ffbf589135c45200a7b47161b155c
George Nash <george.nash@intel.com>
2019-06-26 10:53:57 -0700

    Updates for onboarding_tool
    This commit contains two updates to make the onboarding
    the same as the C version of the tool.
    
    1. It adds the realm-local and site-local discovery options.
    
    unlike the C sample we simply use the numbers 0, 1, and 2 for
    the discovery scope. The C sample used hex numbers 0x02, 0x03,
    and 0x05. I could not find a reason to keep the hex numbers.
    
    2. The device name is now displayed along with the UUID
    of the device when doing device selection.
    
    Change-Id: I974f6de22fe68ce308939993d0cf48aea3f1bf2b
    Signed-off-by: George Nash <george.nash@intel.com>

--- swig/apps/java_onboarding_tool/src/java_onboarding_tool/UnownedDeviceHandler.java
+++ swig/apps/java_onboarding_tool/src/java_onboarding_tool/UnownedDeviceHandler.java
@@ -4,6 +4,7 @@ import org.iotivity.OCEndpoint;
 import org.iotivity.OCEndpointUtil;
 import org.iotivity.OCMain;
 import org.iotivity.OCObtDiscoveryHandler;
+import org.iotivity.OCQos;
 import org.iotivity.OCUuidUtil;
 import org.iotivity.OCUuid;
 
@@ -13,13 +14,15 @@ public class UnownedDeviceHandler implements OCObtDiscoveryHandler {
     public void handler(OCUuid uuid, OCEndpoint endpoints) {
         String deviceId = OCUuidUtil.uuidToString(uuid);
         System.out.println("\nDiscovered unowned device: "+ deviceId + " at:");
+        OCEndpoint ep = endpoints;
         while (endpoints != null) {
             String endpointStr = OCEndpointUtil.toString(endpoints);
             System.out.println(endpointStr);
             endpoints = endpoints.getNext();
         }
 
-        ObtMain.unownedDevices.add(uuid);
+        OCMain.doGet("/oic/d", ep, null, getUnownedDeviceNameHandler, OCQos.LOW_QOS);
     }
 
+    public static GetUnownedDeviceNameHandler getUnownedDeviceNameHandler = new GetUnownedDeviceNameHandler();
 }
