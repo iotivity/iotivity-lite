2d1c32cf24624d85eddb22db4d7175ce4609e309
George Nash <george.nash@intel.com>
2019-07-09 13:57:31 -0700

    Java version of multi device server/client samples
    Change-Id: I2b184a8bbb8007ff64cdc8d5203ec6d75216e33f
    Signed-off-by: George Nash <george.nash@intel.com>

---
+++ swig/apps/java_multi_device_client/src/java_multi_device_client/Thermostat.java
@@ -0,0 +1,10 @@
+package java_multi_device_client;
+
+import org.iotivity.OCEndpoint;
+
+public class Thermostat {
+    public static double temperature;
+
+    public static OCEndpoint serverEndpoint;
+    public static String serverUri;
+}
