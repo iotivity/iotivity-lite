46a511ca6353154b784ad933c1bc2f7afe46bede
George Nash <george.nash@intel.com>
2019-08-22 11:22:42 -0700

    Update Java stringToEndpoint method
    The Java stringToEndpoint method now returns an OCEndpoint
    
    Change-Id: I6040d9b48ca214d83daf49a59f6b2ba8ff2903dd
    Signed-off-by: George Nash <george.nash@intel.com>

---
+++ swig/iotivity-lite-java/junit/org/iotivity/OCEndpointTest.java
@@ -0,0 +1,120 @@
+package org.iotivity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OCEndpointTest {
+
+    @Test
+    public void testStringToEndpoint() {
+        String[] uri = new String[1];
+        OCEndpoint ep = OCEndpointUtil.stringToEndpoint("coaps://10.211.55.3:56789/a/light", uri);
+        assertNotNull(ep);
+        assertEquals(OCTransportFlags.IPV4, ep.getFlags() & OCTransportFlags.IPV4);
+        assertEquals(OCTransportFlags.SECURED, ep.getFlags() & OCTransportFlags.SECURED);
+        assertNotEquals(OCTransportFlags.TCP, ep.getFlags() & OCTransportFlags.TCP);
+        assertEquals(56789, ep.getAddr().getIpv4().getPort());
+        assertEquals("/a/light", uri[0]);
+        assertArrayEquals(new short[]{10, 211, 55, 3}, ep.getAddr().getIpv4().getAddress());
+        OCEndpointUtil.freeEndpoint(ep);
+
+        ep = OCEndpointUtil.stringToEndpoint("coap://openconnectivity.org", uri);
+        assertNotNull(ep);
+        assertTrue(OCTransportFlags.IPV4 == (ep.getFlags() & OCTransportFlags.IPV4) ||
+                   OCTransportFlags.IPV6 == (ep.getFlags() & OCTransportFlags.IPV6));
+        assertNotEquals(OCTransportFlags.SECURED, ep.getFlags() & OCTransportFlags.SECURED);
+        assertNotEquals(OCTransportFlags.TCP, ep.getFlags() & OCTransportFlags.TCP);
+        assertEquals(5683, ep.getAddr().getIpv4().getPort());
+        assertNull(uri[0]);
+        OCEndpointUtil.freeEndpoint(ep);
+
+        ep = OCEndpointUtil.stringToEndpoint("coap://openconnectivity.org/alpha", uri);
+        assertNotNull(ep);
+        assertTrue(OCTransportFlags.IPV4 == (ep.getFlags() & OCTransportFlags.IPV4) ||
+                   OCTransportFlags.IPV6 == (ep.getFlags() & OCTransportFlags.IPV6));
+        assertNotEquals(OCTransportFlags.SECURED, ep.getFlags() & OCTransportFlags.SECURED);
+        assertNotEquals(OCTransportFlags.TCP, ep.getFlags() & OCTransportFlags.TCP);
+        assertEquals(5683, ep.getAddr().getIpv4().getPort());
+        assertEquals("/alpha", uri[0]);
+        OCEndpointUtil.freeEndpoint(ep);
+
+        ep = OCEndpointUtil.stringToEndpoint("coaps://openconnectivity.org:3456/alpha", uri);
+        assertNotNull(ep);
+        assertTrue(OCTransportFlags.IPV4 == (ep.getFlags() & OCTransportFlags.IPV4) ||
+                   OCTransportFlags.IPV6 == (ep.getFlags() & OCTransportFlags.IPV6));
+        assertEquals(OCTransportFlags.SECURED, ep.getFlags() & OCTransportFlags.SECURED);
+        assertNotEquals(OCTransportFlags.TCP, ep.getFlags() & OCTransportFlags.TCP);
+        assertEquals(3456, ep.getAddr().getIpv4().getPort());
+        assertEquals("/alpha", uri[0]);
+        OCEndpointUtil.freeEndpoint(ep);
+    }
+    
+    @Test
+    public void testStringToEndpoint_tcp() {
+        String[] uri = new String[1];
+        OCEndpoint ep = OCEndpointUtil.stringToEndpoint("coaps+tcp://10.211.55.3/a/light", uri);
+        assertNotNull(ep);
+        assertEquals(OCTransportFlags.IPV4, ep.getFlags() & OCTransportFlags.IPV4);
+        assertEquals(OCTransportFlags.SECURED, ep.getFlags() & OCTransportFlags.SECURED);
+        assertEquals(OCTransportFlags.TCP, ep.getFlags() & OCTransportFlags.TCP);
+        assertEquals(5684, ep.getAddr().getIpv4().getPort());
+        assertEquals("/a/light", uri[0]);
+        assertArrayEquals(new short[]{10, 211, 55, 3}, ep.getAddr().getIpv4().getAddress());
+        OCEndpointUtil.freeEndpoint(ep);
+
+        ep = OCEndpointUtil.stringToEndpoint("coap+tcp://1.2.3.4:2568", uri);
+        assertNotNull(ep);
+        assertEquals(OCTransportFlags.IPV4, (ep.getFlags() & OCTransportFlags.IPV4));
+        assertNotEquals(OCTransportFlags.SECURED, (ep.getFlags() & OCTransportFlags.SECURED));
+        assertEquals(OCTransportFlags.TCP, (ep.getFlags() & OCTransportFlags.TCP));
+        assertEquals(2568, ep.getAddr().getIpv4().getPort());
+        assertNull(uri[0]);
+        assertArrayEquals(new short[]{1, 2, 3, 4}, ep.getAddr().getIpv4().getAddress());
+        OCEndpointUtil.freeEndpoint(ep);
+
+        ep = OCEndpointUtil.stringToEndpoint("coaps+tcp://openconnectivity.org:3456", uri);
+        assertNotNull(ep);
+        assertTrue(OCTransportFlags.IPV4 == (ep.getFlags() & OCTransportFlags.IPV4) ||
+                   OCTransportFlags.IPV6 == (ep.getFlags() & OCTransportFlags.IPV6));
+        assertEquals(OCTransportFlags.SECURED, ep.getFlags() & OCTransportFlags.SECURED);
+        assertEquals(OCTransportFlags.TCP, ep.getFlags() & OCTransportFlags.TCP);
+        assertEquals(3456, ep.getAddr().getIpv4().getPort());
+        assertNull(uri[0]);
+        OCEndpointUtil.freeEndpoint(ep);
+
+        ep = OCEndpointUtil.stringToEndpoint("coap+tcp://[ff02::158]", uri);
+        assertNotNull(ep);
+        assertEquals(OCTransportFlags.IPV6, (ep.getFlags() & OCTransportFlags.IPV6));
+        assertNotEquals(OCTransportFlags.SECURED, (ep.getFlags() & OCTransportFlags.SECURED));
+        assertEquals(OCTransportFlags.TCP, (ep.getFlags() & OCTransportFlags.TCP));
+        assertEquals(5683, ep.getAddr().getIpv6().getPort());
+        assertNull(uri[0]);
+        assertArrayEquals(new short[]{0xff, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0x58},
+                          ep.getAddr().getIpv6().getAddress());
+        OCEndpointUtil.freeEndpoint(ep);
+
+        ep = OCEndpointUtil.stringToEndpoint("coaps+tcp://[ff02::158]/a/light", uri);
+        assertNotNull(ep);
+        assertEquals(OCTransportFlags.IPV6, (ep.getFlags() & OCTransportFlags.IPV6));
+        assertEquals(OCTransportFlags.SECURED, (ep.getFlags() & OCTransportFlags.SECURED));
+        assertEquals(OCTransportFlags.TCP, (ep.getFlags() & OCTransportFlags.TCP));
+        assertEquals(5684, ep.getAddr().getIpv6().getPort());
+        assertEquals("/a/light", uri[0]);
+        assertArrayEquals(new short[]{0xff, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0x58},
+                          ep.getAddr().getIpv6().getAddress());
+        OCEndpointUtil.freeEndpoint(ep);
+
+        ep = OCEndpointUtil.stringToEndpoint("coaps+tcp://[fe80::12]:2439/a/light", uri);
+        assertNotNull(ep);
+        assertEquals(OCTransportFlags.IPV6, (ep.getFlags() & OCTransportFlags.IPV6));
+        assertEquals(OCTransportFlags.SECURED, (ep.getFlags() & OCTransportFlags.SECURED));
+        assertEquals(OCTransportFlags.TCP, (ep.getFlags() & OCTransportFlags.TCP));
+        assertEquals(2439, ep.getAddr().getIpv6().getPort());
+        assertEquals("/a/light", uri[0]);
+        assertArrayEquals(new short[]{0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x12},
+                          ep.getAddr().getIpv6().getAddress());
+        OCEndpointUtil.freeEndpoint(ep);
+    }
+
+}
