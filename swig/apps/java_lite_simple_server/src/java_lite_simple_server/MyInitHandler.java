c1f219b36d21af2fc5de29bfa7c88a90cd6c1370
George Nash <george.nash@intel.com>
2019-08-28 09:19:23 -0700

    Add multiple devices to server sample
    Change-Id: I9c956509c127ecc01946129c2449d484d7145b0d
    Signed-off-by: George Nash <george.nash@intel.com>

--- swig/apps/java_lite_simple_server/src/java_lite_simple_server/MyInitHandler.java
+++ swig/apps/java_lite_simple_server/src/java_lite_simple_server/MyInitHandler.java
@@ -8,11 +8,25 @@ public class MyInitHandler implements OCMainInitHandler {
         System.out.println("inside MyInitHandler.initialize()");
         int ret = OCMain.initPlatform("Intel");
         ret |= OCMain.addDevice("/oic/d", "oic.d.light", "Lamp", "ocf.1.0.0", "ocf.res.1.0.0");
+        ret |= OCMain.addDevice("/oic/d", "oic.d.refrigeration", "My fridge", "ocf.1.0.0", "ocf.res.1.0.0");
+        ret |= OCMain.addDevice("/oic/d", "oic.d.thermostat", "My thermostat", "ocf.1.0.0", "ocf.res.1.0.0");
+
+        // Initialize light device values
         Light.name = "John's Light";
         Light.power = 0;
         Light.state = false;
+
         Counter.name = "John's Counter";
         Counter.count = 0;
+
+        // Initialize fridge device values
+        Fridge.filter = 0;
+        Fridge.rapidFreeze = false;
+        Fridge.defrost = false;
+        Fridge.rapidCool = false;
+
+        // Initialize Thermostat
+        Thermostat.temperature = 0.0;
 
         OCMain.setRandomPinHandler(new RandomPinHandler());
         return ret;
@@ -20,6 +34,7 @@ public class MyInitHandler implements OCMainInitHandler {
 
     @Override
     public void registerResources() {
+        // Setup resources for Device 0 'oic.d.light`
         System.out.println("inside MyInitHandler.registerResources()");
         OCResource resource = OCMain.newResource("", "/a/light", (short) 2, 0);
         OCMain.resourceBindResourceType(resource, "core.light");
@@ -65,6 +80,28 @@ public class MyInitHandler implements OCMainInitHandler {
         OCLink link2 = OCMain.newLink(resource2);
         OCMain.collectionAddLink(collection, link2);
         OCMain.addCollection(collection);
+
+        // Setup resources for Device 1 'oic.d.refrigeration`
+        OCResource resource3 = OCMain.newResource("myfridge", "/fridge/1", (short) 1, 1);
+        OCMain.resourceBindResourceType(resource3, "oic.r.refrigeration");
+        OCMain.resourceBindResourceInterface(resource3, OCInterfaceMask.A);
+        OCMain.resourceSetDefaultInterface(resource3, OCInterfaceMask.A);
+        OCMain.resourceSetDiscoverable(resource3, true);
+        OCMain.resourceSetPeriodicObservable(resource3, 1);
+        OCMain.resourceSetRequestHandler(resource3, OCMethod.OC_GET, new GetFridge());
+        OCMain.resourceSetRequestHandler(resource3, OCMethod.OC_POST, new PostFridge());
+        OCMain.addResource(resource3);
+
+        // Setup resources for Device 2 'oic.d.thermostat`
+        OCResource resource4 = OCMain.newResource("tempsetter", "/temp/1", (short) 1, 2);
+        OCMain.resourceBindResourceType(resource4, "oic.r.temperature");
+        OCMain.resourceBindResourceInterface(resource4, OCInterfaceMask.A | OCInterfaceMask.S);
+        OCMain.resourceSetDefaultInterface(resource4, OCInterfaceMask.A);
+        OCMain.resourceSetDiscoverable(resource4, true);
+        OCMain.resourceSetPeriodicObservable(resource4, 1);
+        OCMain.resourceSetRequestHandler(resource4, OCMethod.OC_GET, new GetTemp());
+        OCMain.resourceSetRequestHandler(resource4, OCMethod.OC_POST, new PostTemp());
+        OCMain.addResource(resource4);
     }
 
     @Override
