485125a78ae5712ecd90ca256100516d9c5eff0a
Aleksey Volkov <a.volkov@samsung.com>
2019-09-04 11:29:05 +0300

    [LITE-76] Check rdp identity in TLS handshake
    Compare peer identity and "oic.sec.doxm.rdp"
    before PPSK generation at the random pin otm
    process
    
    Signed-off-by: Aleksey Volkov <a.volkov@samsung.com>
    Change-Id: Ic391133dfc241165ac7c0db2b064c68ed567996f

--- security/oc_tls.c
+++ security/oc_tls.c
@@ -491,10 +491,13 @@ get_psk_cb(void *data, mbedtls_ssl_context *ssl, const unsigned char *identity,
       OC_DBG("oc_tls: Set peer credential to SSL handle");
       return 0;
     } else {
-      OC_DBG("oc_tls: deriving PPSK for PIN OTM");
       oc_sec_doxm_t *doxm = oc_sec_get_doxm(peer->endpoint.device);
       oc_sec_pstat_t *ps = oc_sec_get_pstat(peer->endpoint.device);
-      if (ps->s == OC_DOS_RFOTM && doxm->oxmsel == OC_OXMTYPE_RDP) {
+      if (ps->s == OC_DOS_RFOTM
+          && doxm->oxmsel == OC_OXMTYPE_RDP
+          && identity_len == 16
+          && memcmp(identity, "oic.sec.doxm.rdp", 16)== 0) {
+        OC_DBG("oc_tls: deriving PPSK for PIN OTM");
         memcpy(peer->uuid.id, identity, 16);
 
         uint8_t key[16];
