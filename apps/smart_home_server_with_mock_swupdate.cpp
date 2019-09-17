1f27429ce57a98554b65907340becc2f77d8f408
Kishen Maloor <kishen.maloor@intel.com>
2019-09-17 16:01:40 -0700

    Add mock implementation of SW Update interfaces
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Change-Id: I1eb2a3e67b77d15c8ed465209b670ed4aba435b9

---
+++ apps/smart_home_server_with_mock_swupdate.cpp
@@ -0,0 +1,352 @@
+/*
+// Copyright (c) 2017-2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+*/
+
+#include "oc_api.h"
+#include "oc_pki.h"
+#include "oc_swupdate.h"
+#include "port/oc_clock.h"
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+
+static pthread_mutex_t mutex;
+static pthread_cond_t cv;
+static struct timespec ts;
+static int quit = 0;
+
+static bool switch_state;
+
+#ifdef OC_SOFTWARE_UPDATE
+#include <boost/network/uri.hpp>
+#include <iostream>
+using namespace boost::network;
+
+int
+validate_purl(const char *purl)
+{
+  uri::uri instance(purl);
+  if (instance.is_valid() == 0) {
+    return -1;
+  }
+  return 0;
+}
+
+int
+check_new_version(size_t device, const char *url, const char *version)
+{
+  if (!url) {
+    oc_swupdate_notify_done(device, OC_SWUPDATE_RESULT_INVALID_URL);
+    return -1;
+  }
+  PRINT("Package url %s\n", url);
+  if (version) {
+    PRINT("Package version: %s\n", version);
+  }
+  oc_swupdate_notify_new_version_available(device, "2.0",
+                                           OC_SWUPDATE_RESULT_SUCCESS);
+  return 0;
+}
+
+int
+download_update(size_t device, const char *url)
+{
+  (void)url;
+  oc_swupdate_notify_downloaded(device, "2.0", OC_SWUPDATE_RESULT_SUCCESS);
+  return 0;
+}
+
+int
+perform_upgrade(size_t device, const char *url)
+{
+  (void)url;
+  oc_swupdate_notify_upgrading(device, "2.0", oc_clock_time(),
+                               OC_SWUPDATE_RESULT_SUCCESS);
+
+  oc_swupdate_notify_done(device, OC_SWUPDATE_RESULT_SUCCESS);
+  return 0;
+}
+#endif /* OC_SOFTWARE_UPDATE */
+
+static int
+app_init(void)
+{
+  int err = oc_init_platform("Intel", NULL, NULL);
+
+  err |= oc_add_device("/oic/d", "oic.d.switch", "binary_switch", "ocf.2.4.0",
+                       "ocf.res.1.3.0,ocf.sh.1.3.0", NULL, NULL);
+  return err;
+}
+
+static void
+get_switch(oc_request_t *request, oc_interface_mask_t iface_mask,
+           void *user_data)
+{
+  (void)user_data;
+  PRINT("GET_switch:\n");
+  oc_rep_start_root_object();
+  switch (iface_mask) {
+  case OC_IF_BASELINE:
+    oc_process_baseline_interface(request->resource);
+  /* fall through */
+  case OC_IF_A:
+    oc_rep_set_boolean(root, value, switch_state);
+    break;
+  default:
+    break;
+  }
+  oc_rep_end_root_object();
+
+  oc_send_response(request, OC_STATUS_OK);
+}
+
+static void
+post_switch(oc_request_t *request, oc_interface_mask_t iface_mask,
+            void *user_data)
+{
+  (void)iface_mask;
+  (void)user_data;
+  PRINT("POST_switch:\n");
+  bool state = false, bad_request = false;
+  oc_rep_t *rep = request->request_payload;
+  while (rep != NULL) {
+    switch (rep->type) {
+    case OC_REP_BOOL:
+      state = rep->value.boolean;
+      break;
+    default:
+      if (oc_string_len(rep->name) > 2) {
+        if (strncmp(oc_string(rep->name), "x.", 2) == 0) {
+          break;
+        }
+      }
+      bad_request = true;
+      break;
+    }
+    rep = rep->next;
+  }
+
+  if (!bad_request) {
+    switch_state = state;
+  }
+
+  oc_rep_start_root_object();
+  oc_rep_set_boolean(root, value, switch_state);
+  oc_rep_end_root_object();
+
+  if (!bad_request) {
+    oc_send_response(request, OC_STATUS_CHANGED);
+  } else {
+    oc_send_response(request, OC_STATUS_BAD_REQUEST);
+  }
+}
+
+static void
+register_resources(void)
+{
+  oc_resource_t *bswitch = oc_new_resource(NULL, "/switch", 1, 0);
+  oc_resource_bind_resource_type(bswitch, "oic.r.switch.binary");
+  oc_resource_bind_resource_interface(bswitch, OC_IF_A);
+  oc_resource_set_default_interface(bswitch, OC_IF_A);
+  oc_resource_set_discoverable(bswitch, true);
+  oc_resource_set_request_handler(bswitch, OC_GET, get_switch, NULL);
+  oc_resource_set_request_handler(bswitch, OC_POST, post_switch, NULL);
+  oc_add_resource(bswitch);
+}
+
+static void
+signal_event_loop(void)
+{
+  pthread_mutex_lock(&mutex);
+  pthread_cond_signal(&cv);
+  pthread_mutex_unlock(&mutex);
+}
+
+static void
+handle_signal(int signal)
+{
+  (void)signal;
+  signal_event_loop();
+  quit = 1;
+}
+
+#ifdef OC_SECURITY
+void
+random_pin_cb(const unsigned char *pin, size_t pin_len, void *data)
+{
+  (void)data;
+  PRINT("\n\nRandom PIN: %.*s\n\n", pin_len, pin);
+}
+#endif /* OC_SECURITY */
+
+#if defined(OC_SECURITY) && defined(OC_PKI)
+static int
+read_pem(const char *file_path, char *buffer, size_t *buffer_len)
+{
+  FILE *fp = fopen(file_path, "r");
+  if (fp == NULL) {
+    PRINT("ERROR: unable to read PEM\n");
+    return -1;
+  }
+  if (fseek(fp, 0, SEEK_END) != 0) {
+    PRINT("ERROR: unable to read PEM\n");
+    fclose(fp);
+    return -1;
+  }
+  long pem_len = ftell(fp);
+  if (pem_len < 0) {
+    PRINT("ERROR: could not obtain length of file\n");
+    fclose(fp);
+    return -1;
+  }
+  if (pem_len > (long)*buffer_len) {
+    PRINT("ERROR: buffer provided too small\n");
+    fclose(fp);
+    return -1;
+  }
+  if (fseek(fp, 0, SEEK_SET) != 0) {
+    PRINT("ERROR: unable to read PEM\n");
+    fclose(fp);
+    return -1;
+  }
+  if (fread(buffer, 1, pem_len, fp) < (size_t)pem_len) {
+    PRINT("ERROR: unable to read PEM\n");
+    fclose(fp);
+    return -1;
+  }
+  fclose(fp);
+  *buffer_len = (size_t)pem_len;
+  return 0;
+}
+#endif /* OC_SECURITY && OC_PKI */
+
+void
+factory_presets_cb(size_t device, void *data)
+{
+  (void)device;
+  (void)data;
+#if defined(OC_SECURITY) && defined(OC_PKI)
+  char cert[8192];
+  size_t cert_len = 8192;
+  if (read_pem("pki_certs/ee.pem", cert, &cert_len) < 0) {
+    PRINT("ERROR: unable to read certificates\n");
+    return;
+  }
+
+  char key[4096];
+  size_t key_len = 4096;
+  if (read_pem("pki_certs/key.pem", key, &key_len) < 0) {
+    PRINT("ERROR: unable to read private key");
+    return;
+  }
+
+  int ee_credid = oc_pki_add_mfg_cert(0, (const unsigned char *)cert, cert_len,
+                                      (const unsigned char *)key, key_len);
+
+  if (ee_credid < 0) {
+    PRINT("ERROR installing manufacturer EE cert\n");
+    return;
+  }
+
+  cert_len = 8192;
+  if (read_pem("pki_certs/subca1.pem", cert, &cert_len) < 0) {
+    PRINT("ERROR: unable to read certificates\n");
+    return;
+  }
+
+  int subca_credid = oc_pki_add_mfg_intermediate_cert(
+    0, ee_credid, (const unsigned char *)cert, cert_len);
+
+  if (subca_credid < 0) {
+    PRINT("ERROR installing intermediate CA cert\n");
+    return;
+  }
+
+  cert_len = 8192;
+  if (read_pem("pki_certs/rootca1.pem", cert, &cert_len) < 0) {
+    PRINT("ERROR: unable to read certificates\n");
+    return;
+  }
+
+  int rootca_credid =
+    oc_pki_add_mfg_trust_anchor(0, (const unsigned char *)cert, cert_len);
+  if (rootca_credid < 0) {
+    PRINT("ERROR installing root cert\n");
+    return;
+  }
+
+  oc_pki_set_security_profile(0, OC_SP_BLACK, OC_SP_BLACK, ee_credid);
+#endif /* OC_SECURITY && OC_PKI */
+}
+
+int
+main(void)
+{
+  int init;
+  struct sigaction sa;
+  sigfillset(&sa.sa_mask);
+  sa.sa_flags = 0;
+  sa.sa_handler = handle_signal;
+  sigaction(SIGINT, &sa, NULL);
+
+  static oc_handler_t handler;
+  handler.init = app_init;
+  handler.signal_event_loop = signal_event_loop;
+  handler.register_resources = register_resources;
+
+  oc_clock_time_t next_event;
+  oc_set_con_res_announced(false);
+  oc_set_mtu_size(16384);
+  oc_set_max_app_data_size(16384);
+
+#ifdef OC_SECURITY
+  oc_storage_config("./smart_home_server_with_mock_swupdate_creds");
+#endif /* OC_SECURITY */
+
+  oc_set_factory_presets_cb(factory_presets_cb, NULL);
+#ifdef OC_SECURITY
+  oc_set_random_pin_callback(random_pin_cb, NULL);
+#endif /* OC_SECURITY */
+
+#ifdef OC_SOFTWARE_UPDATE
+  static oc_swupdate_cb_t swupdate_impl;
+  swupdate_impl.validate_purl = validate_purl;
+  swupdate_impl.check_new_version = check_new_version;
+  swupdate_impl.download_update = download_update;
+  swupdate_impl.perform_upgrade = perform_upgrade;
+  oc_swupdate_set_impl(&swupdate_impl);
+#endif /* OC_SOFTWARE_UPDATE */
+
+  init = oc_main_init(&handler);
+  if (init < 0)
+    return init;
+
+  while (quit != 1) {
+    next_event = oc_main_poll();
+    pthread_mutex_lock(&mutex);
+    if (next_event == 0) {
+      pthread_cond_wait(&cv, &mutex);
+    } else {
+      ts.tv_sec = (next_event / OC_CLOCK_SECOND);
+      ts.tv_nsec = (next_event % OC_CLOCK_SECOND) * 1.e09 / OC_CLOCK_SECOND;
+      pthread_cond_timedwait(&cv, &mutex, &ts);
+    }
+    pthread_mutex_unlock(&mutex);
+  }
+
+  oc_main_shutdown();
+
+  return 0;
+}
