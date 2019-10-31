c6df5d0ff259b3ceb52b0335be207fd217ffc20c
Larry Sachs <larry.j.sachs@intel.com>
2019-10-31 13:00:13 -0700

    Multi-Device Server for Android
    Change-Id: I8d525213befc11783520cae0dc8c231cbe00ff31
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ swig/apps/oc/android_multi_device_server/MultiDeviceServer/app/src/main/java/org/iotivity/multideviceserver/InitHandler.java
@@ -0,0 +1,118 @@
+package org.iotivity.multideviceserver;
+
+import android.util.Log;
+
+import org.iotivity.OCInterfaceMask;
+import org.iotivity.OCMainInitHandler;
+import org.iotivity.OCUuidUtil;
+import org.iotivity.oc.OcDevice;
+import org.iotivity.oc.OcPlatform;
+import org.iotivity.oc.OcResource;
+import org.iotivity.oc.OcUtils;
+
+public class InitHandler implements OCMainInitHandler {
+
+    private static final String TAG = InitHandler.class.getSimpleName();
+
+    private ServerActivity activity;
+    private OcPlatform ocPlatform;
+
+    private OcDevice lightDevice;
+    private OcDevice refrigeratorDevice;
+    private OcDevice thermostatDevice;
+
+    private Light light;
+    private Refrigerator refrigerator;
+    private Thermostat thermostat;
+
+    public InitHandler(ServerActivity activity, OcPlatform ocPlatform) {
+        this.activity = activity;
+        this.ocPlatform = ocPlatform;
+    }
+
+    @Override
+    public int initialize() {
+        Log.d(TAG, "inside InitHandler.initialize()");
+        int ret = ocPlatform.platformInit("Intel");
+        if (ret >= 0) {
+            // Create the devices and add them to the platform
+            lightDevice = new OcDevice("/oic/d", "oic.d.light", "Lamp", "ocf.1.0.0", "ocf.res.1.0.0");
+            refrigeratorDevice = new OcDevice("/oic/d", "oic.d.refrigerator", "Refrigerator", "ocf.1.0.0", "ocf.res.1.0.0");
+            thermostatDevice = new OcDevice("/oic/d", "oic.d.thermostat", "Thermostat", "ocf.1.0.0", "ocf.res.1.0.0");
+
+            ret |= ocPlatform.addDevice(lightDevice);
+            ret |= ocPlatform.addDevice(refrigeratorDevice);
+            ret |= ocPlatform.addDevice(thermostatDevice);
+
+            light = new Light(lightDevice.getName());
+            refrigerator = new Refrigerator(refrigeratorDevice.getName());
+            thermostat = new Thermostat(thermostatDevice.getName());
+
+        } else {
+            Log.e(TAG, "Error in platformInit, return value = " + ret);
+        }
+
+        OcUtils.setRandomPinHandler(new RandomPinHandler(activity));
+        return ret;
+    }
+
+    @Override
+    public void registerResources() {
+        Log.d(TAG, "inside InitHandler.registerResources()");
+
+        // Create the resources and add them to their device
+
+        String[] lightResourceTypes = new String[]{"oic.r.switch.binary", "oic.r.light.dimming"};
+        int[] lightInterfaceMasks = new int[]{OCInterfaceMask.RW};
+
+        String[] refrigeratorResourceTypes = new String[]{"oic.r.refrigeration"};
+        int[] refrigeratorInterfaceMasks = new int[]{OCInterfaceMask.A};
+
+        String[] thermostatResourceTypes = new String[]{"oic.r.temperature"};
+        int[] thermostatInterfaceMasks = new int[]{OCInterfaceMask.A, OCInterfaceMask.S};
+
+        if (lightDevice != null) {
+            OcResource lightResource = new OcResource(lightDevice, "light", "/a/light", lightResourceTypes, lightInterfaceMasks);
+            lightResource.setDefaultInterfaceMask(OCInterfaceMask.RW);
+            lightResource.setDiscoverable(true);
+            lightResource.setObservable(true);
+            lightResource.setPeriodicObservable(1);
+            lightResource.setGetRequestHandler(new GetLightRequestHandler(activity, light));
+            lightResource.setPutRequestHandler(new PutLightRequestHandler(activity, light));
+            lightResource.setPostRequestHandler(new PostLightRequestHandler(activity, light));
+            lightDevice.addResource(lightResource);
+        }
+
+        if (refrigeratorDevice != null) {
+            OcResource refrigeratorResource = new OcResource(refrigeratorDevice, "refrigerator", "/a/refrigerator", refrigeratorResourceTypes, refrigeratorInterfaceMasks);
+            refrigeratorResource.setDefaultInterfaceMask(OCInterfaceMask.A);
+            refrigeratorResource.setDiscoverable(true);
+            refrigeratorResource.setObservable(true);
+            refrigeratorResource.setPeriodicObservable(1);
+            refrigeratorResource.setGetRequestHandler(new GetRefrigeratorRequestHandler(activity, refrigerator));
+            refrigeratorResource.setPutRequestHandler(new PutRefrigeratorRequestHandler(activity, refrigerator));
+            refrigeratorResource.setPostRequestHandler(new PostRefrigeratorRequestHandler(activity, refrigerator));
+            refrigeratorDevice.addResource(refrigeratorResource);
+        }
+
+        if (thermostatDevice != null) {
+            OcResource thermostatResource = new OcResource(thermostatDevice, "thermostat", "/a/thermostat", thermostatResourceTypes, thermostatInterfaceMasks);
+            thermostatResource.setDefaultInterfaceMask(OCInterfaceMask.A);
+            thermostatResource.setDiscoverable(true);
+            thermostatResource.setObservable(true);
+            thermostatResource.setPeriodicObservable(1);
+            thermostatResource.setGetRequestHandler(new GetThermostatRequestHandler(activity, thermostat));
+            thermostatResource.setPutRequestHandler(new PutThermostatRequestHandler(activity, thermostat));
+            thermostatResource.setPostRequestHandler(new PostThermostatRequestHandler(activity, thermostat));
+            thermostatDevice.addResource(thermostatResource);
+        }
+    }
+
+    @Override
+    public void requestEntry() {
+        Log.d(TAG, "inside InitHandler.requestEntry()");
+        Log.d(TAG, "Light DeviceId = " + ((lightDevice != null) ? OCUuidUtil.uuidToString(lightDevice.getId()) : "null"));
+        Log.d(TAG, "Refrigerator DeviceId = " + ((refrigeratorDevice != null) ? OCUuidUtil.uuidToString(refrigeratorDevice.getId()) : "null"));
+        Log.d(TAG, "Thermostat DeviceId = " + ((thermostatDevice != null) ? OCUuidUtil.uuidToString(thermostatDevice.getId()) : "null"));
+    }
+}
