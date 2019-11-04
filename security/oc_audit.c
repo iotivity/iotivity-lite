8569bca772fe1415780dcb2f99cc199b4d9a6794
Oleksandr Andrieiev <o.andrieiev@samsung.com>
2019-11-04 03:43:06 -0500

    Initial draft of oc_audit usage
    Signed-off-by: Oleksandr Andrieiev <o.andrieiev@samsung.com>
    Change-Id: I2a2cfc5f74e41f1469d8ea1ff06e506a21107602

---
+++ security/oc_audit.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include "oc_audit.h"
+
+void oc_audit_log(char *message, uint8_t category, uint8_t priority, char **aux, size_t aux_len) {
+  OC_ERR("audit_log: %s %u %u", message, category, priority);
+  if (aux) {
+    size_t i;
+    for (i = 0; i < aux_len; ++i) {
+      OC_ERR("audit_log: %s", aux[i]);
+      free(aux[i]);
+    }
+    free(aux);
+  }
+}
