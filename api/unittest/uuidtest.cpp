aa9731049d43c66a48e4ff948bda858f38702656
Kishen Maloor <kishen.maloor@intel.com>
2019-11-11 20:14:09 -0800

    oc_uuid:Fix handling of wildcard UUIDs
    Signed-off-by: Kishen Maloor <kishen.maloor@intel.com>
    Change-Id: Iff10d5d59d0971ae122754b68debac4d66ce29a6

--- api/unittest/uuidtest.cpp
+++ api/unittest/uuidtest.cpp
@@ -27,9 +27,41 @@
 
 TEST(UUIDGeneration, StrToUUIDTest_P)
 {
-    oc_uuid_t uuid;
-    memset(&uuid, 0, sizeof(oc_uuid_t));
-    oc_uuid_t uuidTemp = uuid;
-    oc_str_to_uuid(UUID, &uuid);
-    EXPECT_NE(uuid.id, uuidTemp.id);
+  oc_uuid_t uuid;
+  memset(&uuid, 0, sizeof(oc_uuid_t));
+  oc_uuid_t uuidTemp = uuid;
+  oc_str_to_uuid(UUID, &uuid);
+  EXPECT_NE(uuid.id, uuidTemp.id);
+}
+
+TEST(UUIDGeneration, WildcardStrToUUID)
+{
+  const char *u = "*";
+  oc_uuid_t uuid;
+  oc_str_to_uuid(u, &uuid);
+  oc_uuid_t wc = { { 0 } };
+  wc.id[0] = '*';
+  EXPECT_EQ(memcmp(wc.id, uuid.id, 16), 0);
+}
+
+TEST(UUIDGeneration, WildcardUUIDToStr)
+{
+  const char *u = "*";
+  char wc[37];
+  oc_uuid_t uuid = { { 0 } };
+  uuid.id[0] = '*';
+  oc_uuid_to_str(&uuid, wc, 37);
+  EXPECT_EQ(strlen(u), strlen(wc));
+  EXPECT_EQ(memcmp(u, wc, strlen(u)), 0);
+}
+
+TEST(UUIDGeneration, NonWildcardUUID)
+{
+  const char *u = "2ef07d57-b2e3-4120-9292-f9fef16b41d7";
+  char nonwc[37];
+  oc_uuid_t uuid;
+  oc_str_to_uuid(u, &uuid);
+  oc_uuid_to_str(&uuid, nonwc, 37);
+  EXPECT_EQ(strlen(u), strlen(nonwc));
+  EXPECT_EQ(memcmp(u, nonwc, strlen(u)), 0);
 }
