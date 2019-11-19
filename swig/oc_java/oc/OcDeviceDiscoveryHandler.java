bb9be930ab2accabe790cdfa6e0a0a4c8c59ffdf
Larry Sachs <larry.j.sachs@intel.com>
2019-11-19 12:26:36 -0800

    Additional work for Android Multi-Device Client
    Moves the discovery logic previously in the client code
    to the swig/oc_java/oc layer.  Added api for a single call
    in OcUtils to discover all devices (and their resources).  Once the
    resource discovery is complete the fully populated device
    is handed to the client application's handler.
    
    Change-Id: Ib71bc6d6f32a268fc143620346578f77819dcbc7
    Signed-off-by: Larry Sachs <larry.j.sachs@intel.com>

---
+++ swig/oc_java/oc/OcDeviceDiscoveryHandler.java
@@ -0,0 +1,5 @@
+package org.iotivity.oc;
+
+public interface OcDeviceDiscoveryHandler {
+    public void discoveredDevice(OcRemoteDevice remoteDevice);
+}
---
+++ swig/oc_java/oc/OcGetRemoteResourcesHandler.java
@@ -0,0 +1,38 @@
+package org.iotivity.oc;
+
+import org.iotivity.*;
+
+public class OcGetRemoteResourcesHandler implements OCDiscoveryAllHandler {
+
+    private OcRemoteDevice device;
+    private OcDeviceDiscoveryHandler deviceDiscoveryHandler;
+
+    public OcGetRemoteResourcesHandler(OcRemoteDevice device, OcDeviceDiscoveryHandler deviceDiscoveryHandler) {
+        this.device = device;
+        this.deviceDiscoveryHandler = deviceDiscoveryHandler;
+    }
+
+    @Override
+    public OCDiscoveryFlags handler(String anchor, String uri, String[] types, int interfaceMask, OCEndpoint endpoints,
+            int resourcePropertiesMask, boolean more) {
+
+        OcRemoteResource resource = new OcRemoteResource(anchor, uri, types, interfaceMask, resourcePropertiesMask);
+
+        OCEndpoint ep = endpoints;
+        while (ep != null) {
+            String endpointStr = OcUtils.endpointToString(ep);
+            resource.addEndpoint(endpointStr);
+            ep = ep.getNext();
+        }
+
+        device.addResource(resource);
+
+        if (!more) {
+            deviceDiscoveryHandler.discoveredDevice(device);
+
+            return OCDiscoveryFlags.OC_STOP_DISCOVERY;
+        }
+
+        return OCDiscoveryFlags.OC_CONTINUE_DISCOVERY;
+    }
+}
