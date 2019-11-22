1c64c96e55acc6f89e85ee279645d967cafa3e33
George Nash <george.nash@intel.com>
2019-11-22 13:17:53 -0800

    Add cleanup to shutdown code
    The code now joins the threads and waits till the thread
    has completed running.
    
    It then clears the jni_list that is used by the jni code to
    map C callbacks to Java callback methods.
    
    Change-Id: I96ea6205274a4003d2486232f6f2298b2a2064cc
    Signed-off-by: George Nash <george.nash@intel.com>

--- swig/java_lang/oc_iotivity_lite_jni.c
+++ swig/java_lang/oc_iotivity_lite_jni.c
@@ -370,6 +370,23 @@ jni_list_remove(jni_callback_data *item)
   OC_DBG("JNI: - unlock %s\n", __func__);
 }
 
+void
+jni_list_clear()
+{
+  OC_DBG("JNI: - lock %s\n", __func__);
+  jni_mutex_lock(jni_sync_lock);
+  jni_callback_data *item = jni_list_get_head();
+  while (item) {
+    JCALL1(DeleteGlobalRef, item->jenv, item->jcb_obj);
+    oc_list_remove(jni_callbacks, item);
+    free(item);
+    item = NULL;
+    item = jni_list_get_head();
+  }
+  jni_mutex_unlock(jni_sync_lock);
+  OC_DBG("JNI: - unlock %s\n", __func__);
+}
+
 jni_callback_data *
 jni_list_get_item_by_java_callback(jobject callback)
 {
