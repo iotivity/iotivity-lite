ed31a4dcbca160a4be72dfa8d7778ee316b8a100
Andy Dolan <A.Dolan@cablelabs.com>
2020-07-06 13:54:50 -0600

    Add ACE provisioning from obtmain

--- apps/dpp_diplomat/remote_obt.c
+++ apps/dpp_diplomat/remote_obt.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 
 #define MAX_NUM_DEVICES (50)
+#define MAX_NUM_RESOURCES (100)
 #define SCANF(...)                                                             \
   do {                                                                         \
     if (scanf(__VA_ARGS__) <= 0) {                                             \
@@ -210,6 +211,20 @@ otm_just_works_cb(oc_uuid_t *uuid, int status, void *data)
   }
 }
 
+static void
+provision_ace2_cb(oc_uuid_t *uuid, int status, void *data)
+{
+  (void)data;
+  char di[37];
+  oc_uuid_to_str(uuid, di, 37);
+
+  if (status >= 0) {
+    PRINT("\nSuccessfully provisioned ACE to device %s\n", di);
+  } else {
+    PRINT("\nERROR provisioning ACE to device %s\n", di);
+  }
+}
+
 /* Locally invoked functions */
 static void
 display_menu(void)
@@ -219,6 +234,7 @@ display_menu(void)
   PRINT("1. Discover unowned devices\n");
   PRINT("2. Discover owned devices\n");
   PRINT("3. Perform JW OTM\n");
+  PRINT("4. Provision ACE2\n");
   PRINT("99. Exit\n");
 }
 
@@ -283,6 +299,199 @@ otm_just_works(void)
   }
 
   pthread_mutex_unlock(&app_lock);
+}
+
+static void
+provision_ace2(void)
+{
+  if (oc_list_length(owned_devices) == 0) {
+    PRINT("\n\nPlease Re-Discover Owned devices\n");
+    return;
+  }
+
+  const char *conn_types[2] = { "anon-clear", "auth-crypt" };
+  int num_resources = 0;
+
+  device_handle_t *devices[MAX_NUM_DEVICES];
+  device_handle_t *device = (device_handle_t *)oc_list_head(owned_devices);
+  int i = 0, dev, sub;
+
+  PRINT("\nProvision ACL2\nMy Devices:\n");
+  while (device != NULL) {
+    devices[i] = device;
+    char di[OC_UUID_LEN];
+    oc_uuid_to_str(&device->uuid, di, OC_UUID_LEN);
+    PRINT("[%d]: %s - %s\n", i, di, device->device_name);
+    i++;
+    device = device->next;
+  }
+
+  if (i == 0) {
+    PRINT("\nNo devices to provision.. Please Re-Discover Owned devices.\n");
+    return;
+  }
+
+  PRINT("\n\nSelect device for provisioning: ");
+  SCANF("%d", &dev);
+  if (dev < 0 || dev >= i) {
+    PRINT("ERROR: Invalid selection\n");
+    return;
+  }
+
+  PRINT("\nSubjects:");
+  device = (device_handle_t *)oc_list_head(owned_devices);
+  PRINT("\n[0]: %s\n", conn_types[0]);
+  PRINT("[1]: %s\n", conn_types[1]);
+  PRINT("[2]: Role\n");
+  i = 0;
+  while (device != NULL) {
+    char di[OC_UUID_LEN];
+    oc_uuid_to_str(&device->uuid, di, OC_UUID_LEN);
+    PRINT("[%d]: %s - %s\n", i + 3, di, device->device_name);
+    i++;
+    device = device->next;
+
+    if (!device) {
+      oc_uuid_to_str(oc_core_get_device_id(0), di, OC_UUID_LEN);
+      PRINT("[%d]: %s - (OBT)\n", i + 3, di);
+      i++;
+    }
+  }
+  PRINT("\nSelect subject: ");
+  SCANF("%d", &sub);
+
+  if (sub >= (i + 3)) {
+    PRINT("ERROR: Invalid selection\n");
+    return;
+  }
+
+  oc_sec_ace_t *ace = NULL;
+  if (sub > 2) {
+    if (sub == (i + 2)) {
+      ace = oc_obt_new_ace_for_subject(oc_core_get_device_id(0));
+    } else {
+      ace = oc_obt_new_ace_for_subject(&devices[sub - 3]->uuid);
+    }
+  } else {
+    if (sub == 0) {
+      ace = oc_obt_new_ace_for_connection(OC_CONN_ANON_CLEAR);
+    } else if (sub == 1) {
+      ace = oc_obt_new_ace_for_connection(OC_CONN_AUTH_CRYPT);
+    } else {
+      char role[64];
+      PRINT("\nEnter role: ");
+      SCANF("%63s", role);
+      int d;
+      PRINT("\nAuthority? [0-No, 1-Yes]: ");
+      SCANF("%d", &d);
+      if (d == 1) {
+        char authority[64];
+        PRINT("\nEnter Authority: ");
+        SCANF("%63s", authority);
+        ace = oc_obt_new_ace_for_role(role, authority);
+      } else {
+        ace = oc_obt_new_ace_for_role(role, NULL);
+      }
+    }
+  }
+
+  if (!ace) {
+    PRINT("\nERROR: Could not create ACE\n");
+    return;
+  }
+
+  while (num_resources <= 0 || num_resources > MAX_NUM_RESOURCES) {
+    if (num_resources != 0) {
+      PRINT("\n\nERROR: Enter valid number\n\n");
+    }
+    PRINT("\nEnter number of resources in this ACE: ");
+    SCANF("%d", &num_resources);
+  }
+
+  int c;
+  PRINT("\nResource properties\n");
+  i = 0;
+  while (i < num_resources) {
+    oc_ace_res_t *res = oc_obt_ace_new_resource(ace);
+
+    if (!res) {
+      PRINT("\nERROR: Could not allocate new resource for ACE\n");
+      oc_obt_free_ace(ace);
+      return;
+    }
+
+    PRINT("Have resource href? [0-No, 1-Yes]: ");
+    SCANF("%d", &c);
+    if (c == 1) {
+      PRINT("Enter resource href (eg. /a/light): ");
+      char href[64];
+      SCANF("%63s", href);
+
+      oc_obt_ace_resource_set_href(res, href);
+      oc_obt_ace_resource_set_wc(res, OC_ACE_NO_WC);
+    } else {
+      PRINT("\nSet wildcard resource? [0-No, 1-Yes]: ");
+      SCANF("%d", &c);
+      if (c == 1) {
+        PRINT("[1]: All NCRs '*' \n"
+              "[2]: All NCRs with >=1 secured endpoint '+'\n"
+              "[3]: All NCRs with >=1 unsecured endpoint '-'\n"
+              "\nSelect wildcard resource: ");
+        SCANF("%d", &c);
+        switch (c) {
+        case 1:
+          oc_obt_ace_resource_set_wc(res, OC_ACE_WC_ALL);
+          break;
+        case 2:
+          oc_obt_ace_resource_set_wc(res, OC_ACE_WC_ALL_SECURED);
+          break;
+        case 3:
+          oc_obt_ace_resource_set_wc(res, OC_ACE_WC_ALL_PUBLIC);
+          break;
+        default:
+          break;
+        }
+      }
+    }
+    i++;
+  }
+
+  PRINT("\nSet ACE2 permissions\n");
+  PRINT("CREATE [0-No, 1-Yes]: ");
+  SCANF("%d", &c);
+  if (c == 1) {
+    oc_obt_ace_add_permission(ace, OC_PERM_CREATE);
+  }
+  PRINT("RETRIEVE [0-No, 1-Yes]: ");
+  SCANF("%d", &c);
+  if (c == 1) {
+    oc_obt_ace_add_permission(ace, OC_PERM_RETRIEVE);
+  }
+  PRINT("UPDATE [0-No, 1-Yes]: ");
+  SCANF("%d", &c);
+  if (c == 1) {
+    oc_obt_ace_add_permission(ace, OC_PERM_UPDATE);
+  }
+  PRINT("DELETE [0-No, 1-Yes]: ");
+  SCANF("%d", &c);
+  if (c == 1) {
+    oc_obt_ace_add_permission(ace, OC_PERM_DELETE);
+  }
+  PRINT("NOTIFY [0-No, 1-Yes]: ");
+  SCANF("%d", &c);
+  if (c == 1) {
+    oc_obt_ace_add_permission(ace, OC_PERM_NOTIFY);
+  }
+
+  pthread_mutex_lock(&app_lock);
+  int ret =
+    oc_obt_provision_ace(&devices[dev]->uuid, ace, provision_ace2_cb, NULL);
+  pthread_mutex_unlock(&app_lock);
+  if (ret >= 0) {
+    PRINT("\nSuccessfully issued request to provision ACE\n");
+  } else {
+    PRINT("\nERROR issuing request to provision ACE\n");
+  }
 }
 
 /* TODO: Implement onboarding kick-off.
@@ -387,6 +596,9 @@ main(void)
         break;
       case 3:
         otm_just_works();
+        break;
+      case 4:
+        provision_ace2();
         break;
       case 99:
         handle_signal(0);
