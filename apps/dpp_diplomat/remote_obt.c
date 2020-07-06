9e0971132b618491e38ea309d2c6671f13737b37
Andy Dolan <A.Dolan@cablelabs.com>
2020-07-06 13:23:51 -0600

    Reorder functions in remote OBT

--- apps/dpp_diplomat/remote_obt.c
+++ apps/dpp_diplomat/remote_obt.c
@@ -18,6 +18,21 @@
     }                                                                          \
   } while (0)
 
+/* Structure in app to track currently discovered owned/unowned devices */
+typedef struct device_handle_t
+{
+  struct device_handle_t *next;
+  oc_uuid_t uuid;
+  char device_name[64];
+} device_handle_t;
+
+/* Pool of device handles */
+OC_MEMB(device_handles, device_handle_t, MAX_OWNED_DEVICES);
+/* List of known owned devices */
+OC_LIST(owned_devices);
+/* List of known un-owned devices */
+OC_LIST(unowned_devices);
+
 /* Event threading variables */
 static pthread_t event_thread;
 static pthread_mutex_t mutex;
@@ -30,15 +45,151 @@ static pthread_mutex_t app_lock;
 /* Logic variables */
 static int quit;
 
-static int
-app_init(void)
+/* Main event thread */
+static void *
+ocf_event_thread(void *data)
 {
-  int ret = oc_init_platform("OCF", NULL, NULL);
-  ret |= oc_add_device("/oic/d", "oic.d.dots", "OBT", "ocf.2.0.5",
-                       "ocf.res.1.0.0,ocf.sh.1.0.0", NULL, NULL);
-  oc_device_bind_resource_type(0, "oic.d.ams");
-  oc_device_bind_resource_type(0, "oic.d.cms");
-  return ret;
+  (void)data;
+  oc_clock_time_t next_event;
+  while (quit != 1) {
+    next_event = oc_main_poll();
+    pthread_mutex_lock(&mutex);
+    if (next_event == 0) {
+      pthread_cond_wait(&cv, &mutex);
+    } else {
+      ts.tv_sec = (next_event / OC_CLOCK_SECOND);
+      ts.tv_nsec = (next_event % OC_CLOCK_SECOND) * 1.e09 / OC_CLOCK_SECOND;
+      pthread_cond_timedwait(&cv, &mutex, &ts);
+    }
+    pthread_mutex_unlock(&mutex);
+  }
+  oc_main_shutdown();
+  oc_obt_shutdown();
+
+  return NULL;
+}
+
+/* Threading Functions */
+static void
+signal_event_loop(void)
+{
+  pthread_mutex_lock(&mutex);
+  pthread_cond_signal(&cv);
+  pthread_mutex_unlock(&mutex);
+}
+
+static void
+handle_signal(int signal)
+{
+  (void)signal;
+  quit = 1;
+  signal_event_loop();
+}
+
+/* App utility functions */
+static device_handle_t *
+is_device_in_list(oc_uuid_t *uuid, oc_list_t list)
+{
+  device_handle_t *device = (device_handle_t *)oc_list_head(list);
+  while (device != NULL) {
+    if (memcmp(device->uuid.id, uuid->id, 16) == 0) {
+      return device;
+    }
+    device = device->next;
+  }
+  return NULL;
+}
+
+static bool
+add_device_to_list(oc_uuid_t *uuid, const char *device_name, oc_list_t list)
+{
+  device_handle_t *device = is_device_in_list(uuid, list);
+
+  if (!device) {
+    device = oc_memb_alloc(&device_handles);
+    if (!device) {
+      return false;
+    }
+    memcpy(device->uuid.id, uuid->id, 16);
+    oc_list_add(list, device);
+  }
+
+  if (device_name) {
+    size_t len = strlen(device_name);
+    len = (len > 63) ? 63 : len;
+    strncpy(device->device_name, device_name, len);
+    device->device_name[len] = '\0';
+  } else {
+    device->device_name[0] = '\0';
+  }
+  return true;
+}
+
+void
+empty_device_list(oc_list_t list)
+{
+  device_handle_t *device = (device_handle_t *)oc_list_pop(list);
+  while (device != NULL) {
+    oc_memb_free(&device_handles, device);
+    device = (device_handle_t *)oc_list_pop(list);
+  }
+}
+/* End of app utility functions */
+
+/* Callback functions */
+static void
+get_device(oc_client_response_t *data)
+{
+  oc_rep_t *rep = data->payload;
+  char *di = NULL, *n = NULL;
+  size_t di_len = 0, n_len = 0;
+
+  if (oc_rep_get_string(rep, "di", &di, &di_len)) {
+    oc_uuid_t uuid;
+    oc_str_to_uuid(di, &uuid);
+    if (!oc_rep_get_string(rep, "n", &n, &n_len)) {
+      n = NULL;
+      n_len = 0;
+    }
+
+    add_device_to_list(&uuid, n, data->user_data);
+  }
+}
+
+static void
+unowned_device_cb(oc_uuid_t *uuid, oc_endpoint_t *eps, void *data)
+{
+  (void)data;
+  char di[37];
+  oc_uuid_to_str(uuid, di, 37);
+  oc_endpoint_t *ep = eps;
+
+  PRINT("\nDiscovered unowned device: %s at:\n", di);
+  while (eps != NULL) {
+    PRINTipaddr(*eps);
+    PRINT("\n");
+    eps = eps->next;
+  }
+
+  oc_do_get("/oic/d", ep, NULL, &get_device, HIGH_QOS, unowned_devices);
+}
+
+/* Locally invoked functions */
+static void
+display_menu(void)
+{
+  PRINT("##### Specialized OBT #####\n");
+  PRINT("1. Discover unowned devices\n");
+  PRINT("99. Exit\n");
+}
+
+static void
+discover_unowned_devices(void)
+{
+  pthread_mutex_lock(&app_lock);
+  oc_obt_discover_unowned_devices(unowned_device_cb, NULL);
+  pthread_mutex_unlock(&app_lock);
+  signal_event_loop();
 }
 
 /* TODO: Implement onboarding kick-off.
@@ -67,28 +218,16 @@ post_obt(oc_request_t *request, oc_interface_mask_t iface_mask, void *user_data)
   oc_send_response(request, OC_STATUS_CHANGED);
 }
 
-/* Main event thread */
-static void *
-ocf_event_thread(void *data)
+/* Init and setup functions */
+static int
+app_init(void)
 {
-  (void)data;
-  oc_clock_time_t next_event;
-  while (quit != 1) {
-    next_event = oc_main_poll();
-    pthread_mutex_lock(&mutex);
-    if (next_event == 0) {
-      pthread_cond_wait(&cv, &mutex);
-    } else {
-      ts.tv_sec = (next_event / OC_CLOCK_SECOND);
-      ts.tv_nsec = (next_event % OC_CLOCK_SECOND) * 1.e09 / OC_CLOCK_SECOND;
-      pthread_cond_timedwait(&cv, &mutex, &ts);
-    }
-    pthread_mutex_unlock(&mutex);
-  }
-  oc_main_shutdown();
-  oc_obt_shutdown();
-
-  return NULL;
+  int ret = oc_init_platform("OCF", NULL, NULL);
+  ret |= oc_add_device("/oic/d", "oic.d.dots", "OBT", "ocf.2.0.5",
+                       "ocf.res.1.0.0,ocf.sh.1.0.0", NULL, NULL);
+  oc_device_bind_resource_type(0, "oic.d.ams");
+  oc_device_bind_resource_type(0, "oic.d.cms");
+  return ret;
 }
 
 static void
@@ -108,47 +247,6 @@ static void
 issue_requests(void)
 {
   oc_obt_init();
-}
-
-static void
-signal_event_loop(void)
-{
-  pthread_mutex_lock(&mutex);
-  pthread_cond_signal(&cv);
-  pthread_mutex_unlock(&mutex);
-}
-
-static void
-handle_signal(int signal)
-{
-  (void)signal;
-  quit = 1;
-  signal_event_loop();
-}
-
-static void
-display_menu(void)
-{
-  PRINT("##### Specialized OBT #####\n");
-  PRINT("1. Discover unowned devices\n");
-  PRINT("99. Exit\n");
-}
-
-static void
-unowned_device_cb(oc_uuid_t *uuid, oc_endpoint_t *eps, void *data)
-{
-  (void)uuid;
-  (void)eps;
-  (void)data;
-}
-
-static void
-discover_unowned_devices(void)
-{
-  pthread_mutex_lock(&app_lock);
-  oc_obt_discover_unowned_devices(unowned_device_cb, NULL);
-  pthread_mutex_unlock(&app_lock);
-  signal_event_loop();
 }
 
 int
