55abf836ec1669603b228e887095258d75e4b8ff
Marek Szkowron <marek.szkowron@comarch.com>
2021-01-28 15:53:23 +0100

    Add ssm to switch resource for server_certification_test app

--- apps/server_certification_tests.c
+++ apps/server_certification_tests.c
@@ -901,6 +901,9 @@ register_resources(void)
   oc_resource_set_request_handler(temp_resource, OC_POST, post_temp, NULL);
   oc_resource_tag_func_desc(temp_resource, OC_ENUM_HEATING);
   oc_resource_tag_pos_desc(temp_resource, OC_POS_CENTRE);
+ #ifdef OC_OSCORE
+  oc_resource_set_secure_mcast(temp_resource, true);
+#endif /* OC_OSCORE */
   oc_add_resource(temp_resource);
   PRINT("\tTemperature resource added.\n");
   bswitch = oc_new_resource(NULL, "/switch", 1, 0);
