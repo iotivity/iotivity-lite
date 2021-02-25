6e49efa394db0b3ae678eafb2e2064c63e9cc4e0
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2021-02-25 17:21:22 +0000

    address review comment from Wouter

--- api/oc_ri.c
+++ api/oc_ri.c
@@ -75,7 +75,7 @@ OC_MEMB(app_resources_s, oc_resource_t, OC_MAX_APP_RESOURCES);
 #ifdef OC_CLIENT
 #include "oc_client_state.h"
 OC_LIST(client_cbs);
-OC_MEMB(client_cbs_s, oc_client_cb_t,  + 1);
+OC_MEMB(client_cbs_s, oc_client_cb_t, OC_MAX_NUM_CONCURRENT_REQUESTS + 1);
 #endif /* OC_CLIENT */
 
 OC_LIST(timed_callbacks);
