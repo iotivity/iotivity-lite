792af49776a26d52ac4214f76ab974545fa6f386
Daniel Adam <daniel.adam1922@protonmail.com>
2021-03-06 21:45:33 +0100

    cloud_server: add OC_DELETE handler to created switches

--- apps/cloud_server.c
+++ apps/cloud_server.c
@@ -424,6 +424,27 @@ get_cswitch(oc_request_t *request, oc_interface_mask_t iface_mask,
 
 #ifdef OC_COLLECTIONS_IF_CREATE
 
+static oc_event_callback_retval_t
+delete_from_collection(void* res)
+{
+  oc_resource_t* r = (oc_resource_t*)res;
+  oc_cloud_delete_resource(r);
+  oc_delete_resource(r);
+  return OC_EVENT_DONE;
+}
+
+static void
+delete_cswitch(oc_request_t *request, oc_interface_mask_t iface_mask,
+            void *user_data)
+{
+  OC_DBG("%s", __func__);
+  (void)request;
+  (void)iface_mask;
+  oc_switch_t *cswitch = (oc_switch_t *)user_data;
+
+  oc_set_delayed_callback(cswitch->resource, delete_from_collection, 0);
+}
+
 /**
  *  Get pointer to the first element that either has no following element
  *  or the following element has an id with larger increment than 1
@@ -487,13 +508,14 @@ get_switch_instance(const char *href, oc_string_array_t *types,
       oc_resource_set_default_interface(cswitch->resource, OC_IF_A);
       oc_resource_set_request_handler(cswitch->resource, OC_GET, get_cswitch,
                                       cswitch);
+      oc_resource_set_request_handler(cswitch->resource, OC_DELETE, delete_cswitch,
+                                      cswitch);
       oc_resource_set_request_handler(cswitch->resource, OC_POST, post_cswitch,
                                       cswitch);
       oc_resource_set_properties_cbs(cswitch->resource, get_switch_properties,
                                      cswitch, set_switch_properties, cswitch);
       oc_add_resource(cswitch->resource);
       oc_set_delayed_callback(cswitch->resource, register_to_cloud, 0);
-
       oc_list_insert(switches, prev, cswitch);
       return cswitch->resource;
     } else {
@@ -506,6 +528,7 @@ get_switch_instance(const char *href, oc_string_array_t *types,
 static void
 free_switch_instance(oc_resource_t *resource)
 {
+  OC_DBG("%s", __func__);
   oc_switch_t *cswitch = (oc_switch_t *)oc_list_head(switches);
   while (cswitch) {
     if (cswitch->resource == resource) {
