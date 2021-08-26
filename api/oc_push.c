147736aee78e51e1150bf74c1087c0f570f2aa93
Joo-Chul Kevin Lee <jclee@icarus2.etri.re.kr>
2021-08-27 01:39:45 +0900

    ongoing...

--- api/oc_push.c
+++ api/oc_push.c
@@ -23,8 +23,17 @@
 
 #include "oc_api.h"
 #include "oc_events.h"
-#include "oc_process.h"
+#include "oc_rep.h"
+#include "util/oc_process.h"
+#include "util/oc_list.h"
 
+/**
+ *
+ * @brief variable for storing new collection member of push config Resource
+ *
+ */
+OC_MEMB(ns_memb, oc_ns_t, 1);
+OC_LIST(ns_list);
 
 OC_PROCESS(oc_push_process, "Push Notification handler");
 
@@ -51,6 +60,92 @@ void post_pushconf(oc_request_t *request, oc_interface_mask_t iface_mask, void *
 
 /**
  *
+ * @brief
+ *
+ * @param resource
+ * @param rep
+ * @param data
+ * @return
+ */
+bool set_ns_properties(oc_resource_t *resource, oc_rep_t *rep, void *data)
+{
+	(void)resource;
+	oc_ns_t *ns_instance = (oc_ns_t *)data;
+	while (rep != NULL) {
+		switch (rep->type) {
+		case OC_REP_STRING:
+			if (oc_string_len(rep->name) == 5 && memcmp(oc_string(rep->name), "phref", 5) == 0)
+			{
+				oc_new_string(&ns_instance->phref, oc_string(rep->value.string), oc_string_len(rep->value.string));
+			}
+			else if (oc_string_len(rep->name) == 3 && memcmp(oc_string(rep->name), "prt", 3) == 0)
+			{
+				oc_new_string_array(ns_instance->prt, rep->value.array.size);
+				oc_array_t *array_item = &rep->value.array;
+				for (int i=0; i<rep->value.array.size; i++) {
+					oc_string_array_add_item(ns_instance->prt, array_item->ptr);
+					array_item = array_item->next;
+				}
+			}
+			/* TODO4ME from here.. 2021/8/27  */
+			
+
+			break;
+
+//		case OC_REP_DOUBLE:
+//			if (oc_string_len(rep->name) == 5 && memcmp(oc_string(rep->name), "power", 5) == 0) {
+//				ec->power = rep->value.double_p;
+//			} else if (oc_string_len(rep->name) == 6 && memcmp(oc_string(rep->name), "energy", 6) == 0) {
+//				ec->energy = rep->value.double_p;
+//			}
+//			break;
+		default:
+			break;
+		}
+		rep = rep->next;
+	}
+	return true;
+}
+
+
+
+/**
+ *
+ * @brief prepare Resource presentation of ["oic.r.notificationselector", "oic.r.pushproxy"]
+ *
+ * @param resource
+ * @param iface_mask
+ * @param data oc_memb struct for ["oic.r.notificationselector", "oic.r.pushproxy"] Resource
+ */
+void get_ns_properties(oc_resource_t *resource, oc_interface_mask_t iface_mask, void *data)
+{
+	oc_ns_t *ns = (oc_ns_t *)data;
+	oc_rep_start_root_object();
+	switch (iface_mask) {
+	case OC_IF_BASELINE:
+		oc_process_baseline_interface(resource);
+		/* fall through */
+	case OC_IF_S:
+		oc_rep_set_double(root, power, ns->power);
+		oc_rep_set_double(root, energy, ns->energy);
+		break;
+	default:
+		break;
+	}
+	oc_rep_end_root_object();
+}
+
+
+
+void get_ns(oc_request_t *request, oc_interface_mask_t iface_mask, void *user_data)
+{
+	get_ns_properties(request->resource, iface_mask, user_data);
+	oc_send_response(request, OC_STATUS_OK);
+}
+
+
+/**
+ *
  * @brief callback for creating new notification selector
  *
  */
@@ -58,8 +153,31 @@ oc_resource_t *get_ns_instance(const char *href, oc_string_array_t *types,
 								oc_resource_properties_t bm, oc_interface_mask_t iface_mask,
 								size_t device)
 {
-	/* TODO4ME from here.. 2021/8/25  */
+	oc_ns_t *ns_instance = (oc_ns_t *)oc_memb_alloc(&ns_memb);
 
+	if (ns_instance) {
+		ns_instance->resource = oc_new_resource(NULL, href, oc_string_array_get_allocated_size(*types), device);
+		if (ns_instance->resource) {
+			size_t i;
+			for (i = 0; i < oc_string_array_get_allocated_size(*types); i++) {
+				const char *rt = oc_string_array_get_item(*types, i);
+				oc_resource_bind_resource_type(ns_instance->resource, rt);
+			}
+			oc_resource_bind_resource_interface(ns_instance->resource, iface_mask);
+			ns_instance->resource->properties = bm;
+			oc_resource_set_default_interface(ns_instance->resource, OC_IF_RW);
+			oc_resource_set_request_handler(ns_instance->resource, OC_GET, get_ns, ns_instance);
+			oc_resource_set_properties_cbs(ns_instance->resource, get_ns_properties, ns_instance, set_ns_properties, ns_instance);
+			oc_add_resource(ns_instance->resource);
+
+			oc_list_add(ns_list, ns_instance);
+			return ns_instance->resource;
+		} else {
+			oc_memb_free(&ns_memb, ns_instance);
+		}
+	}
+
+	return NULL;
 }
 
 
--- include/oc_push.h
+++ include/oc_push.h
@@ -21,6 +21,7 @@
 #define OC_PUSH_H
 
 #include "oc_config.h"
+#include "oc_helpers.h"
 #include "util/oc_process.h"
 
 
@@ -28,6 +29,22 @@
 extern "C"
 {
 #endif
+
+/*
+ * Resource creation and request handlers for ["oic.r.notificationselector", "oic.r.pushproxy"] instances
+ */
+typedef struct oc_ns
+{
+	struct oc_ns *next;
+	oc_resource_t *resource;
+	oc_string_t phref;
+	oc_string_array_t prt;
+	oc_string_array_t pif;
+	oc_string_t push_target;
+	oc_string_t pushqif;
+	oc_string_array_t sourcert;
+	char state;
+} oc_ns_t;
 
 
 OC_PROCESS_NAME(oc_push_process);
