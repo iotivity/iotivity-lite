bdd47651ca0758a9d48bbd1c875ed9de14d398ae
Wouter <wavdb66@gmail.com>
2021-11-02 14:09:15 +0000

    fix formatting

--- python/oc_python.c
+++ python/oc_python.c
@@ -1,5 +1,6 @@
 /*
 // Copyright (c) 2017-2019 Intel Corporation
+//           (c) 2021 Cascoda Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
