fc3b25bf0489afbf56545cbe943f8b7c82b36a62
Daniel Adam <daniel.adam1922@protonmail.com>
2021-12-16 12:04:06 +0100

    linux: run tests also with ASAN and TSAN builds

--- docker/run.sh
+++ docker/run.sh
@@ -14,7 +14,10 @@ umask 0000
 mkdir -p /tmp/logbt-coredumps
 pids=()
 for ((i=0;i<$NUM_DEVICES;i++)); do
-    ${PREFIX_EXEC} /iotivity-lite/port/linux/service $@ > /tmp/$i.log &
+    export ASAN_OPTIONS="log_path=/tmp/${i}.asan.log"
+    # abort on first tsan problem found
+    export TSAN_OPTIONS="halt_on_error=1 abort_on_error=1 log_path=/tmp/${i}.tsan.log"
+    ${PREFIX_EXEC} /iotivity-lite/port/linux/service $@ > /tmp/$i.log 2>&1 &
     pids+=($!)
 done
 
@@ -23,7 +26,7 @@ done
 # more than one service in a container. The container exits with an error
 # if it detects that either of the processes has exited.
 # Otherwise it loops forever, waking up every 60 seconds
-while sleep 10; do 
+while sleep 10; do
 for (( i=0; i<${#pids[@]}; i++ ));
 do
     if ! kill -0 ${pids[$i]} 2>/dev/null; then
