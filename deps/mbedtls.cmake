8bbbdcc2447e7cefcfa8c36af06ce9fa94383a61
Martin Rösch <martin.roesch@kistler.com>
2022-02-08 10:43:02 +0100

    Extend CMake build (#181)
    * Link apps/simpleclient with iotivity-server-client
    
    As for the windows build.
    
    * Add cache variable to build shared libraries
    
    * BUILD_SHARED_LIBS is a global cmake variable. If it is set to ON,
      add_library generates shared libraries if not explicitly set otherwise.
    
    * If BUILD_SHARED_LIBS is ON, mbedtls needs to be built as shared library
      as well. This can be done by setting the USE_STATIC_MBEDTLS_LIBRARY
      and USE_SHARED_MBEDTLS_LIBRARY CMake option from mbedtls as cache variable.
    
    * The iotivity-api interface library has to be linked pubicly so that
      its symbols are exported.
    
    * Set project version of iotivity-lite and set library properties
    
    Setting the version causes a CMake Policy warning CMP0048 because
    mbedtls does not set  the project version.
    
    * Add install targets
    
    * Make tinycbor-master library a INTERFACE library
    
    This makes its linking to iotivity-lite independent of the
    BUILD_SHARED_LIBS variable (handles the PIC compiler flag for shared
    libs build).
    
    * Rename library targets to match port/linux/Makefile
    
    * Generate pkg-config files
    
    * Add missing build definitions
    
    * Move build of mbedtls to iotivity-deps project
    
    * Remove obsolete build variable
    
    * Build port library as object lib and in main project
    
    * Add compile definitions for tinycbor-master
    
    * Build deps libraries as object lib and in main project
    
    * Format port/CMakeLists.txt
    
    * Add build variable for IPv6 DNS lookup
    
    * Build static and shared libraries from object libraries
    
    * Build port in server/client library due to OC_CLIENT switch in ipadapter.c
    
    * Remove dynamic link dependency to mbedtls.
    
    The library is statically linked into the dynamic library.
    
    * Split mbedtls and tinycbor libraries into separate CMake modules
    
    This allows to link mbedtls only when security is enabled.
    
    * Fix mbedtls-patch module
    
    * Link static mbedtls library into shared libraries
    
    * Build mbedtls as object library
    
    * Add build of unit tests
    
    * Link to libm on platforms that require it
    
    * Set properties on static and shared libraries
    
    These properties are propagated to the user of the library.
    
    * Generate and install CMake package for iotivity-lite
    
    * Refactor CMake variable names
    
    * Set BUILD_INTERFACE properties on static and shared libraries
    
    These BUILD_INTERFACE properties help with the compilation of the
    onboarding tool and the sample apps.
    
    * Add build of onboarding tool
    
    * Add build of most example applications
    
    * Add build variable for example applications and onboarding tool
    
    * Add build of test applications
    
    * Add build of python client bindings dynamic library
    
    * Fix server_certification_test: rd_client.h requires OC_CLOUD
    
    * Fix missing compile definition __OC_RANDOM for mbedtls
    
    Unittests now pass even if OC_IPV4 is not set.
    
    * Simplify collection of mbedtls source files
    
    * Fix globbing of obt source files
    
    * CMake: add support for clang-tidy static analysis (#183)
    
    * Append -static for static library names
    
    This resolves the name conflict on Windows.
    
    * Add port to the private include directories for the libraries
    
    The Windows build doesn't find the header files in the port directory.
    
    * The test applications are intended for Linux only
    
    * Build the python client library on Linux only.
    
    * Add windows registry setting for CMake package during installation
    
    * Add missing OC_IPV4 compilation switch in port/windows/ipadapter.c
    
    * Don't build shared libraries with MSVC
    
    * Fix wrong object library for client-server library
    
    Co-authored-by: Daniel Adam <daniel.adam1922@protonmail.com>

---
+++ deps/mbedtls.cmake
@@ -0,0 +1,30 @@
+# Patch mbedtls
+set(OC_REAPPLY_MBEDTLS_PATCHES ON CACHE BOOL "")
+if(OC_REAPPLY_MBEDTLS_PATCHES)
+    include(${PROJECT_SOURCE_DIR}/deps/mbedtls-patch.cmake)
+    set(OC_REAPPLY_MBEDTLS_PATCHES OFF CACHE BOOL
+        "By default, mbedTLS patches are applied upon the first CMake Configure. Set this to ON to reapply the patches on the next configure."
+         FORCE
+    )
+endif()
+
+file(GLOB MBEDTLS_SRC
+    ${PROJECT_SOURCE_DIR}/deps/mbedtls/library/*.c
+)
+list(REMOVE_ITEM MBEDTLS_SRC
+    ${PROJECT_SOURCE_DIR}/deps/mbedtls/library/certs.c
+    ${PROJECT_SOURCE_DIR}/deps/mbedtls/library/memory_buffer_alloc.c
+    ${PROJECT_SOURCE_DIR}/deps/mbedtls/library/x509_crl.c
+)
+add_library(mbedtls OBJECT ${MBEDTLS_SRC})
+target_include_directories(mbedtls PRIVATE
+    ${PROJECT_SOURCE_DIR}
+    ${PROJECT_SOURCE_DIR}/include
+    ${PORT_INCLUDE_DIR}
+    ${PROJECT_SOURCE_DIR}/deps/mbedtls/include
+)
+target_compile_definitions(mbedtls PUBLIC ${PUBLIC_COMPILE_DEFINITIONS} PRIVATE __OC_RANDOM)
+# do not treat warnings as errors on Windows
+if(MSVC)
+    target_compile_options(mbedtls PRIVATE /W1 /WX-)
+endif()
---
+++ tools/clang-tidy.cmake
@@ -0,0 +1,62 @@
+cmake_minimum_required (VERSION 3.10)
+
+include_guard(GLOBAL)
+
+if(UNIX AND CLANG_TIDY_ENABLED)
+    file(GLOB iotivity_directories LIST_DIRECTORIES true "${PROJECT_SOURCE_DIR}/*")
+
+    set(iotivity_dirnames "")
+    foreach(dir ${iotivity_directories})
+        if (NOT IS_DIRECTORY "${dir}")
+            continue()
+        endif()
+
+        get_filename_component(dirname "${dir}" NAME_WE)
+        if (dirname)
+            # skip hidden directories
+            if (dirname MATCHES "^\\.")
+                continue()
+            endif()
+
+            # skip third-party libs
+            if (dirname STREQUAL "deps")
+                continue()
+            endif()
+
+            list(APPEND iotivity_dirnames ${dirname})
+        endif()
+    endforeach()
+
+    if(iotivity_dirnames)
+        get_filename_component(project_source_dirname "${PROJECT_SOURCE_DIR}" NAME_WE)
+        string(REPLACE ";" "|" iotivity_dirs_regexp "${iotivity_dirnames}")
+        set(IOTIVITY_DIR_NAMES_REGEX ".*/${project_source_dirname}/(${iotivity_dirs_regexp})/.*")
+    else()
+        set(IOTIVITY_DIR_NAMES_REGEX ".*")
+    endif()
+
+    configure_file(
+        "${PROJECT_SOURCE_DIR}/tools/.clang-tidy.in"
+        "${PROJECT_SOURCE_DIR}/.clang-tidy"
+        @ONLY)
+endif()
+
+# enable clang-tidy before defining targets you want to run analysis on
+macro(enable_clang_tidy)
+    if(UNIX AND CLANG_TIDY_ENABLED)
+        # use clang-tidy during compilation if its available
+        find_program(CLANG_TIDY_BIN clang-tidy)
+        if(CLANG_TIDY_BIN)
+            set(CMAKE_C_CLANG_TIDY ${CLANG_TIDY_BIN})
+            set(CMAKE_CXX_CLANG_TIDY ${CMAKE_C_CLANG_TIDY})
+        else()
+            message(STATUS "clang-tidy not installed")
+        endif()
+    endif()
+endmacro()
+
+# disable clang-tidy before defining targets you want to skip analysis on
+macro(disable_clang_tidy)
+    set(CMAKE_C_CLANG_TIDY "")
+    set(CMAKE_CXX_CLANG_TIDY "")
+endmacro()
