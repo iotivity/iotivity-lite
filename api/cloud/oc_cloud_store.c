7040cd7f734c6e39c9725d54d177d26d8a3425b8
Martin Roesch <martin.roesch@kistler.com>
2022-02-16 15:42:33 +0100

    Initialize the cloud sid property with NULL.

--- api/cloud/oc_cloud_store.c
+++ api/cloud/oc_cloud_store.c
@@ -313,7 +313,7 @@ cloud_store_initialize(oc_cloud_store_t *store)
   cloud_set_string(&store->uid, NULL, 0);
   cloud_set_string(&store->access_token, NULL, 0);
   cloud_set_string(&store->refresh_token, NULL, 0);
-  cloud_set_string(&store->sid, "00000000-0000-0000-0000-000000000000", 36);
+  cloud_set_string(&store->sid, NULL, 0);
   store->status = 0;
   store->expires_in = 0;
 }
