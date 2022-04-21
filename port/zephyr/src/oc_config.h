030879bcf64f425811538a007f48249362fac662
FrankZRS <86958410+FrankZRS@users.noreply.github.com>
2022-04-21 08:03:46 +0100

    Fix for Issue #247 (#249)
    * Made device name wiping optional
    
    * Turned on name wiping

--- port/zephyr/src/oc_config.h
+++ port/zephyr/src/oc_config.h
@@ -82,7 +82,7 @@ typedef uint64_t oc_clock_time_t;
 #endif
 
 /* Wipe device name during reset, when oic/con is enabled */
-/* Default: Keep name */
-#define OC_WIPE_NAME (0)
+/* Default: Wipe name */
+#define OC_WIPE_NAME (1)
 
 #endif /* OC_CONFIG_H */
