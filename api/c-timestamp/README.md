9c8ea1a33e02bfacc9dfa7419682b95b91bd7d58
Daniel Adam <daniel.adam1922@protonmail.com>
2022-08-30 12:59:13 +0200

    api/c-timestamp: fix issues reported by SonarCloud

--- api/c-timestamp/README.md
+++ api/c-timestamp/README.md
@@ -1,11 +1,11 @@
-[![Build Status](https://travis-ci.org/chansen/c-timestamp.png?branch=master)](https://travis-ci.org/chansen/c-timestamp) [![Coverage Status](https://coveralls.io/repos/chansen/c-timestamp/badge.png)](https://coveralls.io/r/chansen/c-timestamp)
-
-timestamp
+Timestamp
 =========
 
+Fast RFC 3339 (ISO 8601) timestamp parser and formatter implemented in C, zero dependencies.
 
-```c
+[Source](https://github.com/chansen/c-timestamp)
 
+```C
 typedef struct {
     int64_t sec;    /* Number of seconds since the epoch of 1970-01-01T00:00:00Z */
     int32_t nsec;   /* Nanoseconds [0, 999999999] */
@@ -20,6 +20,4 @@ bool        timestamp_valid            (const timestamp_t *tsp);
 struct tm * timestamp_to_tm_utc        (const timestamp_t *tsp, struct tm *tmp);
 struct tm * timestamp_to_tm_local      (const timestamp_t *tsp, struct tm *tmp);
 
-
 ```
-
--- api/c-timestamp/timestamp_parse.c
+++ api/c-timestamp/timestamp_parse.c
@@ -28,6 +28,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "timestamp.h"
+#include <stdbool.h>
 #include <stddef.h>
 
 static int
@@ -49,9 +50,15 @@ month_days(uint16_t y, uint16_t m)
 static int
 parse_2d(const unsigned char *const p, size_t i, uint16_t *vp)
 {
-  unsigned char d0, d1;
-  if (((d0 = p[i + 0] - '0') > 9) || ((d1 = p[i + 1] - '0') > 9))
+  unsigned char d0 = p[i + 0] - '0';
+  if (d0 > 9) {
     return 1;
+  }
+  unsigned char d1 = p[i + 1] - '0';
+  if (d1 > 9) {
+    return 1;
+  }
+
   *vp = d0 * 10 + d1;
   return 0;
 }
@@ -59,10 +66,23 @@ parse_2d(const unsigned char *const p, size_t i, uint16_t *vp)
 static int
 parse_4d(const unsigned char *const p, size_t i, uint16_t *vp)
 {
-  unsigned char d0, d1, d2, d3;
-  if (((d0 = p[i + 0] - '0') > 9) || ((d1 = p[i + 1] - '0') > 9) ||
-      ((d2 = p[i + 2] - '0') > 9) || ((d3 = p[i + 3] - '0') > 9))
+  unsigned char d0 = p[i + 0] - '0';
+  if (d0 > 9) {
     return 1;
+  }
+  unsigned char d1 = p[i + 1] - '0';
+  if (d1 > 9) {
+    return 1;
+  }
+  unsigned char d2 = p[i + 2] - '0';
+  if (d2 > 9) {
+    return 1;
+  }
+  unsigned char d3 = p[i + 3] - '0';
+  if (d3 > 9) {
+    return 1;
+  }
+
   *vp = d0 * 1000 + d1 * 100 + d2 * 10 + d3;
   return 0;
 }
@@ -74,96 +94,154 @@ static const uint32_t Pow10[10] = { 1,         10,        100,     1000,
 static const uint16_t DayOffset[13] = { 0,   306, 337, 0,   31,  61, 92,
                                         122, 153, 184, 214, 245, 275 };
 
+static bool
+check_format(const unsigned char *str, size_t len)
+{
+  if (len < 20 || str[4] != '-' || str[7] != '-' || str[13] != ':' ||
+      str[16] != ':') {
+    return false;
+  }
+
+  unsigned char ch = str[10];
+  return ch == 'T' || ch == ' ' || ch == 't';
+}
+
+static bool
+parse_date(const unsigned char *str, uint16_t *year, uint16_t *month,
+           uint16_t *day)
+{
+  uint16_t y;
+  uint16_t m;
+  uint16_t d;
+  if (parse_4d(str, 0, &y) || y < 1 || parse_2d(str, 5, &m) || m < 1 ||
+      m > 12 || parse_2d(str, 8, &d) || d < 1 || d > 31) {
+    return false;
+  }
+
+  if (d > 28 && d > month_days(y, m)) {
+    return false;
+  }
+  if (m < 3) {
+    y--;
+  }
+
+  *year = y;
+  *month = m;
+  *day = d;
+  return true;
+}
+
+static bool
+parse_time(const unsigned char *str, uint16_t *hour, uint16_t *min,
+           uint16_t *sec)
+{
+  uint16_t h;
+  uint16_t m;
+  uint16_t s;
+  if (parse_2d(str, 11, &h) || h > 23 || parse_2d(str, 14, &m) || m > 59 ||
+      parse_2d(str, 17, &s) || s > 59) {
+    return false;
+  }
+  *hour = h;
+  *min = m;
+  *sec = s;
+  return true;
+}
+
+static bool
+parse_offset(const unsigned char *str, bool minus, int16_t *offset)
+{
+  uint16_t hour;
+  uint16_t min;
+  if (str[2] != ':') {
+    return false;
+  }
+
+  if (parse_2d(str, 0, &hour) || hour > 23 || parse_2d(str, 3, &min) ||
+      min > 59) {
+    return false;
+  }
+
+  int16_t o = hour * 60 + min;
+  if (minus) {
+    o *= -1;
+  }
+  *offset = o;
+  return true;
+}
+
 int
 timestamp_parse(const char *str, size_t len, timestamp_t *tsp)
 {
-  const unsigned char *cur, *end;
-  unsigned char ch;
-  uint16_t year, month, day, hour, min, sec;
-  uint32_t rdn, sod, nsec;
-  int16_t offset;
-
   /*
    *           1
    * 01234567890123456789
    * 2013-12-31T23:59:59Z
    */
-  cur = (const unsigned char *)str;
-  if (len < 20 || cur[4] != '-' || cur[7] != '-' || cur[13] != ':' ||
-      cur[16] != ':')
+  const unsigned char *cur = (const unsigned char *)str;
+  if (!check_format(cur, len)) {
     return 1;
+  }
 
-  ch = cur[10];
-  if (!(ch == 'T' || ch == ' ' || ch == 't'))
+  uint16_t year;
+  uint16_t month;
+  uint16_t day;
+  uint16_t hour;
+  uint16_t min;
+  uint16_t sec;
+  if (!parse_date(cur, &year, &month, &day) ||
+      !parse_time(cur, &hour, &min, &sec)) {
     return 1;
+  }
 
-  if (parse_4d(cur, 0, &year) || year < 1 || parse_2d(cur, 5, &month) ||
-      month < 1 || month > 12 || parse_2d(cur, 8, &day) || day < 1 ||
-      day > 31 || parse_2d(cur, 11, &hour) || hour > 23 ||
-      parse_2d(cur, 14, &min) || min > 59 || parse_2d(cur, 17, &sec) ||
-      sec > 59)
-    return 1;
-
-  if (day > 28 && day > month_days(year, month))
-    return 1;
-
-  if (month < 3)
-    year--;
-
-  rdn =
+  uint32_t rdn =
     (1461 * year) / 4 - year / 100 + year / 400 + DayOffset[month] + day - 306;
-  sod = hour * 3600 + min * 60 + sec;
-  end = cur + len;
+  uint32_t sod = hour * 3600 + min * 60 + sec;
+  const unsigned char *end = cur + len;
   cur = cur + 19;
-  offset = 0;
-  nsec = 0;
 
-  ch = *cur++;
+  uint32_t nsec = 0;
+  unsigned char ch = *cur++;
   if (ch == '.') {
-    const unsigned char *start;
-    size_t ndigits;
-
-    start = cur;
+    const unsigned char *start = cur;
     for (; cur < end; cur++) {
       const unsigned char digit = *cur - '0';
-      if (digit > 9)
+      if (digit > 9) {
         break;
+      }
       nsec = nsec * 10 + digit;
     }
 
-    ndigits = cur - start;
-    if (ndigits < 1 || ndigits > 9)
+    size_t ndigits = cur - start;
+    if (ndigits < 1 || ndigits > 9) {
       return 1;
+    }
 
     nsec *= Pow10[9 - ndigits];
 
-    if (cur == end)
+    if (cur == end) {
       return 1;
+    }
 
     ch = *cur++;
   }
 
+  int16_t offset = 0;
   if (!(ch == 'Z' || ch == 'z')) {
     /*
      *  01234
      * ±00:00
      */
-    if (cur + 5 < end || !(ch == '+' || ch == '-') || cur[2] != ':')
+    if (cur + 5 > end || !(ch == '+' || ch == '-') ||
+        !parse_offset(cur, ch == '-', &offset)) {
       return 1;
-
-    if (parse_2d(cur, 0, &hour) || hour > 23 || parse_2d(cur, 3, &min) ||
-        min > 59)
-      return 1;
-
-    offset = hour * 60 + min;
-    if (ch == '-')
-      offset *= -1;
-
+    }
     cur += 5;
   }
 
-  if (cur != end)
+  if (cur != end) {
     return 1;
+  }
 
   tsp->sec = ((int64_t)rdn - 719163) * 86400 + sod - offset * 60;
   tsp->nsec = nsec;
