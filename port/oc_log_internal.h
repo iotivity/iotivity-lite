cae924a53c7971f7b8b4ad02162a7c4a10bd67a6
Jozef Kralik <jojo.lwin@gmail.com>
2023-04-26 09:25:07 +0000

    logs: fix stack overflow when OC_LOGbytes is defined

--- port/oc_log_internal.h
+++ port/oc_log_internal.h
@@ -45,6 +45,7 @@
 
 #include "api/oc_log_internal.h"
 #include "oc_log.h"
+#include "oc_helpers.h"
 
 #ifndef __FILENAME__
 #ifdef WIN32
@@ -79,8 +80,24 @@
 #endif /* !defined(OC_DEBUG) && !defined(OC_PUSHDEBUG) */
 #endif
 
+#ifndef PRINT_LOG
+#define PRINT_LOG(...) printf(__VA_ARGS__)
+#endif /* !PRINT_LOG */
+
 #ifndef PRINT
-#define PRINT(...) printf(__VA_ARGS__)
+#define PRINT(...)                                                             \
+  do {                                                                         \
+    oc_logger_t *logger = oc_log_get_logger();                                 \
+    if (logger->fn != NULL) {                                                  \
+      if (logger->level < (OC_LOG_LEVEL_DEBUG)) {                              \
+        break;                                                                 \
+      }                                                                        \
+      logger->fn(OC_LOG_LEVEL_DEBUG, OC_LOG_COMPONENT_DEFAULT, __FILENAME__,   \
+                 __LINE__, __func__, __VA_ARGS__);                             \
+      break;                                                                   \
+    }                                                                          \
+    PRINT_LOG(__VA_ARGS__);                                                    \
+  } while (0)
 #endif /* !PRINT */
 
 // port's layer can override this macro to provide its own logger
@@ -98,10 +115,11 @@
     }                                                                          \
     char _oc_log_fn_buf[64] = { 0 };                                           \
     oc_clock_time_rfc3339(_oc_log_fn_buf, sizeof(_oc_log_fn_buf));             \
-    PRINT("[OC %s] %s: %s:%d <%s>: ", _oc_log_fn_buf,                          \
-          oc_log_level_to_label(log_level), __FILENAME__, __LINE__, __func__); \
-    PRINT(__VA_ARGS__);                                                        \
-    PRINT("\n");                                                               \
+    PRINT_LOG("[OC %s] %s: %s:%d <%s>: ", _oc_log_fn_buf,                      \
+              oc_log_level_to_label(log_level), __FILENAME__, __LINE__,        \
+              __func__);                                                       \
+    PRINT_LOG(__VA_ARGS__);                                                    \
+    PRINT_LOG("\n");                                                           \
     fflush(stdout);                                                            \
   } while (0)
 #endif /* !OC_LOG */
@@ -249,11 +267,13 @@
 
 #define SNPRINTFbytes(buff, size, data, len)                                   \
   do {                                                                         \
-    char *beg = (buff);                                                        \
-    char *end = (buff) + (size);                                               \
-    for (size_t i = 0; beg <= (end - 3) && i < (len); i++) {                   \
-      beg += (i == 0) ? SPRINTF(beg, "%02x", (data)[i])                        \
-                      : SPRINTF(beg, ":%02x", (data)[i]);                      \
+    if (len > 0) {                                                             \
+      char *beg = (buff);                                                      \
+      char *end = (buff) + (size);                                             \
+      for (size_t i = 0; beg <= (end - 3) && i < (size_t)(len); i++) {         \
+        beg += (i == 0) ? SPRINTF(beg, "%02x", (data)[i])                      \
+                        : SPRINTF(beg, ":%02x", (data)[i]);                    \
+      }                                                                        \
     }                                                                          \
   } while (0)
 
@@ -275,8 +295,8 @@
     } else {                                                                   \
       char _oc_log_fn_buf[64] = { 0 };                                         \
       oc_clock_time_rfc3339(_oc_log_fn_buf, sizeof(_oc_log_fn_buf));           \
-      PRINT("[OC %s] D: %s:%d <%s>: endpoint %s\n", _oc_log_fn_buf,            \
-            __FILENAME__, __LINE__, __func__, _oc_log_endpoint_buf);           \
+      PRINT_LOG("[OC %s] D: %s:%d <%s>: endpoint %s\n", _oc_log_fn_buf,        \
+                __FILENAME__, __LINE__, __func__, _oc_log_endpoint_buf);       \
       fflush(stdout);                                                          \
     }                                                                          \
   } while (0)
@@ -296,19 +316,28 @@
     if (logger->level < OC_LOG_LEVEL_TRACE) {                                  \
       break;                                                                   \
     }                                                                          \
-    char _oc_log_bytes_buf[2048];                                              \
-    memset(_oc_log_bytes_buf, 0, sizeof(_oc_log_bytes_buf));                   \
-    SNPRINTFbytes(_oc_log_bytes_buf, sizeof(_oc_log_bytes_buf), bytes,         \
-                  (size_t)length);                                             \
+    oc_string_t _oc_log_bytes_buf;                                             \
+    oc_alloc_string(&_oc_log_bytes_buf,                                        \
+                    length * 3 + 1 < 2048 ? length * 3 + 1 : 2048);            \
+    size_t _oc_log_bytes_buf_size = oc_string_len(_oc_log_bytes_buf);          \
+    if (_oc_log_bytes_buf_size == 0) {                                         \
+      break;                                                                   \
+    }                                                                          \
+    char *_oc_log_bytes_buf_ptr = oc_string(_oc_log_bytes_buf);                \
+    memset(_oc_log_bytes_buf_ptr, 0, _oc_log_bytes_buf_size);                  \
+    SNPRINTFbytes(_oc_log_bytes_buf_ptr, _oc_log_bytes_buf_size - 1, bytes,    \
+                  length);                                                     \
     if (logger->fn != NULL) {                                                  \
       logger->fn(OC_LOG_LEVEL_TRACE, OC_LOG_COMPONENT_DEFAULT, __FILENAME__,   \
-                 __LINE__, __func__, "%s", _oc_log_bytes_buf);                 \
+                 __LINE__, __func__, "%s", _oc_log_bytes_buf_ptr);             \
+      oc_free_string(&_oc_log_bytes_buf);                                      \
       break;                                                                   \
     }                                                                          \
     char _oc_log_fn_buf[64] = { 0 };                                           \
     oc_clock_time_rfc3339(_oc_log_fn_buf, sizeof(_oc_log_fn_buf));             \
-    PRINT("[OC %s] V: %s:%d <%s>: bytes %s\n", _oc_log_fn_buf, __FILENAME__,   \
-          __LINE__, __func__, _oc_log_bytes_buf);                              \
+    PRINT_LOG("[OC %s] V: %s:%d <%s>: bytes %s\n", _oc_log_fn_buf,             \
+              __FILENAME__, __LINE__, __func__, _oc_log_bytes_buf_ptr);        \
+    oc_free_string(&_oc_log_bytes_buf);                                        \
     fflush(stdout);                                                            \
   } while (0)
 #endif /* !OC_NO_LOG_BYTES && OC_DEBUG */
