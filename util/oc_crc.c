2442c7b6b04e2a934d377d69cb85fd5fdbe851a8
Daniel Adam <daniel.adam1922@protonmail.com>
2023-10-03 15:25:27 +0200

    Add CRC64 (ECMA-182, reflected form polynomial) calculation

---
+++ util/oc_crc.c
@@ -0,0 +1,50 @@
+/******************************************************************
+ *
+ * Copyright (c) 2023 plgd.dev s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"),
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+#include "util/oc_crc_internal.h"
+
+#ifdef OC_HAS_FEATURE_CRC64
+
+// ECMA-182, reflected form polynomial
+#define CRC64_POLYNOMIAL (0xC96C5795D7870F42ULL)
+
+static uint64_t
+crc64_update(uint64_t crc, uint8_t byte)
+{
+  crc ^= (uint64_t)byte;
+  for (int i = 0; i < 8; ++i) {
+    if (crc & 1) {
+      crc = (crc >> 1) ^ CRC64_POLYNOMIAL;
+    } else {
+      crc >>= 1;
+    }
+  }
+  return crc;
+}
+
+uint64_t
+oc_crc64(const uint8_t *buffer, size_t size)
+{
+  uint64_t crc = 0;
+  for (size_t i = 0; i < size; i++) {
+    crc = crc64_update(crc, buffer[i]);
+  }
+  return crc;
+}
+
+#endif /* OC_HAS_FEATURE_CRC64 */
---
+++ util/oc_crc_internal.h
@@ -0,0 +1,49 @@
+/******************************************************************
+ *
+ * Copyright (c) 2023 plgd.dev s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"),
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************/
+
+#ifndef OC_CRC_INTERNAL_H
+#define OC_CRC_INTERNAL_H
+
+#include "util/oc_compiler.h"
+#include "util/oc_features.h"
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef OC_HAS_FEATURE_CRC64
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Calculate CRC64 for a buffer of data.
+ *
+ * @param buffer The buffer of data to calculate CRC64 for (cannot be NULL);
+ * @param size The size of the buffer of data to calculate CRC64 for.
+ *
+ * @return The CRC64 value.
+ */
+uint64_t oc_crc64(const uint8_t *buffer, size_t size) OC_NONNULL();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* OC_HAS_FEATURE_CRC64 */
+
+#endif /* OC_CRC_INTERNAL_H */
