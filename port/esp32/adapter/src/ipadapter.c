0b456a5e95bdb941baf65fae2ce8aa679282f0d1
sp-martin <kusnier@siprogs.sk>
2024-06-07 07:05:30 +0000

    - esp32 port of Message Queue Limit

--- port/esp32/adapter/src/ipadapter.c
+++ port/esp32/adapter/src/ipadapter.c
@@ -27,6 +27,7 @@
 #include "port/oc_connectivity_internal.h"
 #include "port/oc_log_internal.h"
 #include "port/oc_network_event_handler_internal.h"
+#include "port/oc_random.h"
 #include "ipcontext.h"
 #include "oc_buffer.h"
 #include "oc_core_res.h"
@@ -804,78 +805,250 @@ oc_udp_receive_message(ip_context_t *dev, fd_set *fds, oc_message_t *message)
   return ADAPTER_STATUS_NONE;
 }
 
+#ifdef OC_DYNAMIC_ALLOCATION
+static bool
+fd_sets_are_equal(const fd_set *fd1, const fd_set *fd2)
+{
+  return (memcmp(fd1, fd2, sizeof(fd_set)) == 0);
+}
+
+static int
+fds_max(const fd_set *sourcefds)
+{
+  int max_fd = 0;
+  for (int i = 0; i < FD_SETSIZE; i++) {
+    if (FD_ISSET(i, sourcefds)) {
+      max_fd = i;
+    }
+  }
+  return max_fd;
+}
+
+static int
+fds_count(const fd_set *sourcefds, int max_fd)
+{
+  int rfd_count = 0;
+  for (int i = 0; i <= max_fd; i++) {
+    if (FD_ISSET(i, sourcefds)) {
+      rfd_count++;
+    }
+  }
+  return rfd_count;
+}
+
+static int
+pick_random_fd(const fd_set *sourcefds, int fd_count, int max_fd)
+{
+  assert(fd_count > 0);
+  int random_rfd = (int)oc_random_value() % fd_count;
+  for (int i = 0; i <= max_fd; i++) {
+    if (FD_ISSET(i, sourcefds)) {
+      if (--fd_count == random_rfd) {
+        return i;
+      }
+    }
+  }
+  return -1;
+}
+
+static int
+remove_random_fds(fd_set *rdfds, int rfds_count, int max_fd, int remove_count)
+{
+  int removed = 0;
+  while (removed < remove_count) {
+    int fd = pick_random_fd(rdfds, rfds_count, max_fd);
+    if (fd < 0) {
+      break;
+    }
+    // remove file descriptor from the set
+    FD_CLR(fd, rdfds);
+    --rfds_count;
+    ++removed;
+  }
+  return removed;
+}
+#endif /* OC_DYNAMIC_ALLOCATION */
+
+static bool
+process_wakeup_signal(ip_context_t *dev, fd_set *fds)
+{
+  if (FD_ISSET(dev->wakeup_pipe[0], fds)) {
+    FD_CLR(dev->wakeup_pipe[0], fds);
+    ssize_t len;
+    do {
+      char buf;
+      // write to pipe shall not block - so read the byte we wrote
+      len = read(dev->wakeup_pipe[0], &buf, 1);
+    } while (len < 0 && errno == EINTR);
+    return true;
+  }
+  return false;
+}
+
+static void
+add_control_flow_rfds(fd_set *output_set, const ip_context_t *dev)
+{
+  /* Monitor network interface changes on the platform from only the 0th
+   * logical device
+
+  if (dev->device == 0) {
+    FD_SET(ifchange_sock, output_set);
+  }
+  */
+  FD_SET(dev->wakeup_pipe[0], output_set);
+}
+
+static int
+process_socket_read_event(ip_context_t *dev, fd_set *rdfds)
+{
+  oc_message_t *message = oc_allocate_message();
+  if (message == NULL) {
+    return -1;
+  }
+  message->endpoint.device = dev->device;
+
+  if (oc_udp_receive_message(dev, rdfds, message) == ADAPTER_STATUS_RECEIVE) {
+    OC_DBG("network_event_thread oc_udp_receive_message");
+    goto receive;
+  }
+#ifdef OC_TCP
+  if (oc_tcp_receive_message(dev, rdfds, message) == ADAPTER_STATUS_RECEIVE) {
+    OC_DBG("network_event_thread oc_tcp_receive_message");
+    goto receive;
+  }
+#endif /* OC_TCP */
+
+  oc_message_unref(message);
+  return 0;
+
+receive:
+  OC_DBG("Incoming message of size %zd bytes from", message->length);
+  OC_LOGipaddr(message->endpoint);
+  OC_DBG("interface index %d", message->endpoint.interface_index);
+  OC_DBG("%s", "");
+
+  oc_network_receive_event(message);
+  return 1;
+}
+
+static int
+process_event(ip_context_t *dev, fd_set *rdfds, fd_set *wfds)
+{
+  if (rdfds != NULL) {
+    int ret = process_socket_read_event(dev, rdfds);
+    if (ret != 0) {
+      return ret;
+    }
+  }
+
+#if OC_DBG_IS_ENABLED
+  // GCOVR_EXCL_START
+  if (rdfds != NULL) {
+    for (int i = 0; i < FD_SETSIZE; ++i) {
+      if (FD_ISSET(i, rdfds)) {
+        OC_DBG("no handler found for read event (fd=%d)", i);
+      }
+    }
+  }
+  if (wfds != NULL) {
+    for (int i = 0; i < FD_SETSIZE; ++i) {
+      if (FD_ISSET(i, wfds)) {
+        OC_DBG("no handler found for write event (fd=%d)", i);
+      }
+    }
+  }
+  // GCOVR_EXCL_STOP
+#else  /* !OC_DBG_IS_ENABLED */
+  (void)wfds;
+#endif /* OC_DBG_IS_ENABLED */
+  return 0;
+}
+
+static void
+process_events(ip_context_t *dev, fd_set *rdfds, fd_set *wfds, int fd_count,
+               int max_read_fd)
+{
+  if (fd_count == 0) {
+    OC_DBG("process_events: timeout");
+    return;
+  }
+
+  OC_DBG("processing %d events", fd_count);
+
+  // process control flow events
+  if (process_wakeup_signal(dev, rdfds)) {
+    fd_count--;
+  }
+
+#ifdef OC_DYNAMIC_ALLOCATION
+  // check if network queue can consume all 'ready' events
+  int available_count = OC_DEVICE_MAX_NUM_CONCURRENT_REQUESTS -
+                        oc_network_get_event_queue_length(dev->device);
+  if (available_count < fd_count) {
+    // get the number of read file descriptors
+    int rfds_count = fds_count(rdfds, max_read_fd);
+    int removed = remove_random_fds(rdfds, rfds_count, max_read_fd,
+                                    rfds_count - available_count);
+    fd_count -= removed;
+  }
+#else  /* !OC_DYNAMIC_ALLOCATION */
+  (void)max_read_fd;
+#endif /* OC_DYNAMIC_ALLOCATION */
+
+  for (int i = 0; i < fd_count; i++) {
+    if (process_event(dev, rdfds, wfds) < 0) {
+      break;
+    }
+  }
+}
+
 static void *
 network_event_thread(void *data)
 {
   ip_context_t *dev = (ip_context_t *)data;
-
-  fd_set setfds;
   FD_ZERO(&dev->rfds);
-  /* Monitor network interface changes on the platform from only the 0th logical
-   * device
-  if (dev->device == 0) {
-    FD_SET(ifchange_sock, &dev->rfds);
-  }
-   */
-  FD_SET(dev->wakeup_pipe[0], &dev->rfds);
 
   oc_udp_add_socks_to_fd_set(dev);
+  add_control_flow_rfds(&dev->rfds, dev);
 #ifdef OC_TCP
   oc_tcp_add_socks_to_fd_set(dev);
+  oc_tcp_add_controlflow_socks_to_rfd_set(&dev->rfds, dev);
 #endif /* OC_TCP */
 
-  int i, n;
+  // int i, n;
+  int max_read_fd = FD_SETSIZE;
+  fd_set last_rdfds;
+  FD_ZERO(&last_rdfds);
 
   while (dev->terminate != 1) {
-    setfds = dev->rfds;
-    n = select(FD_SETSIZE, &setfds, NULL, NULL, NULL);
+    // setfds = dev->rfds;
+    fd_set rdfds = dev->rfds;
 
-    if (FD_ISSET(dev->wakeup_pipe[0], &setfds)) {
-      OC_DBG("network_event_thread select: dev->wakeup_pipe[0]");
-      char buf;
-      // write to pipe shall not block - so read the byte we wrote
-      if (read(dev->wakeup_pipe[0], &buf, 1) < 0) {
-        // intentionally left blank
-      }
+#ifdef OC_DYNAMIC_ALLOCATION
+    if (!fd_sets_are_equal(&rdfds, &last_rdfds)) {
+      // fd set has changed -> recalculate max fd
+      max_read_fd = fds_max(&rdfds);
+      last_rdfds = rdfds;
     }
+
+    if (oc_network_get_event_queue_length(dev->device) >=
+        OC_DEVICE_MAX_NUM_CONCURRENT_REQUESTS) {
+      // the queue is full -> add only control flow rfds
+      FD_ZERO(&rdfds);
+      add_control_flow_rfds(&rdfds, dev);
+#ifdef OC_TCP
+      oc_tcp_add_controlflow_socks_to_rfd_set(&rdfds, dev);
+#endif /* OC_TCP */
+    }
+#endif /* OC_DYNAMIC_ALLOCATION */
+
+    int n = select(FD_SETSIZE, &rdfds, NULL, NULL, NULL);
 
     if (dev->terminate) {
       break;
     }
 
-    for (i = 0; i < n; i++) {
-      oc_message_t *message = oc_allocate_message();
-
-      if (!message) {
-        break;
-      }
-
-      message->endpoint.device = dev->device;
-
-      if (oc_udp_receive_message(dev, &setfds, message) ==
-          ADAPTER_STATUS_RECEIVE) {
-        OC_DBG("network_event_thread oc_udp_receive_message");
-        goto common;
-      }
-#ifdef OC_TCP
-      if (oc_tcp_receive_message(dev, &setfds, message) ==
-          ADAPTER_STATUS_RECEIVE) {
-        OC_DBG("network_event_thread oc_tcp_receive_message");
-        goto common;
-      }
-#endif /* OC_TCP */
-
-      oc_message_unref(message);
-      continue;
-
-    common:
-      OC_DBG("Incoming message of size %zd bytes from", message->length);
-      OC_LOGipaddr(message->endpoint);
-      OC_DBG("interface index %d", message->endpoint.interface_index);
-      OC_DBG("%s", "");
-
-      oc_network_receive_event(message);
-    }
+    process_events(dev, &rdfds, NULL, n, max_read_fd);
   }
   return NULL;
 }
@@ -1586,6 +1759,7 @@ signal_event_thread(ip_context_t *dev)
   }
 }
 
+#ifdef OC_DYNAMIC_ALLOCATION
 void
 oc_connectivity_wakeup(size_t device)
 {
@@ -1597,6 +1771,7 @@ oc_connectivity_wakeup(size_t device)
 
   signal_event_thread(dev);
 }
+#endif /* OC_DYNAMIC_ALLOCATION */
 
 void
 oc_connectivity_shutdown(size_t device)
